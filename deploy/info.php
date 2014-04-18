94139935ea07eee556aefe7f6b1339308672f110
Darryl Sokoloski <dsokoloski@clearfoundation.com>
2014-04-18 19:16:28 -0400

    Fixed archive renamer script for the last time...

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'configuration_backup';
-$app['version'] = '1.5.17';
+$app['version'] = '1.5.18';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
