cc9d0ed7a3141bbeda81ed4ef8a8143f90797fbe
pcbaldwin <pbaldwin@clearfoundation.com>
2015-03-13 15:28:36 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'configuration_backup';
-$app['version'] = '2.0.18';
+$app['version'] = '2.0.20';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-configuration-backup.spec
+++ packaging/app-configuration-backup.spec
@@ -1,7 +1,7 @@
 
 Name: app-configuration-backup
 Epoch: 1
-Version: 2.0.18
+Version: 2.0.20
 Release: 1%{dist}
 Summary: Configuration Backup and Restore
 License: GPLv3
