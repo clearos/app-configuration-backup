76141a2ac09632987d4181bdef02336c475360cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:52:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Iṣeto ni afẹyinti ati mimu pada app faye gba ohun IT lati ya a foto ti gbogbo iṣeto ni eto ti awọn eto, gbigba rorun atunse ni awọn iṣẹlẹ ti data pipadanu.';
+$lang['configuration_backup_app_name'] = 'Iṣeto ni Afẹyinti ki o si pada';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Afẹyinti filename ni invalid.';
+$lang['configuration_backup_backup_now'] = 'afẹyinti Bayi';
+$lang['configuration_backup_checking_version'] = 'Yiyewo pamosi version ibamu';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'O wa ti o daju ti o fẹ lati mu pada nipa lilo awọn wọnyi pamosi faili?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory ko le wa ni iyipada.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Gbigba ati fifi apps. Jọwọ jẹ alaisan.';
+$lang['configuration_backup_file_information'] = 'faili Information';
+$lang['configuration_backup_importing_ldap'] = 'Akowọle LDAP liana';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Rẹ eto gbọdọ wa ni aami ṣaaju ki o to pada sipo iṣeto ni eto.';
+$lang['configuration_backup_older_version_warn'] = 'Agbalagba awọn ẹya ma ko ni app fi sori ẹrọ hàn - jọwọ fi sori ẹrọ ti a beere apps lati ọja lẹhin ti pada';
+$lang['configuration_backup_release_mismatch'] = 'Awọn ti ikede alaye ninu awọn afẹyinti faili ko ni ko baramu awọn ti ikede sori ẹrọ lori eto yi.';
+$lang['configuration_backup_release_missing'] = 'Awọn ti ikede alaye ko si ni awọn faili afẹyinti.';
+$lang['configuration_backup_release_unsupported:'] = 'Pada sipo lati awọn wọnyi ti ikede ti wa ni ko ni atilẹyin:';
+$lang['configuration_backup_restarting_service:'] = 'Titun iṣẹ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Pada ni ilọsiwaju.';
+$lang['configuration_backup_restore_complete'] = 'Pada pari.';
+$lang['configuration_backup_restore_file'] = 'Archive File';
+$lang['configuration_backup_restore_from_archive'] = 'Pada Lati Archive';
+$lang['configuration_backup_restore_log'] = 'pada Wọle';
+$lang['configuration_backup_restore_type:'] = 'Pada Iru:';
+$lang['configuration_backup_starting_event_system'] = 'Ti o bere iṣẹlẹ atẹle iṣẹ';
+$lang['configuration_backup_status_data_not_available'] = 'Ipo data ko wa.';
+$lang['configuration_backup_stopping_event_system'] = 'Idekun iṣẹlẹ atẹle iṣẹ';
+$lang['configuration_backup_unable_to_start_restore'] = 'Lagbara lati bẹrẹ pada isẹ';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking pamosi faili';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Nduro fun imudojuiwọn software eto';
+$lang['configuration_backup_yum_busy'] = 'Package faili ni o nšišẹ.';
+
