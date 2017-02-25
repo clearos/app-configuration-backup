76b24ae905319a7df4bd9c663461b2c302450675
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:52:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'I isipele ukumisa bese ubuyisela lokusebenza ivumela umlawuli ukuthatha isithombe wonke amasethingi iswa kohlelo, okuvumela ukubuyiselwa lula uma kwenzeka ukulahleka kwedatha.';
+$lang['configuration_backup_app_name'] = 'Ukucushwa ubuyisele';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'igama lefayela Backup ayivumelekile.';
+$lang['configuration_backup_backup_now'] = 'Backup Manje';
+$lang['configuration_backup_checking_version'] = 'Ihlola archive version ukuhambisana';
+$lang['configuration_backup_clear'] = 'Sula';
+$lang['configuration_backup_confirm_restore'] = 'Uqinisekile ukuthi ufuna ukubuyisela usebenzisa ifayela elandelayo archive?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory ayikwazanga ukuguqulwa.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Ilanda futhi ukufaka izinhlelo zokusebenza. Sicela ubekezele.';
+$lang['configuration_backup_file_information'] = 'Ulwazi Ifayela';
+$lang['configuration_backup_importing_ldap'] = 'Iyangenisa LDAP lwemibhalo';
+$lang['configuration_backup_network_information'] = 'Ulwazi Network';
+$lang['configuration_backup_not_registered'] = 'uhlelo yakho kufanele ibhaliswe ngaphambi uzowubuyisela izilungiselelo ukucushwa.';
+$lang['configuration_backup_older_version_warn'] = 'Izinguqulo ezindala musa ukufaka uhlelo ukufaka manifest - sicela ufake izinhlelo zokusebenza efunekayo Marketplace emva ukubuyisela';
+$lang['configuration_backup_release_mismatch'] = 'Ulwazi nguqulo ifayela yokusekelayo ayimeshi version efakwe kulesi simiso.';
+$lang['configuration_backup_release_missing'] = 'Ulwazi version ayitholakali kule ifayela yokusekelayo.';
+$lang['configuration_backup_release_unsupported:'] = 'Iyabuyisela kusukela version okulandelayo ayisekelwe:';
+$lang['configuration_backup_restarting_service:'] = 'Iqalisa phansi service:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Buyisela kuyaqhubeka.';
+$lang['configuration_backup_restore_complete'] = 'Buyisela ephelele.';
+$lang['configuration_backup_restore_file'] = 'Ifayela Archive';
+$lang['configuration_backup_restore_from_archive'] = 'Buyisela Kusuka Archive';
+$lang['configuration_backup_restore_log'] = 'Buyisela Log';
+$lang['configuration_backup_restore_type:'] = 'Buyisela uhlobo lwensiza:';
+$lang['configuration_backup_starting_event_system'] = 'Iqalisa umcimbi qapha service';
+$lang['configuration_backup_status_data_not_available'] = 'Idatha Isimo ayitholakali.';
+$lang['configuration_backup_stopping_event_system'] = 'Ukuzinika umcimbi qapha service';
+$lang['configuration_backup_unable_to_start_restore'] = 'Ayikwazi ukuqala buyisela futhi ukusebenza';
+$lang['configuration_backup_unpacking_archive'] = 'ifayela archive Unpacking';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Ilinde izivuseleli zama-software uhlelo';
+$lang['configuration_backup_yum_busy'] = 'Imenenja Iphakheji imatasa.';
+
