18176979719df41c1af3d978d2112b3bb4dd3321
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:44:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'ಸಂರಚನಾ ಬ್ಯಾಕ್ಅಪ್ ಮತ್ತು ಅಪ್ಲಿಕೇಶನ್ ಪುನಃಸ್ಥಾಪಿಸಲು ಡೇಟಾ ನಷ್ಟ ಸಂಭವಿಸಿದಾಗ ಸುಲಭ ಪುನಃ ಅವಕಾಶ ವ್ಯವಸ್ಥೆಯ ಎಲ್ಲಾ ಸಂರಚನಾ ಸೆಟ್ಟಿಂಗ್ಗಳನ್ನು ಸ್ನ್ಯಾಪ್ಶಾಟ್ ಅನ್ನು ತೆಗೆದುಕೊಳ್ಳುವ ನಿರ್ವಾಹಕರಾಗಿ ಅನುಮತಿಸುತ್ತದೆ.';
+$lang['configuration_backup_app_name'] = 'ಸಂರಚನೆ ಬ್ಯಾಕ್ಅಪ್ ಮತ್ತು ಪುನಃಸ್ಥಾಪನೆ';
+$lang['configuration_backup_archives'] = 'ಆರ್ಕೈವ್ಸ್';
+$lang['configuration_backup_backup_file_invalid'] = 'ಬ್ಯಾಕಪ್ ಫೈಲ್ ಹೆಸರು ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['configuration_backup_backup_now'] = 'ಈಗ ಬ್ಯಾಕಪ್';
+$lang['configuration_backup_checking_version'] = 'ಆರ್ಕೈವ್ ಆವೃತ್ತಿ ಹೊಂದಾಣಿಕೆ ಪರಿಶೀಲಿಸಲಾಗುತ್ತಿದೆ';
+$lang['configuration_backup_clear'] = 'ಸ್ಪಷ್ಟ';
+$lang['configuration_backup_confirm_restore'] = 'ನೀವು ಈ ಆರ್ಕೈವ್ ಕಡತ ಬಳಸಿಕೊಂಡು ಪುನಃಸ್ಥಾಪಿಸಲು ಬಯಸಿದ್ದೀರಾ?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ಡೈರೆಕ್ಟರಿ ಪರಿವರ್ತನೆ ಸಾಧ್ಯವಿಲ್ಲ.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ಡೌನ್ಲೋಡ್ ಮತ್ತು ಅಪ್ಲಿಕೇಶನ್ಗಳನ್ನು ಸ್ಥಾಪಿಸುವುದಕ್ಕಾಗಿ. ದಯವಿಟ್ಟು ತಾಳ್ಮೆಯಿಂದಿರಿ.';
+$lang['configuration_backup_file_information'] = 'ಫೈಲ್ ಮಾಹಿತಿ';
+$lang['configuration_backup_importing_ldap'] = 'ಆಮದು LDAP ಕೋಶ';
+$lang['configuration_backup_network_information'] = 'ನೆಟ್ವರ್ಕ್ ಮಾಹಿತಿ';
+$lang['configuration_backup_not_registered'] = 'ನಿಮ್ಮ ಸಿಸ್ಟಂ ಕಾನ್ಫಿಗರೇಶನ್ ಸೆಟ್ಟಿಂಗ್ಗಳನ್ನು ಕಾಪಾಡುವ ಮೊದಲು ದಾಖಲಿಸಬೇಕು.';
+$lang['configuration_backup_older_version_warn'] = 'ಹಳೆಯ ಆವೃತ್ತಿಗಳು ಮ್ಯಾನಿಫೆಸ್ಟ್ ಅನುಸ್ಥಾಪಿಸಲು ಅಪ್ಲಿಕೇಶನ್ ಒಳಗೊಂಡಿಲ್ಲ - ಮಾರುಕಟ್ಟೆ ಅಗತ್ಯ ಅಪ್ಲಿಕೇಶನ್ಗಳನ್ನು ಅನುಸ್ಥಾಪಿಸಲು ದಯವಿಟ್ಟು ಪುನಃಸ್ಥಾಪಿಸಲು ನಂತರ';
+$lang['configuration_backup_release_mismatch'] = 'ಬ್ಯಾಕಪ್ ಫೈಲ್ ರಲ್ಲಿ ಆವೃತ್ತಿ ಮಾಹಿತಿ ನಿಮ್ಮ ಗಣಕದಲ್ಲಿ ಅನುಸ್ಥಾಪಿತವಾಗಿರುವ ಆವೃತ್ತಿಗೆ ಹೊಂದಾಣಿಕೆಯಾಗುವುದಿಲ್ಲ.';
+$lang['configuration_backup_release_missing'] = 'ಆವೃತ್ತಿ ಮಾಹಿತಿ ಬ್ಯಾಕಪ್ ಫೈಲ್ ಲಭ್ಯವಿಲ್ಲ.';
+$lang['configuration_backup_release_unsupported:'] = 'ಕೆಳಗಿನ ಆವೃತ್ತಿಯಿಂದ ಕಾಪಾಡುವ ಬೆಂಬಲಿಸುವುದಿಲ್ಲ';
+$lang['configuration_backup_restarting_service:'] = 'ಪುನರ್ ಸೇವೆ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'ಪ್ರಗತಿಯಲ್ಲಿದೆ ಮರುಸ್ಥಾಪಿಸಿ.';
+$lang['configuration_backup_restore_complete'] = 'ಸಂಪೂರ್ಣ ಮರುಸ್ಥಾಪಿಸಿ.';
+$lang['configuration_backup_restore_file'] = 'ಆರ್ಕೈವ್ ಫೈಲ್';
+$lang['configuration_backup_restore_from_archive'] = 'ಆರ್ಕೈವ್ ಮರುಸ್ಥಾಪಿಸಿ';
+$lang['configuration_backup_restore_log'] = 'ಮರುಸ್ಥಾಪಿಸಿ ಲಾಗ್';
+$lang['configuration_backup_restore_type:'] = 'ಮರುಸ್ಥಾಪಿಸಿ ಪ್ರಕಾರ:';
+$lang['configuration_backup_starting_event_system'] = 'ಆರಂಭಿಕ ಕ್ರಿಯೆಯನ್ನು ಮೇಲ್ವಿಚಾರಣಾ ಸೇವೆಯನ್ನು';
+$lang['configuration_backup_status_data_not_available'] = 'ಸ್ಥಿತಿ ಮಾಹಿತಿ ಲಭ್ಯವಿಲ್ಲ.';
+$lang['configuration_backup_stopping_event_system'] = 'ನಿಲ್ಲಿಸುವ ಕ್ರಿಯೆಯನ್ನು ಮೇಲ್ವಿಚಾರಣಾ ಸೇವೆಯನ್ನು';
+$lang['configuration_backup_unable_to_start_restore'] = 'ಪುನಃಸ್ಥಾಪಿಸಲು ಆರಂಭಿಸಲು ಸಾಧ್ಯವಿಲ್ಲ';
+$lang['configuration_backup_unpacking_archive'] = 'ಪೊಟ್ಟಣ ಬಿಚ್ಚುವಿಕೆ ಆರ್ಕೈವ್ ಕಡತ';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'ಸಾಫ್ಟ್ವೇರ್ ನವೀಕರಣಗಳನ್ನು ವ್ಯವಸ್ಥೆಯ ನಿರೀಕ್ಷಿಸಲಾಗುತ್ತಿದೆ';
+$lang['configuration_backup_yum_busy'] = 'ಪ್ಯಾಕೇಜ್ ಮ್ಯಾನೇಜರ್ ಕಾರ್ಯನಿರತವಾಗಿದೆ.';
+
