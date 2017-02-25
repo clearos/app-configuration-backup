d298b2a8f57bff86176eefe5a18082240ebae23d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:48:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Резервное копирование и восстановление конфигурации приложение позволяет администратору сделать снимок всех параметров конфигурации системы, что позволяет легко восстановить в случае потери данных.';
+$lang['configuration_backup_app_name'] = 'Резервное копирование и восстановление конфигурации';
+$lang['configuration_backup_archives'] = 'архив';
+$lang['configuration_backup_backup_file_invalid'] = 'Резервное копирование файла является недействительным.';
+$lang['configuration_backup_backup_now'] = 'Создать резервную копию';
+$lang['configuration_backup_checking_version'] = 'Проверка архива совместимости версий';
+$lang['configuration_backup_clear'] = 'Очистить';
+$lang['configuration_backup_confirm_restore'] = 'Вы уверены, что хотите восстановить, используя следующий файл архива?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Справочник не могут быть преобразованы.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Загрузка и установка приложений. Пожалуйста, будьте терпеливы.';
+$lang['configuration_backup_file_information'] = 'информация о файле';
+$lang['configuration_backup_importing_ldap'] = 'Импорт каталогов LDAP';
+$lang['configuration_backup_network_information'] = 'информация о сети';
+$lang['configuration_backup_not_registered'] = 'Ваша система должна быть зарегистрирована до восстановления параметров конфигурации.';
+$lang['configuration_backup_older_version_warn'] = 'Более старые версии не включают в себя приложение установить манифест - пожалуйста, установите необходимые приложения из Marketplace после восстановления';
+$lang['configuration_backup_release_mismatch'] = 'Информация о версии в файле резервной копии не совпадает с версией, установленной на этой системе.';
+$lang['configuration_backup_release_missing'] = 'Информация о версии не доступна в файле резервной копии.';
+$lang['configuration_backup_release_unsupported:'] = 'Восстановление из следующей версии не поддерживается:';
+$lang['configuration_backup_restarting_service:'] = 'Перезапуск службы:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Восстановление продолжается.';
+$lang['configuration_backup_restore_complete'] = 'Восстановление завершено.';
+$lang['configuration_backup_restore_file'] = 'Архив файлов';
+$lang['configuration_backup_restore_from_archive'] = 'Восстановление из архива';
+$lang['configuration_backup_restore_log'] = 'Восстановление Вход';
+$lang['configuration_backup_restore_type:'] = 'Тип восстановления:';
+$lang['configuration_backup_starting_event_system'] = 'Запуск службы монитора событий';
+$lang['configuration_backup_status_data_not_available'] = 'Данные о состоянии недоступны.';
+$lang['configuration_backup_stopping_event_system'] = 'Остановка службы монитора событий';
+$lang['configuration_backup_unable_to_start_restore'] = 'Невозможно запустить операцию восстановления';
+$lang['configuration_backup_unpacking_archive'] = 'Распаковка архива файлов';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Ожидание системы обновлений программного обеспечения';
+$lang['configuration_backup_yum_busy'] = 'менеджер пакетов занят.';
+
