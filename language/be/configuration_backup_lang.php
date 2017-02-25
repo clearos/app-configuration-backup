025b22d3ce0882290f37f5f9324ed96ec7e2ff5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:39:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Рэзервовае капіяванне і аднаўленне канфігурацыі прыкладанне дазваляе адміністратару зрабіць здымак ўсіх параметраў канфігурацыі сістэмы, што дазваляе лёгка аднавіць у выпадку страты дадзеных.';
+$lang['configuration_backup_app_name'] = 'Рэзервовае капіяванне і аднаўленне канфігурацыі';
+$lang['configuration_backup_archives'] = 'архіў';
+$lang['configuration_backup_backup_file_invalid'] = 'Рэзервовае капіраванне файла з&#39;яўляецца несапраўдным.';
+$lang['configuration_backup_backup_now'] = 'Стварыць рэзервовую копію';
+$lang['configuration_backup_checking_version'] = 'Праверка архіва сумяшчальнасці версій';
+$lang['configuration_backup_clear'] = 'ясна';
+$lang['configuration_backup_confirm_restore'] = 'Вы ўпэўненыя, што хочаце аднавіць, выкарыстоўваючы наступны файл архіва?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Даведнік не могуць быць ператвораныя.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Загрузка і ўстаноўка прыкладанняў. Калі ласка, будзьце цярплівыя.';
+$lang['configuration_backup_file_information'] = 'інфармацыя аб файле';
+$lang['configuration_backup_importing_ldap'] = 'Імпарт каталогаў LDAP';
+$lang['configuration_backup_network_information'] = 'інфармацыя аб сетцы';
+$lang['configuration_backup_not_registered'] = 'Сістэма мусіць быць зарэгістраваная да аднаўлення параметраў канфігурацыі.';
+$lang['configuration_backup_older_version_warn'] = 'Больш старыя версіі не ўключаюць у сябе дадатак ўсталяваць маніфест - калі ласка, усталюйце неабходныя прыкладання з Marketplace пасля аднаўлення';
+$lang['configuration_backup_release_mismatch'] = 'Інфармацыя аб версіі ў файле рэзервовай копіі не супадае з версіяй, усталяванай на гэтай сістэме.';
+$lang['configuration_backup_release_missing'] = 'Інфармацыя аб версіі не даступная ў файле рэзервовай копіі.';
+$lang['configuration_backup_release_unsupported:'] = 'Аднаўленне з наступнай версіі не падтрымліваецца:';
+$lang['configuration_backup_restarting_service:'] = 'Перазапуск службы:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Аднаўленне працягваецца.';
+$lang['configuration_backup_restore_complete'] = 'Аднаўленне завершана.';
+$lang['configuration_backup_restore_file'] = 'архіў файлаў';
+$lang['configuration_backup_restore_from_archive'] = 'Аднаўленне з архіву';
+$lang['configuration_backup_restore_log'] = 'аднаўленне Уваход';
+$lang['configuration_backup_restore_type:'] = 'Тып аднаўлення:';
+$lang['configuration_backup_starting_event_system'] = 'Запуск службы манітора падзей';
+$lang['configuration_backup_status_data_not_available'] = 'Дадзеныя аб стане недаступныя.';
+$lang['configuration_backup_stopping_event_system'] = 'Прыпынак службы манітора падзей';
+$lang['configuration_backup_unable_to_start_restore'] = 'Немагчыма запусціць аперацыю аднаўлення';
+$lang['configuration_backup_unpacking_archive'] = 'Распакаванне архіва файлаў';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Чаканне сістэмы абнаўленняў праграмнага забеспячэння';
+$lang['configuration_backup_yum_busy'] = 'менеджэр пакетаў заняты.';
+
