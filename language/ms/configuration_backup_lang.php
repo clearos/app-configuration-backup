eea8ea9dc6e17f7aa84fdbabef46b81e72164d65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:46:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Sandaran konfigurasi dan memulihkan app membolehkan pentadbir untuk mengambil gambar semua tetapan konfigurasi sistem, yang membolehkan pemulihan mudah sekiranya berlaku kehilangan data.';
+$lang['configuration_backup_app_name'] = 'Konfigurasi Backup dan Simpan semula';
+$lang['configuration_backup_archives'] = 'Arkib';
+$lang['configuration_backup_backup_file_invalid'] = 'nama fail sandaran tidak sah.';
+$lang['configuration_backup_backup_now'] = 'Backup Now';
+$lang['configuration_backup_checking_version'] = 'Semakan arkib keserasian versi';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'Adakah anda pasti anda mahu memulihkan menggunakan fail arkib yang berikut?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Direktori tidak boleh ditukar.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Memuat turun dan memasang aplikasi. Sila bersabar.';
+$lang['configuration_backup_file_information'] = 'Maklumat fail';
+$lang['configuration_backup_importing_ldap'] = 'Mengimport direktori LDAP';
+$lang['configuration_backup_network_information'] = 'rangkaian Maklumat';
+$lang['configuration_backup_not_registered'] = 'Sistem anda mesti mendaftar sebelum mengembalikan tetapan tatarajah.';
+$lang['configuration_backup_older_version_warn'] = 'Versi lama tidak termasuk aplikasi memasang nyata - sila memasang aplikasi yang diperlukan dari Marketplace selepas memulihkan';
+$lang['configuration_backup_release_mismatch'] = 'Maklumat yang versi dalam fail sandaran yang tidak sepadan dengan versi yang dipasang pada sistem ini.';
+$lang['configuration_backup_release_missing'] = 'Maklumat yang versi tidak boleh didapati dalam fail sandaran.';
+$lang['configuration_backup_release_unsupported:'] = 'Memulihkan daripada versi berikut tidak disokong:';
+$lang['configuration_backup_restarting_service:'] = 'perkhidmatan Memulakan semula:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Simpan semula dalam pelaksanaan.';
+$lang['configuration_backup_restore_complete'] = 'Simpan semula selesai.';
+$lang['configuration_backup_restore_file'] = 'Arkib Fail';
+$lang['configuration_backup_restore_from_archive'] = 'Memulihkan Dari Arkib';
+$lang['configuration_backup_restore_log'] = 'Simpan semula Log';
+$lang['configuration_backup_restore_type:'] = 'Simpan semula jenis:';
+$lang['configuration_backup_starting_event_system'] = 'Bermula perkhidmatan monitor acara';
+$lang['configuration_backup_status_data_not_available'] = 'data Status tidak boleh didapati.';
+$lang['configuration_backup_stopping_event_system'] = 'Menghentikan perkhidmatan monitor acara';
+$lang['configuration_backup_unable_to_start_restore'] = 'Tidak dapat memulakan operasi pengembalian';
+$lang['configuration_backup_unpacking_archive'] = 'fail arkib Mengeluarkan mesin dari bungkusan';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Menunggu sistem kemas kini perisian';
+$lang['configuration_backup_yum_busy'] = 'pengurus pakej sibuk.';
+
