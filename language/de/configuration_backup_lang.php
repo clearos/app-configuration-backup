9c9fb89d958746b23304441309ef8dcdd7bb28f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:42:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Die Konfiguration Backup- und Restore-App ermöglicht es einem Administrator einen Snapshot aller Konfigurationseinstellungen des Systems zu übernehmen, so dass die einfache Wiederherstellung im Falle von Datenverlust.';
+$lang['configuration_backup_app_name'] = 'Konfiguration Sichern und Wiederherstellen';
+$lang['configuration_backup_archives'] = 'Archiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup-Dateiname ist ungültig.';
+$lang['configuration_backup_backup_now'] = 'Backup Now';
+$lang['configuration_backup_checking_version'] = 'Überprüfen Archiv Versionskompatibilität';
+$lang['configuration_backup_clear'] = 'Klar';
+$lang['configuration_backup_confirm_restore'] = 'Sind Sie sicher, dass Sie mit dem folgenden Archivdatei wiederherstellen möchten?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Verzeichnis konnte nicht konvertiert werden.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Das Herunterladen und Installieren von Apps. Bitte haben Sie Geduld.';
+$lang['configuration_backup_file_information'] = 'Dateiinformationen';
+$lang['configuration_backup_importing_ldap'] = 'Importieren von LDAP-Verzeichnis';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Ihr System muss vor der Wiederherstellung Konfigurationseinstellungen registriert werden.';
+$lang['configuration_backup_older_version_warn'] = 'Ältere Versionen beinhalten keine App installieren manifestieren - bitte erforderlich Apps aus dem Marketplace installieren nach der Wiederherstellung';
+$lang['configuration_backup_release_mismatch'] = 'Die Versionsinformationen in der Sicherungsdatei nicht mit der Version auf dem System installiert ist.';
+$lang['configuration_backup_release_missing'] = 'Die Versionsinformation ist in der Sicherungsdatei nicht verfügbar ist.';
+$lang['configuration_backup_release_unsupported:'] = 'Wiederherstellen aus der folgenden Version wird nicht unterstützt:';
+$lang['configuration_backup_restarting_service:'] = 'Neustarten Service:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Wiederherstellung im Gange.';
+$lang['configuration_backup_restore_complete'] = 'Wiederherstellung abgeschlossen.';
+$lang['configuration_backup_restore_file'] = 'Archivdatei';
+$lang['configuration_backup_restore_from_archive'] = 'Wiederherstellen von Archiv';
+$lang['configuration_backup_restore_log'] = 'Wiederherstellen Log';
+$lang['configuration_backup_restore_type:'] = 'Wiederherstellen Typ:';
+$lang['configuration_backup_starting_event_system'] = 'Startereignisüberwachung';
+$lang['configuration_backup_status_data_not_available'] = 'Statusdaten nicht zur Verfügung.';
+$lang['configuration_backup_stopping_event_system'] = 'Stoppen Ereignisüberwachung';
+$lang['configuration_backup_unable_to_start_restore'] = 'Konnte nicht gestartet Betrieb wieder herstellen';
+$lang['configuration_backup_unpacking_archive'] = 'Auspacken Archivdatei';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Warten auf Software-Updates System';
+$lang['configuration_backup_yum_busy'] = 'Package Manager ist besetzt.';
+
