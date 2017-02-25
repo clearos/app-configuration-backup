e47fed786160212543d01a23c9d849df2e218397
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:49:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'backup konfiguracijo in obnovitev aplikacije omogoča skrbniku, da bo posnetek vseh nastavitev konfiguracije sistema, ki omogoča enostavno obnovitev v primeru izgube podatkov.';
+$lang['configuration_backup_app_name'] = 'Konfiguracija varnostno kopiranje in obnovitev';
+$lang['configuration_backup_archives'] = 'Arhiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup ime datoteke je neveljavna.';
+$lang['configuration_backup_backup_now'] = 'backup Now';
+$lang['configuration_backup_checking_version'] = 'Preverjanje arhiv združljivost različice';
+$lang['configuration_backup_clear'] = 'Počisti';
+$lang['configuration_backup_confirm_restore'] = 'Ali ste prepričani, da želite obnoviti s pomočjo naslednje arhivske datoteke?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Imenika ni bilo mogoče pretvoriti.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Prenos in nameščanje aplikacij. Prosim, bodite potrpežljivi.';
+$lang['configuration_backup_file_information'] = 'Informacije datoteke';
+$lang['configuration_backup_importing_ldap'] = 'Uvažanje imenika LDAP';
+$lang['configuration_backup_network_information'] = 'Informacijska mreža';
+$lang['configuration_backup_not_registered'] = 'Sistem mora biti registrirana pred obnovo nastavitve.';
+$lang['configuration_backup_older_version_warn'] = 'Starejše različice ne vključujejo namestitev aplikacije manifest - prosim namestite zahtevane aplikacije iz Marketplace po obnovi';
+$lang['configuration_backup_release_mismatch'] = 'Informacije o različici iz varnostne kopije ne ujema z različico, nameščeno na tem sistemu.';
+$lang['configuration_backup_release_missing'] = 'Informacije o različici ni na voljo v varnostne kopije.';
+$lang['configuration_backup_release_unsupported:'] = 'Obnavljanje iz naslednjega različica ni podprta:';
+$lang['configuration_backup_restarting_service:'] = 'Ponovni zagon storitve:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Povrni v teku.';
+$lang['configuration_backup_restore_complete'] = 'Obnovitev končana.';
+$lang['configuration_backup_restore_file'] = 'Arhiv datotek';
+$lang['configuration_backup_restore_from_archive'] = 'Obnovitev Od Arhiv';
+$lang['configuration_backup_restore_log'] = 'Obnovitev Prijava';
+$lang['configuration_backup_restore_type:'] = 'Obnovitev vrsto:';
+$lang['configuration_backup_starting_event_system'] = 'Zagon storitve nadzornik dogodkov';
+$lang['configuration_backup_status_data_not_available'] = 'Podatki stanja ni na voljo.';
+$lang['configuration_backup_stopping_event_system'] = 'Ustavljanje storitev nadzornik dogodkov';
+$lang['configuration_backup_unable_to_start_restore'] = 'Ni moč zagnati obnovitev delovanja';
+$lang['configuration_backup_unpacking_archive'] = 'Razpakiranje arhiv';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Čakam na sistem posodobitve programske opreme';
+$lang['configuration_backup_yum_busy'] = 'paket manager je zaseden.';
+
