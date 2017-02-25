e612830081ba0b48fbd3005f840522a735bc840e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:50:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'ఆకృతీకరణ బ్యాకప్ మరియు అనువర్తనం పునరుద్ధరణ డేటా నష్టం జరిగినప్పుడు సులభం పునరుద్ధరణ అనుమతిస్తుంది, సిస్టమ్ యొక్క అన్ని కాన్ఫిగరేషన్ సెట్టింగులను ఒక స్నాప్షాట్ తీసుకోవాలని ఒక నిర్వాహకుడు అనుమతిస్తుంది.';
+$lang['configuration_backup_app_name'] = 'ఆకృతీకరణ బ్యాకప్ మరియు పునరుద్ధరించు';
+$lang['configuration_backup_archives'] = 'ఆర్కైవ్స్';
+$lang['configuration_backup_backup_file_invalid'] = 'బ్యాకప్ ఫైల్ పేరు చెల్లదు.';
+$lang['configuration_backup_backup_now'] = 'భద్రపరచు';
+$lang['configuration_backup_checking_version'] = 'ఆర్కైవ్ వెర్షన్ అనుకూలతను తనిఖీ';
+$lang['configuration_backup_clear'] = 'ప్రశాంతంగా';
+$lang['configuration_backup_confirm_restore'] = 'మీరు క్రింది ఆర్కైవ్ ఫైల్ ఉపయోగించి పునరుద్ధరించడానికి కావలసిన నిశ్చయించుకున్నారా?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'డైరెక్టరీ మార్చబడతాయి కాలేదు.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'డౌన్లోడ్ మరియు అనువర్తనాలను వ్యవస్థాపించడం. దయచేసి ఓపిక పట్టండి.';
+$lang['configuration_backup_file_information'] = 'ఫైలు సమాచారం';
+$lang['configuration_backup_importing_ldap'] = 'దిగుమతి LDAP డైరెక్టరీ';
+$lang['configuration_backup_network_information'] = 'నెట్వర్క్ ఇన్ఫర్మేషన్';
+$lang['configuration_backup_not_registered'] = 'మీ సిస్టమ్ కాన్ఫిగరేషన్ సెట్టింగులను పునరుద్ధరణ ముందు నమోదు చేయాలి.';
+$lang['configuration_backup_older_version_warn'] = 'పాత వెర్షన్లు అనువర్తనం చేర్చవద్దు మానిఫెస్ట్ ఇన్స్టాల్ - Marketplace నుండి అవసరమైన అనువర్తనాలను ఇన్స్టాల్ దయచేసి పునరుద్ధరించడానికి తరువాత';
+$lang['configuration_backup_release_mismatch'] = 'బ్యాకప్ ఫైల్ లో వెర్షన్ సమాచారాన్ని ఈ సిస్టమ్పై ఇన్స్టాల్ వెర్షన్ సరిపోలడం లేదు.';
+$lang['configuration_backup_release_missing'] = 'వెర్షన్ సమాచారం బ్యాకప్ ఫైల్ లో అందుబాటులో లేదు.';
+$lang['configuration_backup_release_unsupported:'] = 'క్రింది వెర్షన్ నుండి పునరుద్ధరించడం మద్దతు లేదు:';
+$lang['configuration_backup_restarting_service:'] = 'పునఃప్రారంభించి సేవ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'పురోగతి లో పునరుద్ధరించు.';
+$lang['configuration_backup_restore_complete'] = 'పూర్తి పునరుద్ధరించు.';
+$lang['configuration_backup_restore_file'] = 'ఆర్కైవ్ ఫైల్';
+$lang['configuration_backup_restore_from_archive'] = 'ఆర్కైవ్ నుండి పునరుద్ధరించు';
+$lang['configuration_backup_restore_log'] = 'లోనికి ప్రవేశించండి పునరుద్ధరించు';
+$lang['configuration_backup_restore_type:'] = 'రకం పునరుద్ధరించు:';
+$lang['configuration_backup_starting_event_system'] = 'ఈవెంట్ మానిటర్ సేవ మొదలు';
+$lang['configuration_backup_status_data_not_available'] = 'అందుబాటులో లేదు హోదా డేటా.';
+$lang['configuration_backup_stopping_event_system'] = 'ఈవెంట్ మానిటర్ సేవ ఆపడం';
+$lang['configuration_backup_unable_to_start_restore'] = 'పునరుద్ధరణ చర్యను ప్రారంభించడం సాధ్యం కాదు';
+$lang['configuration_backup_unpacking_archive'] = 'మూట విడదీయుట ఆర్కైవ్ ఫైల్';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'సాఫ్ట్వేర్ నవీకరణలు వ్యవస్థ కోసం వేచి';
+$lang['configuration_backup_yum_busy'] = 'ప్యాకేజీ మేనేజర్ బిజీగా ఉంది.';
+
