df6bf1faee351658601e3a0c823b5584f0776d68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:44:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'ការបម្រុងទុកការកំណត់រចនាសម្ព័ន្ធនិងការស្តារកម្មវិធីអនុញ្ញាតឱ្យអ្នកគ្រប់គ្រងដើម្បីចាប់យករូបថតនៃការកំណត់ការកំណត់រចនាសម្ព័ន្ធទាំងអស់នៃប្រព័ន្ធមួយដែលអនុញ្ញាតឱ្យការស្តារការងាយស្រួលនៅក្នុងព្រឹត្តិការណ៍នៃការបាត់បង់ទិន្នន័យ។';
+$lang['configuration_backup_app_name'] = 'ការបម្រុងទុកការកំណត់រចនាសម្ព័ន្ធនិងការស្តារ';
+$lang['configuration_backup_archives'] = 'បណ្ណសារ';
+$lang['configuration_backup_backup_file_invalid'] = 'ឈ្មោះឯកសារបម្រុងទុកគឺមិនត្រឹមត្រូវ។';
+$lang['configuration_backup_backup_now'] = 'ការបម្រុងទុកឥឡូវនេះ';
+$lang['configuration_backup_checking_version'] = 'កំពុងពិនិត្យមើលភាពឆបគ្នាកំណែប័ណ្ណសារ';
+$lang['configuration_backup_clear'] = 'ការច្បាស់លាស់';
+$lang['configuration_backup_confirm_restore'] = 'តើអ្នកពិតជាចង់ស្តារការប្រើឯកសារប័ណ្ណសារខាងក្រោម?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ថតមិនអាចត្រូវបានបម្លែង។';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ទាញយកនិងដំឡើងកម្មវិធី។ សូម​អត់ធ្មត់។';
+$lang['configuration_backup_file_information'] = 'ពឯកសារ';
+$lang['configuration_backup_importing_ldap'] = 'ថត LDAP នាំចូល';
+$lang['configuration_backup_network_information'] = 'បណ្តាញព';
+$lang['configuration_backup_not_registered'] = 'ប្រព័ន្ធរបស់អ្នកត្រូវតែចុះបញ្ជីនៅមុនពេលស្ដារឡើងវិញនូវការកំណត់ការកំណត់រចនាសម្ព័ន្ធ។';
+$lang['configuration_backup_older_version_warn'] = 'កំណែចាស់មិនរួមបញ្ចូលកម្មវិធីដំឡើងសម្ដែង - សូមដំឡើងកម្មវិធីតម្រូវឱ្យមានពីទីផ្សារនេះបន្ទាប់ពីការស្តារ';
+$lang['configuration_backup_release_mismatch'] = 'ពកំណែនៅក្នុងឯកសារបម្រុងទុកមិនផ្គូផ្គងកំណែដែលបានដំឡើងនៅលើប្រព័ន្ធនេះ។';
+$lang['configuration_backup_release_missing'] = 'ពកំណែនេះគឺមិនអាចរកបាននៅក្នុងឯកសារបម្រុងទុក។';
+$lang['configuration_backup_release_unsupported:'] = 'ស្ដារពីកំណែដូចខាងក្រោមនេះមិនត្រូវបានគាំទ្រ:';
+$lang['configuration_backup_restarting_service:'] = 'សេវាចាប់ផ្ដើម:';
+$lang['configuration_backup_restore_already_in_progress'] = 'ស្តារក្នុងវឌ្ឍនភាព។';
+$lang['configuration_backup_restore_complete'] = 'ស្តារបញ្ចប់។';
+$lang['configuration_backup_restore_file'] = 'ឯកសារបណ្ណសារ';
+$lang['configuration_backup_restore_from_archive'] = 'ស្តារពីបណ្ណសារ';
+$lang['configuration_backup_restore_log'] = 'ស្តារចូល';
+$lang['configuration_backup_restore_type:'] = 'ស្តារប្រភេទ:';
+$lang['configuration_backup_starting_event_system'] = 'ការចាប់ផ្តើមសេវាម៉ូនីទ័រព្រឹត្តិការណ៍';
+$lang['configuration_backup_status_data_not_available'] = 'ទិន្នន័យស្ថានភាពមិនអាចរកបាន។';
+$lang['configuration_backup_stopping_event_system'] = 'បញ្ឈប់សេវាម៉ូនីទ័រព្រឹត្តិការណ៍';
+$lang['configuration_backup_unable_to_start_restore'] = 'មិនអាចចាប់ផ្ដើមប្រតិបត្តិការស្តារឡើង';
+$lang['configuration_backup_unpacking_archive'] = 'ឯកសារប័ណ្ណសារស្រាយ';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'កំពុងរង់ចាំសម្រាប់ប្រព័ន្ធឱ្យទាន់សម័យកម្មវិធី';
+$lang['configuration_backup_yum_busy'] = 'កម្មវិធីគ្រប់គ្រងកញ្ចប់គឺជាប់រវល់។';
+
