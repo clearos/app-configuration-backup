0cfc59462aa6f8932f0a1823aeab4fc7df79bc69
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:47:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'ਸੰਰਚਨਾ ਬੈਕਅੱਪ ਅਤੇ ਐਪ ਨੂੰ ਮੁੜ ਡਾਟਾ ਖਰਾਬ ਹੋਣ ਦੀ ਸੂਰਤ ਵਿਚ ਆਸਾਨ ਬਹਾਲੀ, ਜਿਸ ਨਾਲ ਸਿਸਟਮ ਦੀ ਸਭ ਸੰਰਚਨਾ ਸੈਟਿੰਗ ਦਾ ਸਨੈਪਸ਼ਾਟ ਲੈਣ ਲਈ ਇੱਕ ਪਰਬੰਧਕ ਲਈ ਸਹਾਇਕ ਹੈ,.';
+$lang['configuration_backup_app_name'] = 'ਸੰਰਚਨਾ ਬੈਕਅੱਪ ਅਤੇ ਮੁੜ';
+$lang['configuration_backup_archives'] = 'ਆਰਕਾਈਵ';
+$lang['configuration_backup_backup_file_invalid'] = 'ਬੈਕਅੱਪ ਫਾਇਲ ਗਲਤ ਹੈ.';
+$lang['configuration_backup_backup_now'] = 'ਬੈਕਅੱਪ ਹੁਣ';
+$lang['configuration_backup_checking_version'] = 'ਚੈੱਕ ਅਕਾਇਵ ਨੂੰ ਵਰਜਨ ਅਨੁਕੂਲਤਾ';
+$lang['configuration_backup_clear'] = 'ਆਸਮਾਨ';
+$lang['configuration_backup_confirm_restore'] = 'ਜੇਕਰ ਤੁਹਾਨੂੰ ਇਹ ਯਕੀਨੀ ਤੁਹਾਨੂੰ ਹੇਠ ਪੁਰਾਲੇਖ ਫਾਇਲ ਵਰਤ ਨੂੰ ਬਹਾਲ ਕਰਨ ਲਈ ਚਾਹੁੰਦੇ ਹੋ?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ਡਾਇਰੈਕਟਰੀ ਵਿੱਚ ਤਬਦੀਲ ਨਹੀ ਕੀਤਾ ਜਾ ਸਕਦਾ ਹੈ.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ਡਾਊਨਲੋਡ ਅਤੇ ਐਪਸ ਨੂੰ ਇੰਸਟਾਲ. ਕਿਰਪਾ ਕਰਕੇ ਮਰੀਜ਼ ਹੋ.';
+$lang['configuration_backup_file_information'] = 'ਫਾਇਲ ਜਾਣਕਾਰੀ';
+$lang['configuration_backup_importing_ldap'] = 'ਅਯਾਤ LDAP ਡਾਇਰੈਕਟਰੀ';
+$lang['configuration_backup_network_information'] = 'ਨੈੱਟਵਰਕ ਜਾਣਕਾਰੀ';
+$lang['configuration_backup_not_registered'] = 'ਤੁਹਾਡਾ ਸਿਸਟਮ ਸੰਰਚਨਾ ਸੈਟਿੰਗ ਨੂੰ ਮੁੜ ਅੱਗੇ ਦਰਜ ਕੀਤਾ ਜਾਣਾ ਚਾਹੀਦਾ ਹੈ.';
+$lang['configuration_backup_older_version_warn'] = 'ਪੁਰਾਣੇ ਵਰਜਨ ਪ੍ਰਗਟ ਇੰਸਟਾਲ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਸ਼ਾਮਲ ਨਾ ਕਰੋ - ਬਾਜ਼ਾਰ ਤੱਕ ਦੀ ਲੋੜ ਐਪਸ ਇੰਸਟਾਲ ਕਰੋ ਜੀ ਬਾਅਦ ਨੂੰ ਮੁੜ';
+$lang['configuration_backup_release_mismatch'] = 'ਬੈਕਅੱਪ ਫਾਇਲ ਵਿੱਚ ਵਰਜਨ ਜਾਣਕਾਰੀ ਵਰਜਨ ਇਸ ਸਿਸਟਮ ਤੇ ਇੰਸਟਾਲ ਨਹੀ ਹੈ.';
+$lang['configuration_backup_release_missing'] = 'ਵਰਜਨ ਜਾਣਕਾਰੀ ਬੈਕਅੱਪ ਫਾਇਲ ਵਿੱਚ ਉਪਲੱਬਧ ਨਹੀ ਹੈ.';
+$lang['configuration_backup_release_unsupported:'] = 'ਹੇਠ ਵਰਜਨ ਤੱਕ ਮੁੜ ਸਹਿਯੋਗ ਨਹੀ ਹੈ:';
+$lang['configuration_backup_restarting_service:'] = 'ਮੁੜ ਸੇਵਾ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'ਤਰੱਕੀ ਵਿੱਚ ਮੁੜ.';
+$lang['configuration_backup_restore_complete'] = 'ਮੁਕੰਮਲ ਹੋ ਮੁੜ.';
+$lang['configuration_backup_restore_file'] = 'ਅਕਾਈਵ ਫਾਇਲ ਨੂੰ';
+$lang['configuration_backup_restore_from_archive'] = 'ਅਕਾਇਵ ਮੁੜ';
+$lang['configuration_backup_restore_log'] = 'ਮੁੜ ਲਾਗ';
+$lang['configuration_backup_restore_type:'] = 'ਮੁੜ ਕਿਸਮ:';
+$lang['configuration_backup_starting_event_system'] = 'ਸ਼ੁਰੂ ਹੋ ਰਿਹਾ ਹੈ ਘਟਨਾ ਮਾਨੀਟਰ ਸਰਵਿਸ';
+$lang['configuration_backup_status_data_not_available'] = 'ਸਥਿਤੀ ਡਾਟਾ ਉਪਲੱਬਧ ਨਹੀ.';
+$lang['configuration_backup_stopping_event_system'] = 'ਬੰਦ ਘਟਨਾ ਮਾਨੀਟਰ ਸਰਵਿਸ';
+$lang['configuration_backup_unable_to_start_restore'] = 'ਕਾਰਵਾਈ ਨੂੰ ਮੁੜ ਸ਼ੁਰੂ ਕਰਨ ਲਈ ਅਸਫ਼ਲ ਹੈ';
+$lang['configuration_backup_unpacking_archive'] = 'ਅਨਪੈਕਿੰਗ ਪੁਰਾਲੇਖ ਫਾਇਲ';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ ਸਿਸਟਮ ਲਈ ਉਡੀਕ';
+$lang['configuration_backup_yum_busy'] = 'ਪੈਕੇਜ ਮੈਨੇਜਰ ਰੁੱਝਿਆ ਹੋਇਆ ਹੈ.';
+
