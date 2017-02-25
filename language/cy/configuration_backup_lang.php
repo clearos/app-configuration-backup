100b2af1aeb625c874fc486a80fead026f4569fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:52:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Mae&#39;r backup cyfluniad ac adfer app yn caniatáu i weinyddwr gymryd cipolwg o&#39;r holl leoliadau cyfluniad y system, gan ganiatáu adfer hawdd mewn achos o golli data.';
+$lang['configuration_backup_app_name'] = 'Backup a Adfer Configuration';
+$lang['configuration_backup_archives'] = 'Archifau';
+$lang['configuration_backup_backup_file_invalid'] = 'filename Backup yn annilys.';
+$lang['configuration_backup_backup_now'] = 'Backup Nawr';
+$lang['configuration_backup_checking_version'] = 'Gwirio fersiwn archif cydnawsedd';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'A ydych yn sicr eich bod am adfer defnyddio&#39;r ffeil archif canlynol?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Ni allai cyfeiriadur eu trosi.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Lawrlwytho a gosod apps. Byddwch yn amyneddgar.';
+$lang['configuration_backup_file_information'] = 'Gwybodaeth ffeil';
+$lang['configuration_backup_importing_ldap'] = 'Mewnforio cyfeiriadur LDAP';
+$lang['configuration_backup_network_information'] = 'Gwybodaeth rhwydwaith';
+$lang['configuration_backup_not_registered'] = 'Rhaid i&#39;ch system yn cael ei gofrestru cyn adfer gosodiadau cyfluniad.';
+$lang['configuration_backup_older_version_warn'] = 'Nid yw fersiynau hŷn yn cynnwys app osod amlwg - os gwelwch yn dda gorsedda apps ei angen gan y Marketplace ar ôl adfer';
+$lang['configuration_backup_release_mismatch'] = 'Nid yw&#39;r wybodaeth fersiwn yn y ffeil wrth gefn yn cyfateb â fersiwn gosod ar y system hon.';
+$lang['configuration_backup_release_missing'] = 'Nid yw&#39;r wybodaeth fersiwn ar gael yn y ffeil wrth gefn.';
+$lang['configuration_backup_release_unsupported:'] = 'Nid Adfer o fersiwn ganlynol yn cael ei gefnogi:';
+$lang['configuration_backup_restarting_service:'] = 'gwasanaeth ailgychwyn:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Adfer y gweill.';
+$lang['configuration_backup_restore_complete'] = 'Adfer gyflawn.';
+$lang['configuration_backup_restore_file'] = 'Ffeil archif';
+$lang['configuration_backup_restore_from_archive'] = 'Adfer O&#39;r Archif';
+$lang['configuration_backup_restore_log'] = 'adfer Log';
+$lang['configuration_backup_restore_type:'] = 'Adfer Math:';
+$lang['configuration_backup_starting_event_system'] = 'Dechrau gwasanaeth monitro digwyddiad';
+$lang['configuration_backup_status_data_not_available'] = 'data Statws ddim ar gael.';
+$lang['configuration_backup_stopping_event_system'] = 'Mae rhoi&#39;r gorau i wasanaeth monitro digwyddiad';
+$lang['configuration_backup_unable_to_start_restore'] = 'Methu cychwyn adfer gweithrediad';
+$lang['configuration_backup_unpacking_archive'] = 'ffeil archif Dadbacio';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Aros am system diweddariadau meddalwedd';
+$lang['configuration_backup_yum_busy'] = 'rheolwr pecyn yn brysur.';
+
