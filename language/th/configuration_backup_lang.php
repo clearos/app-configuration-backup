7385c4a7eb66f6caabad5e0b3f7777810af3e421
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:50:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'การสำรองข้อมูลและเรียกคืนการตั้งค่า app ที่ช่วยให้ผู้ดูแลระบบเพื่อถ่ายภาพของการตั้งค่าทั้งหมดของระบบที่ช่วยให้การฟื้นฟูง่ายในกรณีที่มีการสูญเสียข้อมูล';
+$lang['configuration_backup_app_name'] = 'การสำรองข้อมูลและกู้คืนการตั้งค่าคอนฟิก';
+$lang['configuration_backup_archives'] = 'หอจดหมายเหตุ';
+$lang['configuration_backup_backup_file_invalid'] = 'ชื่อไฟล์สำรองข้อมูลไม่ถูกต้อง';
+$lang['configuration_backup_backup_now'] = 'การสำรองข้อมูลในขณะนี้';
+$lang['configuration_backup_checking_version'] = 'ตรวจสอบรุ่นเก็บความเข้ากันได้';
+$lang['configuration_backup_clear'] = 'ชัดเจน';
+$lang['configuration_backup_confirm_restore'] = 'คุณแน่ใจหรือว่าต้องการคืนค่าใช้เก็บไฟล์ดังต่อไปนี้?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ไดเรกทอรีไม่สามารถแปลง';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ดาวน์โหลดและติดตั้งแอพพลิเค กรุณารอสักครู่';
+$lang['configuration_backup_file_information'] = 'ข้อมูลแฟ้ม';
+$lang['configuration_backup_importing_ldap'] = 'ไดเร็กทอรี LDAP การนำเข้า';
+$lang['configuration_backup_network_information'] = 'เครือข่ายสารสนเทศ';
+$lang['configuration_backup_not_registered'] = 'ระบบของคุณจะต้องลงทะเบียนก่อนที่จะคืนการตั้งค่า';
+$lang['configuration_backup_older_version_warn'] = 'รุ่นเก่าไม่รวมถึงการติดตั้งแอปประจักษ์ - โปรดติดตั้งแอปที่ต้องการจากตลาดหลังจากเรียกคืน';
+$lang['configuration_backup_release_mismatch'] = 'ข้อมูลเกี่ยวกับรุ่นในแฟ้มสำรองข้อมูลไม่ตรงกับรุ่นที่ติดตั้งในระบบนี้';
+$lang['configuration_backup_release_missing'] = 'ข้อมูลรุ่นไม่สามารถใช้ได้ในแฟ้มสำรอง';
+$lang['configuration_backup_release_unsupported:'] = 'เรียกคืนจากรุ่นต่อไปนี้จะไม่ได้รับการสนับสนุน:';
+$lang['configuration_backup_restarting_service:'] = 'เริ่มบริการ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restore ในความคืบหน้า';
+$lang['configuration_backup_restore_complete'] = 'Restore สมบูรณ์';
+$lang['configuration_backup_restore_file'] = 'แฟ้มเก็บ';
+$lang['configuration_backup_restore_from_archive'] = 'เรียกคืนจากการเก็บถาวร';
+$lang['configuration_backup_restore_log'] = 'เรียกคืนเข้าสู่ระบบ';
+$lang['configuration_backup_restore_type:'] = 'Restore ประเภทนี้:';
+$lang['configuration_backup_starting_event_system'] = 'เริ่มต้นให้บริการการตรวจสอบเหตุการณ์';
+$lang['configuration_backup_status_data_not_available'] = 'ข้อมูลสถานะไม่สามารถใช้ได้';
+$lang['configuration_backup_stopping_event_system'] = 'หยุดให้บริการการตรวจสอบเหตุการณ์';
+$lang['configuration_backup_unable_to_start_restore'] = 'ไม่สามารถเริ่มดำเนินการคืนค่า';
+$lang['configuration_backup_unpacking_archive'] = 'แฟ้มเก็บเอาออก';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'รอให้ระบบการปรับปรุงซอฟต์แวร์';
+$lang['configuration_backup_yum_busy'] = 'ผู้จัดการแพคเกจไม่ว่าง';
+
