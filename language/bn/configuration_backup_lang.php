726782c465241673cc4c11a503f0eaa928bfaf1f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:39:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'কনফিগারেশন ব্যাকআপ এবং অ্যাপ্লিকেশন পুনরুদ্ধার তথ্য হারানোর ঘটনা সহজ পুন অনুমতি সিস্টেমের সমস্ত কনফিগারেশন সেটিংস এর একটি স্ন্যাপশট নিতে একজন প্রশাসক দেয়.';
+$lang['configuration_backup_app_name'] = 'কনফিগারেশন ব্যাকআপ এবং পুনঃস্থাপন করুন';
+$lang['configuration_backup_archives'] = 'নথিপত্র';
+$lang['configuration_backup_backup_file_invalid'] = 'ব্যাকআপ ফাইলের নাম অবৈধ.';
+$lang['configuration_backup_backup_now'] = 'এখনি ব্যাকআপ করে নিন';
+$lang['configuration_backup_checking_version'] = 'চেক করা সংরক্ষণাগার সংস্করণ কম্প্যাটিবিলিটি';
+$lang['configuration_backup_clear'] = 'পরিষ্কার';
+$lang['configuration_backup_confirm_restore'] = 'আপনি কি নিশ্চিত যে আপনি নিম্নলিখিত সংরক্ষণাগার ফাইল ব্যবহার করে পুনরুদ্ধার করতে চান?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ডিরেক্টরি পরিবর্তন করা যায়নি.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ডাউনলোড এবং অ্যাপস ইনস্টল. দয়া করে ধৈর্য ধরুন.';
+$lang['configuration_backup_file_information'] = 'ফাইল তথ্য';
+$lang['configuration_backup_importing_ldap'] = 'আমদানি করা LDAP ডিরেক্টরি';
+$lang['configuration_backup_network_information'] = 'নেটওয়ার্ক ইনফরমেশন';
+$lang['configuration_backup_not_registered'] = 'আপনার সিস্টেম কনফিগারেশন সেটিংস পুনরূদ্ধার আগে অবশ্যই নিবন্ধিত হতে হবে.';
+$lang['configuration_backup_older_version_warn'] = 'পুরোনো সংস্করণগুলি উদ্ভিন্ন ইনস্টল অ্যাপ্লিকেশন অন্তর্ভুক্ত করবেন না - মার্কেটপ্লেস থেকে প্রয়োজনীয় অ্যাপ ইন্সটল দয়া পরে পুনরুদ্ধার';
+$lang['configuration_backup_release_mismatch'] = 'ব্যাকআপ ফাইলে সংস্করণ তথ্য সংস্করণ এই সিস্টেমে ইনস্টল সাথে মিলছে না.';
+$lang['configuration_backup_release_missing'] = 'সংস্করণ তথ্য ব্যাকআপ ফাইল পাওয়া যায় না.';
+$lang['configuration_backup_release_unsupported:'] = 'নিম্নলিখিত সংস্করণ থেকে পুনরুদ্ধার করা সমর্থন করে না:';
+$lang['configuration_backup_restarting_service:'] = 'পুনরায় চালু সেবা:';
+$lang['configuration_backup_restore_already_in_progress'] = 'প্রগতিতে পুনঃস্থাপন করুন.';
+$lang['configuration_backup_restore_complete'] = 'সম্পূর্ণ পুনরুদ্ধার.';
+$lang['configuration_backup_restore_file'] = 'আর্কাইভ ফাইল';
+$lang['configuration_backup_restore_from_archive'] = 'আর্কাইভ থেকে পুনরুদ্ধার';
+$lang['configuration_backup_restore_log'] = 'পুনরুদ্ধার লগ';
+$lang['configuration_backup_restore_type:'] = 'Restore টাইপ:';
+$lang['configuration_backup_starting_event_system'] = 'শুরু হচ্ছে ঘটনা মনিটর সেবা';
+$lang['configuration_backup_status_data_not_available'] = 'স্ট্যাটাস তথ্য পাওয়া যায় না.';
+$lang['configuration_backup_stopping_event_system'] = 'বাঁধন ঘটনা মনিটর সেবা';
+$lang['configuration_backup_unable_to_start_restore'] = 'অপারেশন পুনরুদ্ধার শুরু করতে অক্ষম';
+$lang['configuration_backup_unpacking_archive'] = 'আন-প্যাক সংরক্ষণাগার ফাইল';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'সফটওয়্যার আপডেট সিস্টেমের জন্য অপেক্ষা করা হচ্ছে';
+$lang['configuration_backup_yum_busy'] = 'প্যাকেজ ম্যানেজার ব্যস্ত.';
+
