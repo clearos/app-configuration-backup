b6294846b6bef2461ab6742b19662873c60b45f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:48:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'වින්යාස උපස්ථ සහ යෙදුම නැවත දත්ත අහිමි කිරීමේ සිද්ධිය සම්බන්ධයෙන් පහසු යළි ඉඩ, පද්ධතිය සියලු වින්යාසයන් තිරයේ චිත්රයක් මවයි ගැනීමට පරිපාලක හැක.';
+$lang['configuration_backup_app_name'] = 'වින්යාස බැකප් යථා තත්වයට පත්';
+$lang['configuration_backup_archives'] = 'ලේඛනාරක්ෂක';
+$lang['configuration_backup_backup_file_invalid'] = 'උපස්ථ ගොනු වලංගු නැත.';
+$lang['configuration_backup_backup_now'] = 'දැන් බැකප්';
+$lang['configuration_backup_checking_version'] = 'සංරක්ෂිත අනුවාදය ගැළපුම පරීක්ෂා';
+$lang['configuration_backup_clear'] = 'පැහැදිලිව';
+$lang['configuration_backup_confirm_restore'] = 'ඔබ පහත සඳහන් සංරක්ෂිත ගොනුව භාවිතා යථාවත් කිරීමට අවශ්ය බව ඔබට විශ්වාසද?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'නාමාවලිය පරිවර්තනය කළ නොහැකි විය.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'බාගත සහ යෙදුම් ස්ථාපනය. කරුණාකර ඉවසන්න.';
+$lang['configuration_backup_file_information'] = 'ගොනු තොරතුරු';
+$lang['configuration_backup_importing_ldap'] = 'LDAP බහලුම ආනයනය';
+$lang['configuration_backup_network_information'] = 'ජාල තොරතුරු';
+$lang['configuration_backup_not_registered'] = 'ඔබේ පද්ධතිය වින්යාසයන් තිරයේ නැවත ප්රතිසංස්කරණය පෙර ලියාපදිංචි කළ යුතු ය.';
+$lang['configuration_backup_older_version_warn'] = 'පැරණි අනුවාද යෙදුම ප්රකාශ ස්ථාපනය ඇතුළත් නොවන අතර පසු - වෙළඳපොළ සිට අවශ්ය යෙදුම් ස්ථාපනය කරන්න නැවත';
+$lang['configuration_backup_release_mismatch'] = 'බැකප් ගොනුව තුල අනුවාදය තොරතුරු පද්ධතිය තුල ස්ථාපනය කර ඇති අනුවාද නොගැලපේ.';
+$lang['configuration_backup_release_missing'] = 'වෙළුම තොරතුරු බැකප් ගොනුව දී ලබාගත නොහැක.';
+$lang['configuration_backup_release_unsupported:'] = 'පහත සඳහන් අනුවාදය සිට යළි තහවුරු සහාය නොදක්වයි:';
+$lang['configuration_backup_restarting_service:'] = 'නැවත පණ ගැන්වීමකින් සේවා:';
+$lang['configuration_backup_restore_already_in_progress'] = 'ප්රගතිය, පිළිනැගුම.';
+$lang['configuration_backup_restore_complete'] = 'සම්පූර්ණ පිළිනැගුම.';
+$lang['configuration_backup_restore_file'] = 'සංරක්ෂිත ලේඛණ සේප්පුව ගොනුව';
+$lang['configuration_backup_restore_from_archive'] = 'සංරක්ෂිත ලේඛණ සේප්පුව සිට යථා තත්වයට පත් කිරීම';
+$lang['configuration_backup_restore_log'] = 'ඇතුළුවන්න පිළිනැගුම';
+$lang['configuration_backup_restore_type:'] = 'වර්ගය, පිළිනැගුම:';
+$lang['configuration_backup_starting_event_system'] = 'උත්සවය අධීක්ෂණය සේවාව ආරම්භ';
+$lang['configuration_backup_status_data_not_available'] = 'තත්ත්වය දත්ත ලබා ගත නොවේ.';
+$lang['configuration_backup_stopping_event_system'] = 'උත්සවය අධීක්ෂණය සේවා නතර කිරීම';
+$lang['configuration_backup_unable_to_start_restore'] = 'මෙහෙයුම් නැවත පිහිටුවීම ආරම්භ කිරීමට නොහැකි විය';
+$lang['configuration_backup_unpacking_archive'] = 'සංරක්ෂිත ගොනුව විහිදු';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'මෘදුකාංග යාවත්කාලීන පද්ධතිය සඳහා බලා';
+$lang['configuration_backup_yum_busy'] = 'ඇසුරුම් කළමනාකරු කාර්යබහුලයි.';
+
