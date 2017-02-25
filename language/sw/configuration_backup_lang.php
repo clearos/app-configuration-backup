e72b0e30d3bd86c30fad5235145e4969e2698f5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:49:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Backup Configuration na kurejesha programu inaruhusu msimamizi wa kuchukua snapshot ya mazingira yote Configuration ya mfumo, kuruhusu marejesho rahisi katika tukio la hasara data.';
+$lang['configuration_backup_app_name'] = 'Configuration Backup na Rejesha';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup filename ni batili.';
+$lang['configuration_backup_backup_now'] = 'Backup Sasa';
+$lang['configuration_backup_checking_version'] = 'Kuangalia archive version utangamano';
+$lang['configuration_backup_clear'] = 'wazi';
+$lang['configuration_backup_confirm_restore'] = 'Je, una uhakika unataka kurejesha kutumia zifuatazo archive file?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Saraka inaweza kuwa waongofu.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Kushusha na kufunga programu. Tafadhali kuwa na subira.';
+$lang['configuration_backup_file_information'] = 'faili Habari';
+$lang['configuration_backup_importing_ldap'] = 'Kuagiza LDAP directory';
+$lang['configuration_backup_network_information'] = 'mtandao wa Habari';
+$lang['configuration_backup_not_registered'] = 'Mfumo wako lazima kusajiliwa kabla kurejesha mipangilio ya usanidi.';
+$lang['configuration_backup_older_version_warn'] = 'matoleo ya zamani si ni pamoja na programu kufunga wazi - tafadhali kufunga programu required kutoka Marketplace baada ya kurejesha';
+$lang['configuration_backup_release_mismatch'] = 'Maelezo toleo katika faili la akiba hailingani na toleo imewekwa juu ya mfumo huu.';
+$lang['configuration_backup_release_missing'] = 'Maelezo version haipatikani katika faili la akiba.';
+$lang['configuration_backup_release_unsupported:'] = 'Kurejesha kutoka toleo zifuatazo si mkono:';
+$lang['configuration_backup_restarting_service:'] = 'Kuanzisha tena huduma:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Kurejesha katika maendeleo.';
+$lang['configuration_backup_restore_complete'] = 'Kurejesha kamili.';
+$lang['configuration_backup_restore_file'] = 'File archive';
+$lang['configuration_backup_restore_from_archive'] = 'Kurejesha From Archive';
+$lang['configuration_backup_restore_log'] = 'kurejesha Log';
+$lang['configuration_backup_restore_type:'] = 'Kurejesha aina:';
+$lang['configuration_backup_starting_event_system'] = 'Kuanzia tukio huduma kufuatilia';
+$lang['configuration_backup_status_data_not_available'] = 'Hali data haipatikani.';
+$lang['configuration_backup_stopping_event_system'] = 'Kuacha tukio huduma kufuatilia';
+$lang['configuration_backup_unable_to_start_restore'] = 'Hawezi kuanza kurejesha operesheni';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking archive file';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Kusubiri kwa ajili ya programu updates mfumo';
+$lang['configuration_backup_yum_busy'] = 'Mfuko meneja ni busy.';
+
