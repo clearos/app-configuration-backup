164dabae61f65c746a06ddc6ff52d7b93cb468fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:47:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfigurasjonen sikkerhetskopiering og gjenoppretting app lar en administrator for å ta et bilde av alle konfigurasjonsinnstillingene for systemet, noe som gir enkel restaurering i tilfelle av tap av data.';
+$lang['configuration_backup_app_name'] = 'Konfigurasjon Sikkerhetskopiering og gjenoppretting';
+$lang['configuration_backup_archives'] = 'Arkiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup filnavnet er ugyldig.';
+$lang['configuration_backup_backup_now'] = 'backup nå';
+$lang['configuration_backup_checking_version'] = 'Kontrollere arkiv versjon kompatibilitets';
+$lang['configuration_backup_clear'] = 'Klar';
+$lang['configuration_backup_confirm_restore'] = 'Er du sikker på at du vil gjenopprette ved hjelp av følgende arkivfil?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Katalogen ble ikke konvertert.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Laste ned og installere apps. Vær tålmodig.';
+$lang['configuration_backup_file_information'] = 'Filinformasjon';
+$lang['configuration_backup_importing_ldap'] = 'Importere LDAP-katalog';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Systemet må være registrert før du gjenoppretter konfigurasjonsinnstillinger.';
+$lang['configuration_backup_older_version_warn'] = 'Eldre versjoner inkluderer ikke appinstallasjoner manifest - vennligst installere nødvendige programmer fra Market etter gjenoppretting';
+$lang['configuration_backup_release_mismatch'] = 'Versjonsinformasjonen i backup-filen ikke samsvarer med den versjonen som er installert på dette systemet.';
+$lang['configuration_backup_release_missing'] = 'Versjonen informasjon er ikke tilgjengelig i sikkerhetskopifilen.';
+$lang['configuration_backup_release_unsupported:'] = 'Gjenopprette fra følgende versjon støttes ikke:';
+$lang['configuration_backup_restarting_service:'] = 'Omstart tjeneste:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Gjenopprett pågår.';
+$lang['configuration_backup_restore_complete'] = 'Gjenopprett komplett.';
+$lang['configuration_backup_restore_file'] = 'Arkiv File';
+$lang['configuration_backup_restore_from_archive'] = 'Gjenopprette Fra Arkiv';
+$lang['configuration_backup_restore_log'] = 'Gjenopprett Log';
+$lang['configuration_backup_restore_type:'] = 'Gjenopprett Type:';
+$lang['configuration_backup_starting_event_system'] = 'Starter hendelse monitor tjeneste';
+$lang['configuration_backup_status_data_not_available'] = 'Status data ikke tilgjengelig.';
+$lang['configuration_backup_stopping_event_system'] = 'Stoppe event monitor tjeneste';
+$lang['configuration_backup_unable_to_start_restore'] = 'Kan ikke starte gjenopprett';
+$lang['configuration_backup_unpacking_archive'] = 'Utpakking arkivfil';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Venter på programvareoppdateringer system';
+$lang['configuration_backup_yum_busy'] = 'Pakkebehandleren er opptatt.';
+
