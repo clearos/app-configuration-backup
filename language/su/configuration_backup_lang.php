17a9a6b79373e309cf475722da8b42c285b4441a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:49:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Nu cadangan konfigurasi tur malikkeun app ngamungkinkeun kuncén pikeun nyokot snapshot tina sadaya setelan konfigurasi tina sistem, sahingga restorasi gampang dina acara leungitna data.';
+$lang['configuration_backup_app_name'] = 'Konfigurasi Nyadangkeun jeung Mulangkeun';
+$lang['configuration_backup_archives'] = 'simpenan sesuratan penting';
+$lang['configuration_backup_backup_file_invalid'] = 'Nyadangkeun Ngaran koropak geus sah.';
+$lang['configuration_backup_backup_now'] = 'Nyadangkeun Ayeuna';
+$lang['configuration_backup_checking_version'] = 'Mariksa kasaluyuan versi arsip';
+$lang['configuration_backup_clear'] = 'atra';
+$lang['configuration_backup_confirm_restore'] = 'Dupi anjeun yakin rék mulangkeun ngagunakeun file arsip di handap?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Diréktori teu bisa dirobah.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Ngundeur jeung masang aktip. Mangga jadi sabar.';
+$lang['configuration_backup_file_information'] = 'Sejenna file';
+$lang['configuration_backup_importing_ldap'] = 'Importing diréktori LDAP';
+$lang['configuration_backup_network_information'] = 'Sejenna Network';
+$lang['configuration_backup_not_registered'] = 'sistim Anjeun kudu kadaptar saméméh malikkeun setélan konfigurasi.';
+$lang['configuration_backup_older_version_warn'] = 'vérsi heubeul teu kaasup aplikasi masang manifest - mangga masang aktip diperlukeun ti pasaran sanggeus mulangkeun';
+$lang['configuration_backup_release_mismatch'] = 'Informasi versi dina payel cadangan teu cocog versi dipasang dina sistim ieu.';
+$lang['configuration_backup_release_missing'] = 'Informasi Vérsi teu sadia di payel cadangan.';
+$lang['configuration_backup_release_unsupported:'] = 'Malikkeun ti versi handap ieu teu dirojong:';
+$lang['configuration_backup_restarting_service:'] = 'Dibalikan deui service:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Mulangkeun dina kamajuan.';
+$lang['configuration_backup_restore_complete'] = 'Mulangkeun nu lengkep.';
+$lang['configuration_backup_restore_file'] = 'arsip File';
+$lang['configuration_backup_restore_from_archive'] = 'Mulangkeun Ti Archive';
+$lang['configuration_backup_restore_log'] = 'mulangkeun Log';
+$lang['configuration_backup_restore_type:'] = 'Mulangkeun tipe:';
+$lang['configuration_backup_starting_event_system'] = 'Dimimitian service monitor kajadian';
+$lang['configuration_backup_status_data_not_available'] = 'data Status teu sadia.';
+$lang['configuration_backup_stopping_event_system'] = 'Stopping service monitor kajadian';
+$lang['configuration_backup_unable_to_start_restore'] = 'Bisa ngamimitian mulangkeun operasi';
+$lang['configuration_backup_unpacking_archive'] = 'file arsip Unpacking';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Nungguan sistim apdet software';
+$lang['configuration_backup_yum_busy'] = 'Paket manager anu riweuh.';
+
