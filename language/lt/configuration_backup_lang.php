848f213d9f698bcc921f32d44bdb0a0f3c093521
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:45:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfigūracijos atsarginę kopiją ir atkurti programą leidžia administratoriui imtis visų konfigūracijos nustatymus sistemos fotografiją, leidžia lengvai atkurti į duomenų praradimo atveju.';
+$lang['configuration_backup_app_name'] = 'Konfigūracijos atsarginę kopiją ir atkurti';
+$lang['configuration_backup_archives'] = 'Archyvas';
+$lang['configuration_backup_backup_file_invalid'] = 'Atsarginė failo yra neteisinga.';
+$lang['configuration_backup_backup_now'] = 'Atsarginė Dabar';
+$lang['configuration_backup_checking_version'] = 'Tikrinimas archyvas versiją suderinamumo';
+$lang['configuration_backup_clear'] = 'aiškus';
+$lang['configuration_backup_confirm_restore'] = 'Ar tikrai norite atstatyti naudojant šią archyvo failą?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Katalogas negalėjo būti pakeista.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Atsisiųsdami ir įdiegdami programas. Būkite kantrūs.';
+$lang['configuration_backup_file_information'] = 'failo informacija';
+$lang['configuration_backup_importing_ldap'] = 'Importuoti LDAP katalogo';
+$lang['configuration_backup_network_information'] = 'tinklo informacijos';
+$lang['configuration_backup_not_registered'] = 'Jūsų sistema turi būti įregistruota iki atkurti konfigūracijos nustatymus.';
+$lang['configuration_backup_older_version_warn'] = 'Senesnės versijos neapima programą įdiegti manifestas - įdiekite reikalingas programas iš Marketplace po atkurti';
+$lang['configuration_backup_release_mismatch'] = 'Versija informacija atsarginės kopijos neatitinka versija įdiegta ši sistema.';
+$lang['configuration_backup_release_missing'] = 'Versija informacija nėra prieinama failo atsarginę kopiją.';
+$lang['configuration_backup_release_unsupported:'] = 'Atkūrimas iš šios versijos nepalaiko:';
+$lang['configuration_backup_restarting_service:'] = 'Atnaujinus paslaugas:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Atkurti vyksta.';
+$lang['configuration_backup_restore_complete'] = 'Atkurti baigtas.';
+$lang['configuration_backup_restore_file'] = 'Archyvas Failo';
+$lang['configuration_backup_restore_from_archive'] = 'Atkurti iš archyvo';
+$lang['configuration_backup_restore_log'] = 'atkurti Prisijungti';
+$lang['configuration_backup_restore_type:'] = 'Atkurti tipas:';
+$lang['configuration_backup_starting_event_system'] = 'Nuo įvykis monitorius tarnyba';
+$lang['configuration_backup_status_data_not_available'] = 'Statusas nėra duomenų.';
+$lang['configuration_backup_stopping_event_system'] = 'Stabdymo įvykis monitorius tarnyba';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nepavyko pradėti atstatymo operaciją';
+$lang['configuration_backup_unpacking_archive'] = 'Išpakavimas failo archyvas';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Laukiama programinės įrangos atnaujinimus sistemos';
+$lang['configuration_backup_yum_busy'] = 'Package Manager yra užimtas.';
+
