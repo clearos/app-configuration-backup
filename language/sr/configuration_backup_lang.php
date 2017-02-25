cb780cc19b086b398ece9f3ea406e06f96fb1882
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:48:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'бацкуп конфигурација и враћање апликација омогућава администратору да преузму снимак свих подешавања конфигурације система, што омогућава лако враћање у случају губитка података.';
+$lang['configuration_backup_app_name'] = 'Конфигурација Бацкуп и Ресторе';
+$lang['configuration_backup_archives'] = 'архива';
+$lang['configuration_backup_backup_file_invalid'] = 'Бацкуп датотеке је неважећи.';
+$lang['configuration_backup_backup_now'] = 'бацкуп Сада';
+$lang['configuration_backup_checking_version'] = 'Провера архиву версион компатибилност';
+$lang['configuration_backup_clear'] = 'Јасно';
+$lang['configuration_backup_confirm_restore'] = 'Да ли сте сигурни да желите да вратите помоћу следеће архивску датотеку?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Директоријум не може бити конвертован.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Преузимање и инсталирање апликације. Будите стрпљиви.';
+$lang['configuration_backup_file_information'] = 'informacije о датотеци';
+$lang['configuration_backup_importing_ldap'] = 'Увоз ЛДАП директоријум';
+$lang['configuration_backup_network_information'] = 'mreža informacije';
+$lang['configuration_backup_not_registered'] = 'Ваш систем мора бити регистрован прије враћања подешавања конфигурације.';
+$lang['configuration_backup_older_version_warn'] = 'Старије верзије не укључују инсталирање апликације манифестује - инсталирајте потребне апликације са тржишта након враћања';
+$lang['configuration_backup_release_mismatch'] = 'Информације верзија у резервну копију датотеке не одговара верзију инсталиран на овом систему.';
+$lang['configuration_backup_release_missing'] = 'Информације верзија није доступна у датотеци резервне копије.';
+$lang['configuration_backup_release_unsupported:'] = 'Враћање из следећег верзије није подржан:';
+$lang['configuration_backup_restarting_service:'] = 'Поновно покретање услуге:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Враћање у току.';
+$lang['configuration_backup_restore_complete'] = 'Враћање потпуна.';
+$lang['configuration_backup_restore_file'] = 'Архива фајла';
+$lang['configuration_backup_restore_from_archive'] = 'Ресторе Из архиве';
+$lang['configuration_backup_restore_log'] = 'Ресторе Приступи';
+$lang['configuration_backup_restore_type:'] = 'Ресторе тип:';
+$lang['configuration_backup_starting_event_system'] = 'Покретање догађај монитора сервис';
+$lang['configuration_backup_status_data_not_available'] = 'Статус подаци нису доступни.';
+$lang['configuration_backup_stopping_event_system'] = 'Заустављање догађај монитора сервис';
+$lang['configuration_backup_unable_to_start_restore'] = 'Није могуће покренути операцију опоравка';
+$lang['configuration_backup_unpacking_archive'] = 'Распакивање датотека архиве';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Ваитинг фор Софтваре Упдатес систем';
+$lang['configuration_backup_yum_busy'] = 'менаџер пакета је заузет.';
+
