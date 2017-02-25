e1dba0105156f822a2567021f6f30f65cc18c00f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:51:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'ترتیب کے بیک اپ اور بحال اے پی پی ڈیٹا کے نقصان کی صورت میں آسان بحالی کی اجازت دی، نظام کی تمام ترتیب کی ترتیبات کی ایک سنیپ شاٹ لینے کے لئے ایک منتظم کی اجازت دیتا ہے.';
+$lang['configuration_backup_app_name'] = 'ترتیب بیک اپ اور بحال';
+$lang['configuration_backup_archives'] = 'آرکائیو';
+$lang['configuration_backup_backup_file_invalid'] = 'بیک اپ فائل کا نام غلط ہے.';
+$lang['configuration_backup_backup_now'] = 'بیک اپ اب';
+$lang['configuration_backup_checking_version'] = 'محفوظ شدہ دستاویزات ورژن مطابقت کی جانچ ہو رہی';
+$lang['configuration_backup_clear'] = 'صاف';
+$lang['configuration_backup_confirm_restore'] = 'کیا آپ واقعی مندرجہ ذیل آرکائیو فائل کا استعمال کرتے ہوئے بحال کرنا چاہتے ہیں؟';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ڈائریکٹری تبدیل نہیں کیا جا سکتا تھا.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ڈاؤن لوڈ کرنا اور اطلاقات کو نصب. صبر کریں.';
+$lang['configuration_backup_file_information'] = 'فائل کی معلومات';
+$lang['configuration_backup_importing_ldap'] = 'کی درآمد LDAP ڈائرکٹری';
+$lang['configuration_backup_network_information'] = 'نیٹ ورک کی معلومات';
+$lang['configuration_backup_not_registered'] = 'آپ کے سسٹم کی ترتیب کی ترتیبات کو بحال کرنے سے پہلے رجسٹر کرنا ہوگا.';
+$lang['configuration_backup_older_version_warn'] = 'پرانے ورژن اے پی پی صریح انسٹال شامل نہیں ہے - مارکیٹ سے مطلوبہ ایپس انسٹال کریں براہ مہربانی بحال کیا';
+$lang['configuration_backup_release_mismatch'] = 'بیک اپ فائل میں ورژن کی معلومات ورژن اس نظام پر نصب میل نہیں کھاتا.';
+$lang['configuration_backup_release_missing'] = 'ورژن کی معلومات بیک اپ فائل میں دستیاب نہیں ہے.';
+$lang['configuration_backup_release_unsupported:'] = 'مندرجہ ذیل ورژن سے بحال کیا جا کی سہولت نہیں ہے:';
+$lang['configuration_backup_restarting_service:'] = 'دوبارہ شروع سروس:';
+$lang['configuration_backup_restore_already_in_progress'] = 'پیش رفت میں بحال کریں.';
+$lang['configuration_backup_restore_complete'] = 'مکمل بحال.';
+$lang['configuration_backup_restore_file'] = 'آرکائیو فائل';
+$lang['configuration_backup_restore_from_archive'] = 'محفوظ شدہ دستاویزات سے بحال';
+$lang['configuration_backup_restore_log'] = 'بحال دلے';
+$lang['configuration_backup_restore_type:'] = 'بحال کی قسم:';
+$lang['configuration_backup_starting_event_system'] = 'شروع واقعہ مانیٹر سروس';
+$lang['configuration_backup_status_data_not_available'] = 'دستیاب نہیں رتبہ ڈیٹا.';
+$lang['configuration_backup_stopping_event_system'] = 'روکنا واقعہ مانیٹر سروس';
+$lang['configuration_backup_unable_to_start_restore'] = 'آپریشن بحال شروع کرنے سے قاصر';
+$lang['configuration_backup_unpacking_archive'] = 'پیک کھولنا آرکائیو فائل';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'سافٹ ویئر اپ ڈیٹ کے نظام کے لئے انتظار کر رہا ہے';
+$lang['configuration_backup_yum_busy'] = 'پیکج مینیجر مصروف ہے.';
+
