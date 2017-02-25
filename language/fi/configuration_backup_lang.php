ef7885d3084c80b8ac86023b1c33315d99985ed4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:41:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfiguraatio varmuuskopioida ja palauttaa app avulla pääkäyttäjä voi ottaa tilannekuvan konfigurointi järjestelmän, joka mahdollistaa helpon palauttamisen, jos tietojen menetyksen.';
+$lang['configuration_backup_app_name'] = 'Kokoonpano varmuuskopiointi ja palautus';
+$lang['configuration_backup_archives'] = 'arkisto';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup tiedostonimi ei kelpaa.';
+$lang['configuration_backup_backup_now'] = 'Backup Now';
+$lang['configuration_backup_checking_version'] = 'Tarkistetaan arkiston versio yhteensopivuus';
+$lang['configuration_backup_clear'] = 'Asia selvä';
+$lang['configuration_backup_confirm_restore'] = 'Oletko varma, että haluat palauttaa seuraavan arkistotiedosto?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Hakemisto ei voitu muuntaa.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Lataaminen ja sovellusten asentamisesta. Olkaa kärsivällisiä.';
+$lang['configuration_backup_file_information'] = 'Tiedoston tiedot';
+$lang['configuration_backup_importing_ldap'] = 'Tuominen LDAP-hakemistoon';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Järjestelmä on rekisteröitävä ennen palauttamista asetuksia.';
+$lang['configuration_backup_older_version_warn'] = 'Vanhemmat versiot eivät sisällä sovellusasennus ilmeinen - Asenna tarvittavat sovellukset markkinapaikalta jälkeen palauttaa';
+$lang['configuration_backup_release_mismatch'] = 'Version tiedot varmuuskopio ei vastaa versio asennettu tähän järjestelmään.';
+$lang['configuration_backup_release_missing'] = 'Version tietoja ei ole saatavilla varmuuskopio.';
+$lang['configuration_backup_release_unsupported:'] = 'Palauttaminen seuraavasta versiosta ei tueta:';
+$lang['configuration_backup_restarting_service:'] = 'Uudelleenkäynnistystä palvelu:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Palauta käynnissä.';
+$lang['configuration_backup_restore_complete'] = 'Palautus valmis.';
+$lang['configuration_backup_restore_file'] = 'Archive File';
+$lang['configuration_backup_restore_from_archive'] = 'Palauta arkistosta';
+$lang['configuration_backup_restore_log'] = 'Palauta Log';
+$lang['configuration_backup_restore_type:'] = 'Palauta tyyppi:';
+$lang['configuration_backup_starting_event_system'] = 'Käynnistäminen tapahtuman järjestäjä palvelu';
+$lang['configuration_backup_status_data_not_available'] = 'Status tietoja ei ole saatavilla.';
+$lang['configuration_backup_stopping_event_system'] = 'Lopettaminen tapahtuman järjestäjä palvelu';
+$lang['configuration_backup_unable_to_start_restore'] = 'Ei voi käynnistää palautuksen';
+$lang['configuration_backup_unpacking_archive'] = 'Pakkauksen purkaminen arkistotiedostoa';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Odotetaan ohjelmistopäivityksiä järjestelmä';
+$lang['configuration_backup_yum_busy'] = 'Paketti johtaja on varattu.';
+
