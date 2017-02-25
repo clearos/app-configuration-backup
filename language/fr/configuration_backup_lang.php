f228a5b54533106455c9c274155b6748f394b6eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:41:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'La sauvegarde de la configuration et de restaurer l&#39;application permet à un administrateur de prendre un instantané de tous les paramètres de configuration du système, ce qui permet la restauration facile en cas de perte de données.';
+$lang['configuration_backup_app_name'] = 'Sauvegarde de configuration et de restauration';
+$lang['configuration_backup_archives'] = 'Les archives';
+$lang['configuration_backup_backup_file_invalid'] = 'filename de sauvegarde est incorrect.';
+$lang['configuration_backup_backup_now'] = 'Sauvegarder maintenant';
+$lang['configuration_backup_checking_version'] = 'Vérification de la compatibilité des versions d&#39;archives';
+$lang['configuration_backup_clear'] = 'Clair';
+$lang['configuration_backup_confirm_restore'] = 'Etes-vous sûr que vous voulez restaurer en utilisant le fichier d&#39;archive ci-dessous?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory n&#39;a pas pu être converti.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Téléchargement et installation d&#39;applications. S&#39;il vous plaît être patient.';
+$lang['configuration_backup_file_information'] = 'Informations sur le fichier';
+$lang['configuration_backup_importing_ldap'] = 'Importation de l&#39;annuaire LDAP';
+$lang['configuration_backup_network_information'] = 'Réseau d&#39;information';
+$lang['configuration_backup_not_registered'] = 'Votre système doit être enregistré avant de restaurer les paramètres de configuration.';
+$lang['configuration_backup_older_version_warn'] = 'Les anciennes versions ne comprennent pas l&#39;application manifeste d&#39;installation - s&#39;il vous plaît installer des applications requises du marché après la restauration';
+$lang['configuration_backup_release_mismatch'] = 'Les informations de version dans le fichier de sauvegarde ne correspond pas à la version installée sur ce système.';
+$lang['configuration_backup_release_missing'] = 'Les informations de version ne sont pas disponibles dans le fichier de sauvegarde.';
+$lang['configuration_backup_release_unsupported:'] = 'Restauration à partir de la version suivante est pas pris en charge:';
+$lang['configuration_backup_restarting_service:'] = 'Service Redémarrage:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restauration en cours.';
+$lang['configuration_backup_restore_complete'] = 'Restauration terminée.';
+$lang['configuration_backup_restore_file'] = 'Fichier d&#39;archive';
+$lang['configuration_backup_restore_from_archive'] = 'Restaurer à partir de Archive';
+$lang['configuration_backup_restore_log'] = 'Restaurer Connexion';
+$lang['configuration_backup_restore_type:'] = 'Type de restauration:';
+$lang['configuration_backup_starting_event_system'] = 'Démarrage du service du moniteur d&#39;événements';
+$lang['configuration_backup_status_data_not_available'] = 'Données d&#39;état non disponible.';
+$lang['configuration_backup_stopping_event_system'] = 'Arrêt du service du moniteur d&#39;événements';
+$lang['configuration_backup_unable_to_start_restore'] = 'Impossible de démarrer l&#39;opération de restauration';
+$lang['configuration_backup_unpacking_archive'] = 'fichier d&#39;archive Déballage';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'En attente de système de mises à jour logicielles';
+$lang['configuration_backup_yum_busy'] = 'gestionnaire de paquet est occupé.';
+
