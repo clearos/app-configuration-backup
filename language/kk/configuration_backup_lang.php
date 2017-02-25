3d3a5707c5ed6dfe6098b4633e0184b35ee4e809
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:44:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'конфигурация сақтық көшірме және қалпына келтіру қолданбасын деректер жоғалған жағдайда оңай қалпына келтіру мүмкіндік беретін, жүйесінің барлық конфигурация параметрлерін түсірім жасау үшін әкімші мүмкіндік береді.';
+$lang['configuration_backup_app_name'] = 'Конфигурация көшіру және қалпына келтіру';
+$lang['configuration_backup_archives'] = 'мұрағат';
+$lang['configuration_backup_backup_file_invalid'] = 'Сақтық көшірме файл аты жарамсыз.';
+$lang['configuration_backup_backup_now'] = 'Енді Сақтық көшірме';
+$lang['configuration_backup_checking_version'] = 'Мұрағат нұсқасы үйлесімділігін тексеру';
+$lang['configuration_backup_clear'] = 'айқын';
+$lang['configuration_backup_confirm_restore'] = 'Сіз мына мұрағат файлын пайдаланып қалпына келтіру үшін шынымен қалайсыз ба?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory түрлендіру мүмкін емес еді.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Жүктеу және орнату бағдарламаларды. Пожалуйста шыдамды.';
+$lang['configuration_backup_file_information'] = 'Файл туралы ақпарат';
+$lang['configuration_backup_importing_ldap'] = 'LDAP каталогын импорттау';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Сіздің жүйесі конфигурациясының параметрлерін қалпына келтіру бұрын тіркелген болуы тиіс.';
+$lang['configuration_backup_older_version_warn'] = 'Егде нұсқалары Қолданба манифест орнату мыналарды қамтымайды - қалпына келтіру кейін Marketplace қажетті бағдарламаларды орнатыңыз';
+$lang['configuration_backup_release_mismatch'] = 'сақтық көшірме файлында нұсқасы ақпарат осы жүйеде орнатылған нұсқасын сәйкес келмейді.';
+$lang['configuration_backup_release_missing'] = 'нұсқасы ақпарат сақтық көшірме файлында қол жетімді емес.';
+$lang['configuration_backup_release_unsupported:'] = 'мынадай нұсқасын қалпына келтіру қолдау көрсетілмейді:';
+$lang['configuration_backup_restarting_service:'] = 'Қайта іске қосу қызметі:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Қалпынакелтіруорындалуда.';
+$lang['configuration_backup_restore_complete'] = 'толық қалпына келтіру.';
+$lang['configuration_backup_restore_file'] = 'Мұрағат Файл';
+$lang['configuration_backup_restore_from_archive'] = 'Мұрағат бастап қалпына келтіру';
+$lang['configuration_backup_restore_log'] = 'Кіру қалпына келтіру';
+$lang['configuration_backup_restore_type:'] = 'түрін қалпына келтіру:';
+$lang['configuration_backup_starting_event_system'] = 'Іс-шара монитор қызмет бастап';
+$lang['configuration_backup_status_data_not_available'] = 'Күй деректер қол жетімді емес.';
+$lang['configuration_backup_stopping_event_system'] = 'Іс-шара монитор қызмет тоқтату';
+$lang['configuration_backup_unable_to_start_restore'] = 'жұмыс қабілетін қалпына келтіру жегілмеді';
+$lang['configuration_backup_unpacking_archive'] = 'мұрағат файлын Қораптан шығару';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'бағдарламалық құрал жаңартулары жүйесін күту';
+$lang['configuration_backup_yum_busy'] = 'Пакет менеджері бос емес.';
+
