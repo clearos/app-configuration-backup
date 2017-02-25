aedb999b3329c7dbcc4722a7894fd43a0ee174b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:38:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Die opset Friends en inligting te herstel kan &#39;n administrateur om &#39;n oorsig van al konfigurasie-instellings van die stelsel te neem, sodat maklik herstel in die geval van die verlies van data.';
+$lang['configuration_backup_app_name'] = 'Opset Backup en herstel';
+$lang['configuration_backup_archives'] = 'Argiewe';
+$lang['configuration_backup_backup_file_invalid'] = 'Friends lêernaam is ongeldig.';
+$lang['configuration_backup_backup_now'] = 'Maak nou &#39;n rugsteun';
+$lang['configuration_backup_checking_version'] = 'Nagaan argief weergawe verenigbaarheid';
+$lang['configuration_backup_clear'] = 'duidelik';
+$lang['configuration_backup_confirm_restore'] = 'Is jy seker jy wil herstel met behulp van die volgende argief?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Gids kon nie tot bekering gekom.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Aflaai en installering van programme. Wees asseblief geduldig.';
+$lang['configuration_backup_file_information'] = 'lêer inligting';
+$lang['configuration_backup_importing_ldap'] = 'Die invoer van LDAP directory';
+$lang['configuration_backup_network_information'] = 'netwerk inligting';
+$lang['configuration_backup_not_registered'] = 'Jou stelsel moet voor die herstel van konfigurasie-instellings geregistreer word.';
+$lang['configuration_backup_older_version_warn'] = 'Ouer weergawes nie app sluit installeer openbaar - installeer vereis programme van die mark na herstel';
+$lang['configuration_backup_release_mismatch'] = 'Die weergawe inligting in die rugsteunlêer kom nie ooreen met die weergawe van die stelsel.';
+$lang['configuration_backup_release_missing'] = 'Die weergawe inligting is nie beskikbaar in die rugsteunlêer.';
+$lang['configuration_backup_release_unsupported:'] = 'Herstel van die volgende weergawe word nie ondersteun nie:';
+$lang['configuration_backup_restarting_service:'] = 'Weer te begin diens:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Herstel aan die gang.';
+$lang['configuration_backup_restore_complete'] = 'Herstel voltooi.';
+$lang['configuration_backup_restore_file'] = 'argief';
+$lang['configuration_backup_restore_from_archive'] = 'Herstel Van Argief';
+$lang['configuration_backup_restore_log'] = 'herstel Meld';
+$lang['configuration_backup_restore_type:'] = 'Herstel type:';
+$lang['configuration_backup_starting_event_system'] = 'Begin gebeurtenis monitor diens';
+$lang['configuration_backup_status_data_not_available'] = 'Status data nie beskikbaar nie.';
+$lang['configuration_backup_stopping_event_system'] = 'Stop geval monitor diens';
+$lang['configuration_backup_unable_to_start_restore'] = 'Kan nie begin herstel operasie';
+$lang['configuration_backup_unpacking_archive'] = 'Uiteensetting argief';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Wag vir sagteware-bywerkings stelsel';
+$lang['configuration_backup_yum_busy'] = 'Pakket bestuurder is besig.';
+
