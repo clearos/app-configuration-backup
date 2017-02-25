f174e4b96bc63b8a416e5be6582539a03f2d7f90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:46:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'कॉन्फिगरेशन बॅकअप आणि अनुप्रयोग पुनर्संचयित डेटा नुकसान झाल्यास सोपे जीर्णोद्धार, ज्यामुळे प्रणाली सर्व संरचना सेटिंग्ज स्नॅपशॉट प्रशासक परवानगी देते.';
+$lang['configuration_backup_app_name'] = 'संरचना बॅकअप आणि पुनर्संचयित करा';
+$lang['configuration_backup_archives'] = 'संग्रहण';
+$lang['configuration_backup_backup_file_invalid'] = 'बॅकअप फाइलनाव अवैध आहे.';
+$lang['configuration_backup_backup_now'] = 'आताच साठवून ठेवा';
+$lang['configuration_backup_checking_version'] = 'संग्रहण आवृत्ती सुसंगतता तपासणी';
+$lang['configuration_backup_clear'] = 'साफ करा';
+$lang['configuration_backup_confirm_restore'] = 'आपण खालील आर्काइव्ह फाइल वापरून पुनर्संचयित करू इच्छिता आहे?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'निर्देशिका रूपांतरीत केले जाऊ शकत नाही.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'डाउनलोड आणि अनुप्रयोग स्थापित. कृपया धीर धरा.';
+$lang['configuration_backup_file_information'] = 'फाइल माहिती';
+$lang['configuration_backup_importing_ldap'] = 'आयात LDAP डिरेक्ट्री';
+$lang['configuration_backup_network_information'] = 'नेटवर्क माहिती';
+$lang['configuration_backup_not_registered'] = 'आपले सिस्टम कॉन्फिगरेशन सेटिंग्ज पुनर्संचयित आधी नोंदणी करणे आवश्यक आहे.';
+$lang['configuration_backup_older_version_warn'] = 'जुनी आवृत्ती अनुप्रयोग समाविष्ट नाही स्पष्ट प्रतिष्ठापीत - बाजार आवश्यक अनुप्रयोग स्थापित परत नंतर करा';
+$lang['configuration_backup_release_mismatch'] = 'बॅकअप फाइल आवृत्ती माहिती आवृत्ती या प्रणालीवर प्रतिष्ठापीत जुळत नाही.';
+$lang['configuration_backup_release_missing'] = 'आवृत्ती माहिती बॅकअप फाइल उपलब्ध नाही.';
+$lang['configuration_backup_release_unsupported:'] = 'खालील आवृत्ती पुनर्संचयित समर्थित नाही:';
+$lang['configuration_backup_restarting_service:'] = 'पुनः सुरू करा सेवा:';
+$lang['configuration_backup_restore_already_in_progress'] = 'प्रगतीपथावर पुनर्संचयित करा.';
+$lang['configuration_backup_restore_complete'] = 'पुनर्संचयित पूर्ण.';
+$lang['configuration_backup_restore_file'] = 'आर्काइव्ह फाइल';
+$lang['configuration_backup_restore_from_archive'] = 'पासून संग्रहण पुनर्संचयित करा';
+$lang['configuration_backup_restore_log'] = 'पुनर्संचयित करा लॉग';
+$lang['configuration_backup_restore_type:'] = 'प्रकार पूर्ववत';
+$lang['configuration_backup_starting_event_system'] = 'कार्यक्रम मॉनिटर सेवा सुरू';
+$lang['configuration_backup_status_data_not_available'] = 'स्थिती डेटा उपलब्ध नाही.';
+$lang['configuration_backup_stopping_event_system'] = 'कार्यक्रम मॉनिटर सेवा थांबता';
+$lang['configuration_backup_unable_to_start_restore'] = 'ऑपरेशन पुनर्संचयित सुरू करण्यात अक्षम';
+$lang['configuration_backup_unpacking_archive'] = 'पॅकिंग आर्काइव्ह फाइल';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'सॉफ्टवेअर सुधारणा प्रणाली वाट पाहत';
+$lang['configuration_backup_yum_busy'] = 'पॅकेज व्यवस्थापक व्यस्त आहे.';
+
