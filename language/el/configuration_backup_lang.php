fc8515efa048c1ec904e425c85db5bd8abb98b06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:42:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Η ρύθμιση αντιγράφων ασφαλείας και επαναφοράς app επιτρέπει σε έναν διαχειριστή να πάρετε ένα στιγμιότυπο όλων των ρυθμίσεων διαμόρφωσης του συστήματος, επιτρέποντας την εύκολη αποκατάσταση σε περίπτωση απώλειας δεδομένων.';
+$lang['configuration_backup_app_name'] = 'Δημιουργία αντιγράφων ασφαλείας και επαναφορά ρυθμίσεων';
+$lang['configuration_backup_archives'] = 'Αρχεία';
+$lang['configuration_backup_backup_file_invalid'] = 'Δημιουργία αντιγράφων ασφαλείας αρχείου δεν είναι έγκυρο.';
+$lang['configuration_backup_backup_now'] = 'αντιγράφων ασφαλείας Τώρα';
+$lang['configuration_backup_checking_version'] = 'Έλεγχος αρχείο συμβατότητα έκδοση';
+$lang['configuration_backup_clear'] = 'Σαφή';
+$lang['configuration_backup_confirm_restore'] = 'Είστε σίγουροι ότι θέλετε να επαναφέρετε χρησιμοποιώντας το παρακάτω αρχείο αρχείο;';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory δεν θα μπορούσε να μετατραπεί.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Τη λήψη και την εγκατάσταση εφαρμογών. Παρακαλώ να είστε υπομονετικοί.';
+$lang['configuration_backup_file_information'] = 'Πληροφορίες αρχείου';
+$lang['configuration_backup_importing_ldap'] = 'Εισαγωγή καταλόγου LDAP';
+$lang['configuration_backup_network_information'] = 'Πληροφορίες δικτύου';
+$lang['configuration_backup_not_registered'] = 'Το σύστημά σας θα πρέπει να έχουν εγγραφεί πριν από την επαναφορά των ρυθμίσεων διαμόρφωσης.';
+$lang['configuration_backup_older_version_warn'] = 'Οι παλαιότερες εκδόσεις δεν περιλαμβάνουν εγκατάσταση εφαρμογής πρόδηλη - παρακαλούμε να εγκαταστήσετε το απαιτούμενο εφαρμογές από το Marketplace μετά την αποκατάσταση';
+$lang['configuration_backup_release_mismatch'] = 'Οι πληροφορίες έκδοσης στο αρχείο αντιγράφου ασφαλείας δεν ταιριάζει με την έκδοση εγκατασταθεί σε αυτό το σύστημα.';
+$lang['configuration_backup_release_missing'] = 'Οι πληροφορίες έκδοση δεν είναι διαθέσιμη στο αρχείο αντιγράφου ασφαλείας.';
+$lang['configuration_backup_release_unsupported:'] = 'Επαναφορά από την ακόλουθη έκδοση δεν υποστηρίζεται:';
+$lang['configuration_backup_restarting_service:'] = 'Επανεκκίνηση των υπηρεσιών:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Επαναφορά σε εξέλιξη.';
+$lang['configuration_backup_restore_complete'] = 'Επαναφορά πλήρης.';
+$lang['configuration_backup_restore_file'] = 'Αποθήκη αρχείων';
+$lang['configuration_backup_restore_from_archive'] = 'Επαναφορά από το αρχείο';
+$lang['configuration_backup_restore_log'] = 'Επαναφορά Σύνδεση';
+$lang['configuration_backup_restore_type:'] = 'Τύπος Επαναφορά:';
+$lang['configuration_backup_starting_event_system'] = 'Ξεκινώντας την παρακολούθηση των υπηρεσιών εκδήλωση';
+$lang['configuration_backup_status_data_not_available'] = 'δεδομένα κατάστασης δεν είναι διαθέσιμα.';
+$lang['configuration_backup_stopping_event_system'] = 'Η διακοπή υπηρεσίας οθόνη εκδήλωση';
+$lang['configuration_backup_unable_to_start_restore'] = 'Αδύνατη η εκκίνηση αποκατάσταση της λειτουργίας';
+$lang['configuration_backup_unpacking_archive'] = 'Αποσυσκευασία αρχείο αρχειοθέτησης';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Αναμονή για το σύστημα ενημερώσεις λογισμικού';
+$lang['configuration_backup_yum_busy'] = 'διαχειριστής πακέτων είναι απασχολημένος.';
+
