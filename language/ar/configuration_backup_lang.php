f37dd5410680a905a96b029adb0d1d4f2ddd80a3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:38:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'النسخ الاحتياطي التكوين واستعادة التطبيق يسمح مسؤول لأخذ لقطة من كافة الإعدادات تكوين النظام، مما يتيح استعادة سهلة في حال فقدان البيانات.';
+$lang['configuration_backup_app_name'] = 'النسخ الاحتياطي والاستعادة التكوين';
+$lang['configuration_backup_archives'] = 'أرشيف';
+$lang['configuration_backup_backup_file_invalid'] = 'اسم الملف النسخ الاحتياطي غير صالح.';
+$lang['configuration_backup_backup_now'] = 'اعمل نسخة احتياطية الان';
+$lang['configuration_backup_checking_version'] = 'التحقق من توافق الإصدار أرشيف';
+$lang['configuration_backup_clear'] = 'واضح';
+$lang['configuration_backup_confirm_restore'] = 'هل أنت متأكد أنك تريد استعادة باستخدام ملف الأرشيف التالية؟';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'لا يمكن تحويل الدليل.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'تحميل وتثبيت التطبيقات. يرجى أن يكون المريض.';
+$lang['configuration_backup_file_information'] = 'معلومات الملف';
+$lang['configuration_backup_importing_ldap'] = 'استيراد دليل LDAP';
+$lang['configuration_backup_network_information'] = 'معلومات الشبكة';
+$lang['configuration_backup_not_registered'] = 'يجب تسجيل النظام الخاص بك قبل استعادة إعدادات التكوين.';
+$lang['configuration_backup_older_version_warn'] = 'لا تشمل الإصدارات القديمة التطبيق تثبيت واضح - يرجى تثبيت التطبيقات المطلوبة من السوق بعد استعادة';
+$lang['configuration_backup_release_mismatch'] = 'لا يطابق معلومات الإصدار في ملف النسخ الاحتياطي الإصدار المثبت على هذا النظام.';
+$lang['configuration_backup_release_missing'] = 'معلومات الإصدار غير متوفرة في ملف النسخة الاحتياطية.';
+$lang['configuration_backup_release_unsupported:'] = 'استعادة من النسخة التالية غير معتمد:';
+$lang['configuration_backup_restarting_service:'] = 'إعادة تشغيل الخدمة:';
+$lang['configuration_backup_restore_already_in_progress'] = 'استعادة في التقدم.';
+$lang['configuration_backup_restore_complete'] = 'استعادة كاملة.';
+$lang['configuration_backup_restore_file'] = 'ملف أرشيف';
+$lang['configuration_backup_restore_from_archive'] = 'استعادة من الأرشيف';
+$lang['configuration_backup_restore_log'] = 'استعادة دخول';
+$lang['configuration_backup_restore_type:'] = 'استعادة نوع:';
+$lang['configuration_backup_starting_event_system'] = 'بدء خدمة رصد الحدث';
+$lang['configuration_backup_status_data_not_available'] = 'البيانات الحالة غير متوفرة.';
+$lang['configuration_backup_stopping_event_system'] = 'وقف خدمة رصد الحدث';
+$lang['configuration_backup_unable_to_start_restore'] = 'غير قادر على بدء عملية استعادة';
+$lang['configuration_backup_unpacking_archive'] = 'التفريغ ملف أرشيف';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'انتظار لنظام تحديثات البرامج';
+$lang['configuration_backup_yum_busy'] = 'مدير مجموعة مشغول.';
+
