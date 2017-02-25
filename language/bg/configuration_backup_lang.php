1020d07680c18bd5f4560507ceaa630f188b3c69
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:40:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'резервно копие на конфигурацията и възстанови приложение позволява на администратора да се направи моментна снимка на всички настройки за конфигурация на системата, което позволява лесно възстановяване в случай на загуба на данни.';
+$lang['configuration_backup_app_name'] = 'Конфигурация за архивиране и възстановяване';
+$lang['configuration_backup_archives'] = 'архиви';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup файла е невалиден.';
+$lang['configuration_backup_backup_now'] = 'Архивиране сега';
+$lang['configuration_backup_checking_version'] = 'Проверка архив съвместимост версия';
+$lang['configuration_backup_clear'] = 'ясно';
+$lang['configuration_backup_confirm_restore'] = 'Сигурни ли сте, че искате да се възстанови с помощта на следната архив файл?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Категория не може да се преобразува.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Изтеглянето и инсталирането на приложения. Моля, бъдете търпеливи.';
+$lang['configuration_backup_file_information'] = 'Информация за файла';
+$lang['configuration_backup_importing_ldap'] = 'Внос LDAP директория';
+$lang['configuration_backup_network_information'] = 'информационна мрежа';
+$lang['configuration_backup_not_registered'] = 'Вашата система трябва да бъдат регистрирани преди възстановяване на конфигурационни настройки.';
+$lang['configuration_backup_older_version_warn'] = 'По-старите версии не включват инсталиране на приложения явна - моля инсталирате необходимите приложения от Marketplace след възстановяване';
+$lang['configuration_backup_release_mismatch'] = 'информация за версията в архивния файл не съвпада с версията инсталирана на тази система.';
+$lang['configuration_backup_release_missing'] = 'информация за версията не е достъпно в архивния файл.';
+$lang['configuration_backup_release_unsupported:'] = 'Възстановяване от следващата версия не се поддържа:';
+$lang['configuration_backup_restarting_service:'] = 'Рестартирането на услуги:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Възстановяване в прогрес.';
+$lang['configuration_backup_restore_complete'] = 'Възстановяване на пълен.';
+$lang['configuration_backup_restore_file'] = 'Архив на файла';
+$lang['configuration_backup_restore_from_archive'] = 'Възстановяване от архив';
+$lang['configuration_backup_restore_log'] = 'Възстановяване Вход';
+$lang['configuration_backup_restore_type:'] = 'Възстановяване на тип:';
+$lang['configuration_backup_starting_event_system'] = 'Започвайки услуга събитие монитор';
+$lang['configuration_backup_status_data_not_available'] = 'не е налична информация за състоянието.';
+$lang['configuration_backup_stopping_event_system'] = 'Спиране на услуга събитие монитор';
+$lang['configuration_backup_unable_to_start_restore'] = 'Не може да започне операция по възстановяване';
+$lang['configuration_backup_unpacking_archive'] = 'Разопаковане архивен файл';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'В очакване на софтуерни актуализации система';
+$lang['configuration_backup_yum_busy'] = 'управление на пакетите е зает.';
+
