a609473064f4f20caf8003229ab4204af6eeee82
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:47:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'پشتیبان گیری و بازیابی تنظیمات برنامه اجازه می دهد یک مدیر برای گرفتن یک عکس فوری از تمام تنظیمات پیکربندی سیستم، اجازه می دهد ترمیم آسان در صورت از دست رفتن داده ها.';
+$lang['configuration_backup_app_name'] = 'پشتیبان گیری و بازگرداندن تنظیمات';
+$lang['configuration_backup_archives'] = 'بایگانی';
+$lang['configuration_backup_backup_file_invalid'] = 'نام فایل پشتیبان گیری نامعتبر است.';
+$lang['configuration_backup_backup_now'] = 'همین حالا نسخه پشتیبان تهیه کن';
+$lang['configuration_backup_checking_version'] = 'چک کردن آرشیو سازگاری نسخه';
+$lang['configuration_backup_clear'] = 'واضح';
+$lang['configuration_backup_confirm_restore'] = 'آیا مطمئن هستید که می خواهید برای بازگرداندن با استفاده از فایل آرشیو زیر؟';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'راهنمای نمی تواند تغییر یابد.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'دانلود و نصب برنامه های. لطفا صبور باشید.';
+$lang['configuration_backup_file_information'] = 'اطلاعات فایل';
+$lang['configuration_backup_importing_ldap'] = 'واردات دایرکتوری LDAP';
+$lang['configuration_backup_network_information'] = 'اطلاعات شبکه';
+$lang['configuration_backup_not_registered'] = 'سیستم شما باید قبل از بازگرداندن تنظیمات ثبت شده است.';
+$lang['configuration_backup_older_version_warn'] = 'نسخه های قدیمی تر انجام برنامه را شامل نمی شود نصب آشکار - لطفا نصب برنامه های مورد نیاز از بازار پس از بازگرداندن';
+$lang['configuration_backup_release_mismatch'] = 'اطلاعات نسخه در فایل پشتیبان از نسخه نصب بر روی این سیستم مطابقت ندارد.';
+$lang['configuration_backup_release_missing'] = 'اطلاعات نسخه در فایل پشتیبان در دسترس نیست.';
+$lang['configuration_backup_release_unsupported:'] = 'بازیابی از نسخه زیر پشتیبانی نمی:';
+$lang['configuration_backup_restarting_service:'] = 'راه اندازی مجدد سرویس:';
+$lang['configuration_backup_restore_already_in_progress'] = 'بازیابی در حال پیشرفت است.';
+$lang['configuration_backup_restore_complete'] = 'بازیابی کامل.';
+$lang['configuration_backup_restore_file'] = 'آرشیو فایل';
+$lang['configuration_backup_restore_from_archive'] = 'بازگرداندن از بایگانی';
+$lang['configuration_backup_restore_log'] = 'بازگرداندن ورود';
+$lang['configuration_backup_restore_type:'] = 'بازگرداندن نوع:';
+$lang['configuration_backup_starting_event_system'] = 'شروع سرویس مانیتور رویداد';
+$lang['configuration_backup_status_data_not_available'] = 'داده های وضعیت در دسترس نیست.';
+$lang['configuration_backup_stopping_event_system'] = 'توقف خدمات مانیتور رویداد';
+$lang['configuration_backup_unable_to_start_restore'] = 'قادر به شروع عملیات را بازیابی';
+$lang['configuration_backup_unpacking_archive'] = 'فایل آرشیو باز کردن بسته بندی';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'در حال انتظار برای سیستم به روز رسانی نرم افزار';
+$lang['configuration_backup_yum_busy'] = 'مدیر بسته مشغول است.';
+
