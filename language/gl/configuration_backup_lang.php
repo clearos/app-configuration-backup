460da9fb4b052ebd33fbdea67592194a9595af50
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:42:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'O backup de configuración e restaurar aplicación permite que un administrador para sacar unha instantánea de todas as opcións do sistema, permitindo a fácil recuperación en caso de perda de datos.';
+$lang['configuration_backup_app_name'] = 'Configuration Backup e Restauración';
+$lang['configuration_backup_archives'] = 'arquivo';
+$lang['configuration_backup_backup_file_invalid'] = 'filename backup é válido.';
+$lang['configuration_backup_backup_now'] = 'backup Now';
+$lang['configuration_backup_checking_version'] = 'Comprobar a compatibilidade versión de ficheiro';
+$lang['configuration_backup_clear'] = 'claro';
+$lang['configuration_backup_confirm_restore'] = 'Está seguro de que quere restaurar usando o seguinte ficheiro?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory non se pode converter.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Descargar e instalar aplicacións. Por favor, sexa paciente.';
+$lang['configuration_backup_file_information'] = 'Información de ficheiro';
+$lang['configuration_backup_importing_ldap'] = 'directorio LDAP importar';
+$lang['configuration_backup_network_information'] = 'Información de rede';
+$lang['configuration_backup_not_registered'] = 'O sistema debe ser rexistrado antes de dar opcións de configuración.';
+$lang['configuration_backup_older_version_warn'] = 'As versións máis antigas non inclúen instalación de aplicacións manifesto - por favor, instalar aplicacións necesarios do mercado tras a restauración';
+$lang['configuration_backup_release_mismatch'] = 'A información de versión no arquivo de copia de seguridade non coincide coa versión instalada neste sistema.';
+$lang['configuration_backup_release_missing'] = 'A información de versión non está dispoñible no arquivo de copia de seguridade.';
+$lang['configuration_backup_release_unsupported:'] = 'Restaurar a partir da seguinte versión non está soportado:';
+$lang['configuration_backup_restarting_service:'] = 'Reiniciar servizo:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restauración en marcha.';
+$lang['configuration_backup_restore_complete'] = 'Restauración completa.';
+$lang['configuration_backup_restore_file'] = 'archive File';
+$lang['configuration_backup_restore_from_archive'] = 'Restaurar de Arquivo';
+$lang['configuration_backup_restore_log'] = 'restaurar Log';
+$lang['configuration_backup_restore_type:'] = 'Restauración Tipo:';
+$lang['configuration_backup_starting_event_system'] = 'Iniciando o servizo do monitor de eventos';
+$lang['configuration_backup_status_data_not_available'] = 'datos de estado non dispoñible.';
+$lang['configuration_backup_stopping_event_system'] = 'Deter o servizo do monitor de eventos';
+$lang['configuration_backup_unable_to_start_restore'] = 'Non se pode iniciar operación de restauración';
+$lang['configuration_backup_unpacking_archive'] = 'arquivo de desempaquetar';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Á espera de sistema de actualizacións de software';
+$lang['configuration_backup_yum_busy'] = 'xestor de paquetes está ocupado.';
+
