4f1716bb05b35c16a61026319911fd807f192075
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:43:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Ceadaíonn an cúltaca chumraíocht agus app ar ais riarthóir a ghlacadh léargas ar gach suíomh chumraíocht an chórais, rud a ligeann athchóiriú éasca i gcás an chaillteanais sonraí.';
+$lang['configuration_backup_app_name'] = 'Cúltaca Cumraíocht agus Athchóirigh';
+$lang['configuration_backup_archives'] = 'Cartlann';
+$lang['configuration_backup_backup_file_invalid'] = 'Is filename Cúltaca neamhbhailí.';
+$lang['configuration_backup_backup_now'] = 'cúltaca Anois';
+$lang['configuration_backup_checking_version'] = 'Seiceáil leagan cartlainne comhoiriúnacht';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'An bhfuil tú cinnte gur mian leat a chur ar ais ag baint úsáide as an cnuaschomhad a leanas?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Níorbh fhéidir eolaire a thiontú.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Íosluchtú agus apps shuiteáil. Tabhair faoi a n-othar.';
+$lang['configuration_backup_file_information'] = 'Faisnéis comhad';
+$lang['configuration_backup_importing_ldap'] = 'Iompórtáil eolaire LDAP';
+$lang['configuration_backup_network_information'] = 'líonra Faisnéise';
+$lang['configuration_backup_not_registered'] = 'Ní mór do do chóras a bheith cláraithe sula athchóiriú suímh chumraíocht.';
+$lang['configuration_backup_older_version_warn'] = 'Ní áirítear leaganacha níos sine app a shuiteáil léiriú - le do thoil shuiteáil apps ag teastáil ón Marketplace ndiaidh ar ais';
+$lang['configuration_backup_release_mismatch'] = 'Ní dhéanann an t-eolas leagan sa chomhad cúltaca mheaitseáil leis an leagan suiteáilte ar an gcóras seo.';
+$lang['configuration_backup_release_missing'] = 'Níl an t-eolas atá ar fáil leagan sa chomhad cúltaca.';
+$lang['configuration_backup_release_unsupported:'] = 'Níl a athchóiriú ón leagan a leanas tacaíocht:';
+$lang['configuration_backup_restarting_service:'] = 'Seirbhís Restarting:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Athchóirigh siúl.';
+$lang['configuration_backup_restore_complete'] = 'Athchóirigh críochnaithe.';
+$lang['configuration_backup_restore_file'] = 'Comhad Cartlann';
+$lang['configuration_backup_restore_from_archive'] = 'Athchóirigh Ó Cartlann';
+$lang['configuration_backup_restore_log'] = 'Athchóirigh Logáil';
+$lang['configuration_backup_restore_type:'] = 'Athchóirigh cineál:';
+$lang['configuration_backup_starting_event_system'] = 'Ag tosú Seirbhís monatóireacht imeacht';
+$lang['configuration_backup_status_data_not_available'] = 'sonraí stádais ar fáil.';
+$lang['configuration_backup_stopping_event_system'] = 'Stopadh Seirbhís monatóireacht imeacht';
+$lang['configuration_backup_unable_to_start_restore'] = 'Níféidir thosú ar ais oibríocht';
+$lang['configuration_backup_unpacking_archive'] = 'comhad cartlann díphacáil';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Ag feitheamh le córas nuashonruithe bogearraí';
+$lang['configuration_backup_yum_busy'] = 'Is bainisteoir pacáiste gnóthach.';
+
