bdec19f9874aa7d40e727f351cde1012ec964c8e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:46:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'ക്രമീകരണ ബാക്കപ്പ് അപ്ലിക്കേഷൻ പുനഃസ്ഥാപിക്കാൻ ഡാറ്റ നഷ്ടം ഇവന്റ് എളുപ്പമല്ല പുനഃസ്ഥാപിക്കണമെന്ന് അനുവദിച്ചുകൊണ്ട് സിസ്റ്റത്തിന്റെ എല്ലാ ക്രമീകരണങ്ങളും ക്രമീകരണങ്ങളുടെ ഒരു സ്നാപ്പ്ഷോട്ടെടുക്കാൻ ഒരു അഡ്മിനിസ്ട്രേറ്റർ അനുവദിക്കുന്നു.';
+$lang['configuration_backup_app_name'] = 'ക്രമീകരണ ബാക്കപ്പ് പുനഃസ്ഥാപിക്കുകയും';
+$lang['configuration_backup_archives'] = 'ഗന്ഥരക്ഷാലയം';
+$lang['configuration_backup_backup_file_invalid'] = 'ബാക്കപ്പ് ഫയൽനാമം അസാധുവാണ്.';
+$lang['configuration_backup_backup_now'] = 'ഇപ്പോൾ ബാക്കപ്പ്';
+$lang['configuration_backup_checking_version'] = 'ആർക്കൈവ് പതിപ്പ് അനുയോജ്യത പരിശോധിക്കുന്നു';
+$lang['configuration_backup_clear'] = 'തെളിഞ്ഞ';
+$lang['configuration_backup_confirm_restore'] = 'നിങ്ങൾക്ക് ഉറപ്പാണോ താഴെ ആർക്കൈവ് ഫയൽ ഉപയോഗിച്ച് പുനഃസ്ഥാപിക്കാൻ താൽപ്പര്യപ്പെടുന്നോ?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ഡയറക്ടറി പരിവർത്തനം ചെയ്യാൻ കഴിഞ്ഞില്ല.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ഡൌൺലോഡ് അപ്ലിക്കേഷനുകൾ ഇൻസ്റ്റാൾ. ദയവായി ക്ഷമിക്കുക.';
+$lang['configuration_backup_file_information'] = 'ഫയൽ വിവരങ്ങൾ';
+$lang['configuration_backup_importing_ldap'] = 'LDAP ഡയറക്ടറി ഇറക്കുമതി';
+$lang['configuration_backup_network_information'] = 'നെറ്റ്വർക്ക് വിവരം';
+$lang['configuration_backup_not_registered'] = 'നിങ്ങളുടെ സിസ്റ്റം കോൺഫിഗറേഷൻ ക്രമീകരണങ്ങൾ പുനഃസ്ഥാപിക്കുക മുമ്പ് രജിസ്റ്റർ ചെയ്യണം.';
+$lang['configuration_backup_older_version_warn'] = 'പഴയ പതിപ്പുകൾ അപ്ലിക്കേഷൻ വ്യക്തമായ ഇൻസ്റ്റാൾ ഉൾപ്പെടുത്തരുത് - പുനഃസ്ഥാപിക്കാൻ ശേഷം Marketplace- ൽ നിന്നും ആവശ്യമായ അപ്ലിക്കേഷനുകൾ ഇൻസ്റ്റാൾ ചെയ്യുക';
+$lang['configuration_backup_release_mismatch'] = 'ബാക്കപ്പ് ഫയലിൽ പതിപ്പ് വിവരങ്ങൾ ഈ സിസ്റ്റത്തിൽ ഇൻസ്റ്റാൾ പതിപ്പ് പൊരുത്തപ്പെടുന്നില്ല.';
+$lang['configuration_backup_release_missing'] = 'പതിപ്പ് വിവരങ്ങൾ ബാക്കപ്പ് ഫയല് ലഭ്യമല്ല.';
+$lang['configuration_backup_release_unsupported:'] = 'താഴെ പതിപ്പ് നിന്ന് പുനഃസ്ഥാപിക്കുന്നു പിന്തുണയ്ക്കുന്നില്ല:';
+$lang['configuration_backup_restarting_service:'] = 'ആരംഭിക്കുന്നു സർവ്വീസ്:';
+$lang['configuration_backup_restore_already_in_progress'] = 'പുരോഗതിയിലാണ് പുനഃസ്ഥാപിക്കുക.';
+$lang['configuration_backup_restore_complete'] = 'പുനഃസ്ഥാപിക്കുന്നത് പൂർത്തിയായി.';
+$lang['configuration_backup_restore_file'] = 'ആർക്കൈവ് ഫയൽ';
+$lang['configuration_backup_restore_from_archive'] = 'ആർക്കൈവിൽ നിന്ന് പുനഃസ്ഥാപിക്കുക';
+$lang['configuration_backup_restore_log'] = 'പ്രവേശിക്കുക പുനഃസ്ഥാപിക്കുക';
+$lang['configuration_backup_restore_type:'] = 'പുനഃസ്ഥാപിക്കുക തരം:';
+$lang['configuration_backup_starting_event_system'] = 'ഇവന്റ് മോണിറ്റർ സേവനം ആരംഭിക്കുന്നു';
+$lang['configuration_backup_status_data_not_available'] = 'സ്റ്റാറ്റസ് ഡാറ്റ ലഭ്യമല്ല.';
+$lang['configuration_backup_stopping_event_system'] = 'ഇവന്റ് മോണിറ്റർ സേവനം നിർത്തുന്നു';
+$lang['configuration_backup_unable_to_start_restore'] = 'പ്രവർത്തനം പുനഃസ്ഥാപിക്കുക ആരംഭിക്കാൻ കഴിയുന്നില്ല';
+$lang['configuration_backup_unpacking_archive'] = 'ആർക്കൈവ് ഫയൽ അൺപാക്കുചെയ്യുന്നു';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'സോഫ്റ്റ്വെയർ അപ്ഡേറ്റുകൾ സിസ്റ്റം കാത്തിരിക്കുന്നു';
+$lang['configuration_backup_yum_busy'] = 'പാക്കേജ് മാനേജർ തിരക്കിലാണ്.';
+
