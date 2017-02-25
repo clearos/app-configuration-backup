c24227adfec5e93320769439e4d638cbd42ebce2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:51:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Yapılandırma yedekleme ve geri yükleme uygulaması veri kaybı durumunda kolay restorasyon sağlayan, sistemin tüm yapılandırma ayarlarının bir fotoğrafını çekmek için bir yönetici sağlar.';
+$lang['configuration_backup_app_name'] = 'Yapılandırma Yedekleme ve Geri Yükleme';
+$lang['configuration_backup_archives'] = 'arşiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Yedekleme dosya adı geçersiz.';
+$lang['configuration_backup_backup_now'] = 'Şimdi yedekle';
+$lang['configuration_backup_checking_version'] = 'arşiv versiyonu uyumluluğunun kontrol';
+$lang['configuration_backup_clear'] = 'Açık';
+$lang['configuration_backup_confirm_restore'] = 'Eğer aşağıdaki arşiv dosyasını kullanarak geri yüklemek istediğinizden emin misiniz?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Dizin dönüştürülemedi.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'İndirme ve uygulama yükleme. Lütfen sabırlı olun.';
+$lang['configuration_backup_file_information'] = 'dosya Bilgileri';
+$lang['configuration_backup_importing_ldap'] = 'İthalat LDAP dizin';
+$lang['configuration_backup_network_information'] = 'ağ Bilgi';
+$lang['configuration_backup_not_registered'] = 'Sizin sistem yapılandırma ayarlarını geri yüklemeden önce kayıtlı olması gerekir.';
+$lang['configuration_backup_older_version_warn'] = 'Eski sürümler bildirim yüklemek uygulamayı içermez - sonra geri Marketplace&#39;den gerekli uygulamaları kurun';
+$lang['configuration_backup_release_mismatch'] = 'yedekleme dosyası sürüm bilgileri bu sistemde yüklü sürümüyle eşleşmiyor.';
+$lang['configuration_backup_release_missing'] = 'sürüm bilgileri yedekleme dosyası mevcut değildir.';
+$lang['configuration_backup_release_unsupported:'] = 'Aşağıdaki sürümünden geri yükleme desteklenmiyor:';
+$lang['configuration_backup_restarting_service:'] = 'hizmetini yeniden:';
+$lang['configuration_backup_restore_already_in_progress'] = 'sürüyor geri yükleyin.';
+$lang['configuration_backup_restore_complete'] = 'Geri yükleme tamamlandı.';
+$lang['configuration_backup_restore_file'] = 'Arşiv Dosyası';
+$lang['configuration_backup_restore_from_archive'] = 'Arşiv itibaren Restore';
+$lang['configuration_backup_restore_log'] = 'Oturum Restore';
+$lang['configuration_backup_restore_type:'] = 'türünü Restore:';
+$lang['configuration_backup_starting_event_system'] = 'Başlangıç ​​olay monitör hizmeti';
+$lang['configuration_backup_status_data_not_available'] = 'Durum veriler mevcut değildir.';
+$lang['configuration_backup_stopping_event_system'] = 'Durdurma olay monitör hizmeti';
+$lang['configuration_backup_unable_to_start_restore'] = 'geri yükleme işlemi başlatılamıyor';
+$lang['configuration_backup_unpacking_archive'] = 'Ambalajın açılması arşiv dosyası';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'yazılım güncellemeleri sistemi için bekleyen';
+$lang['configuration_backup_yum_busy'] = 'Paket yöneticisi meşgul.';
+
