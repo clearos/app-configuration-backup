097a2b11b14b045fd00eacefaa2a2438b10d1e24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:45:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfigurācijas backup un atjaunot app ļauj administratoram veikt momentuzņēmums visiem konfigurācijas uzstādījumus sistēmas, kas ļauj viegli atjaunot gadījumā datu zudumu.';
+$lang['configuration_backup_app_name'] = 'Konfigurācija Backup un Restore';
+$lang['configuration_backup_archives'] = 'arhīvs';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup faila nosaukums ir nederīgs.';
+$lang['configuration_backup_backup_now'] = 'Backup Tagad';
+$lang['configuration_backup_checking_version'] = 'Pārbaude arhīvs versija saderību';
+$lang['configuration_backup_clear'] = 'skaidrs';
+$lang['configuration_backup_confirm_restore'] = 'Vai tiešām vēlaties atjaunot, izmantojot šādu arhīva failu?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Direktoriju nevar pārvērst.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Lejupielādējot un instalējot progr. Lūdzu, esiet pacietīgi.';
+$lang['configuration_backup_file_information'] = 'Informācija par failu';
+$lang['configuration_backup_importing_ldap'] = 'Importēšana LDAP direktoriju';
+$lang['configuration_backup_network_information'] = 'tīkla informācija';
+$lang['configuration_backup_not_registered'] = 'Jūsu sistēma ir jāreģistrē pirms konfigurācijas uzstādījumus atjaunot.';
+$lang['configuration_backup_older_version_warn'] = 'Vecākas versijas neietver app instalēt manifests - lūdzu, instalējiet nepieciešamos progr no tirgus pēc atjaunošanas';
+$lang['configuration_backup_release_mismatch'] = 'Versijas informācija backup failu neatbilst versija uzstādīta uz šo sistēmu.';
+$lang['configuration_backup_release_missing'] = 'Versija informācija nav pieejama backup failu.';
+$lang['configuration_backup_release_unsupported:'] = 'Atjaunojot no nākamā versija netiek atbalstīta:';
+$lang['configuration_backup_restarting_service:'] = 'Atsākšana pakalpojumu:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Atjaunot progress.';
+$lang['configuration_backup_restore_complete'] = 'Atjaunot pabeigta.';
+$lang['configuration_backup_restore_file'] = 'arhīvs File';
+$lang['configuration_backup_restore_from_archive'] = 'Atjaunot no arhīva';
+$lang['configuration_backup_restore_log'] = 'atjaunot Ieiet';
+$lang['configuration_backup_restore_type:'] = 'Atjaunot veids:';
+$lang['configuration_backup_starting_event_system'] = 'Sākot notikums uzraudzīt pakalpojumu';
+$lang['configuration_backup_status_data_not_available'] = 'Statusa dati nav pieejami.';
+$lang['configuration_backup_stopping_event_system'] = 'Apstāšanās notikums uzraudzīt pakalpojumu';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nevar sākt atjaunot darbību';
+$lang['configuration_backup_unpacking_archive'] = 'Izpakošana arhīva failu';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Gaida programmatūras atjauninājumus sistēmai';
+$lang['configuration_backup_yum_busy'] = 'Paketes vadītājs ir aizņemts.';
+
