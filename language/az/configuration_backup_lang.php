9f6cb19afe484e3a9772047430de99e4ab5eae05
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:39:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'konfiqurasiya backup və app bərpa məlumatların itirilməsi halında asan bərpa imkan verir, sistem bütün konfiqurasiya parametrlərini bir snapshot almaq üçün administrator imkan verir.';
+$lang['configuration_backup_app_name'] = 'Konfiqurasiya Backup və bərpa';
+$lang['configuration_backup_archives'] = 'arxiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup fayl yanlışdır.';
+$lang['configuration_backup_backup_now'] = 'Backup Now';
+$lang['configuration_backup_checking_version'] = 'Arxiv version uyğunluğu yoxlanılması';
+$lang['configuration_backup_clear'] = 'aydın';
+$lang['configuration_backup_confirm_restore'] = 'Siz aşağıdakı arxiv faylı istifadə edərək, bərpa etmək istəyirsiniz?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory konvertasiya edilə bilməz.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Yükləmə və apps quraşdırma. Səbr edin.';
+$lang['configuration_backup_file_information'] = 'File Məlumat';
+$lang['configuration_backup_importing_ldap'] = 'İthalat LDAP kataloq';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Sistem konfiqurasiya parametrlərini bərpa əvvəl qeydiyyatdan keçməlidir.';
+$lang['configuration_backup_older_version_warn'] = 'Eski versiyasını açıq-aşkar yüklemek app daxil deyil - sonra bərpa Marketplace tələb apps bərpa edin';
+$lang['configuration_backup_release_mismatch'] = 'backup fayl versiyası məlumat bu sistem yüklü versiyası uyğun deyil.';
+$lang['configuration_backup_release_missing'] = 'versiyası məlumat backup fayl mövcud deyil.';
+$lang['configuration_backup_release_unsupported:'] = 'aşağıdakı redaksiyada bərpa dəstəklənmir:';
+$lang['configuration_backup_restarting_service:'] = 'xidmət başlatmaqdan:';
+$lang['configuration_backup_restore_already_in_progress'] = 'davam bərpa edin.';
+$lang['configuration_backup_restore_complete'] = 'tam bərpa edin.';
+$lang['configuration_backup_restore_file'] = 'Arxiv File';
+$lang['configuration_backup_restore_from_archive'] = 'Arxiv bərpa';
+$lang['configuration_backup_restore_log'] = 'Giriş bərpa';
+$lang['configuration_backup_restore_type:'] = 'növü bərpa:';
+$lang['configuration_backup_starting_event_system'] = 'Başlayaraq hadisə monitor xidmət';
+$lang['configuration_backup_status_data_not_available'] = 'Status data mövcud deyil.';
+$lang['configuration_backup_stopping_event_system'] = 'Dayandırılması hadisə monitor xidmət';
+$lang['configuration_backup_unable_to_start_restore'] = 'əməliyyat bərpa başlamaq üçün bilmir';
+$lang['configuration_backup_unpacking_archive'] = 'Açıldığı arxiv fayl';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'proqram yenilikləri sistemi üçün gözləyir';
+$lang['configuration_backup_yum_busy'] = 'Package Manager məşğuldur.';
+
