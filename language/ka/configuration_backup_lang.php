4c93a488662542b35d5b72b8599549c2fdccee28
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:44:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'კონფიგურაციის სარეზერვო და აღდგენა app საშუალებას აძლევს ადმინისტრატორს მიიღოს Snapshot ყველა კონფიგურაციის სისტემა, რომელიც საშუალებას იძლევა ადვილად აღდგენა იმ შემთხვევაში, მონაცემების დაკარგვა.';
+$lang['configuration_backup_app_name'] = 'კონფიგურაციის შენახვა და რეზერვირება';
+$lang['configuration_backup_archives'] = 'არქივი';
+$lang['configuration_backup_backup_file_invalid'] = 'სარეზერვო ფაილის არასწორია.';
+$lang['configuration_backup_backup_now'] = 'სარეზერვო ახლა';
+$lang['configuration_backup_checking_version'] = 'შემოწმების არქივი ვერსია თავსებადობა';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'დარწმუნებული ხართ, რომ გსურთ აღდგენა გამოყენების შემდეგ არქივის ფაილი?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'დირექტორია ვერ მოაქცია.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'გადმოწერის და დაყენების apps. გთხოვთ მოთმინება.';
+$lang['configuration_backup_file_information'] = 'ფაილის ინფორმაცია';
+$lang['configuration_backup_importing_ldap'] = 'იმპორტი LDAP დირექტორია';
+$lang['configuration_backup_network_information'] = 'ქსელის საინფორმაციო';
+$lang['configuration_backup_not_registered'] = 'შენი სისტემა უნდა იყოს რეგისტრირებული ადრე აღდგენის კონფიგურაციის პარამეტრები.';
+$lang['configuration_backup_older_version_warn'] = 'ძველი ვერსიები არ შეიცავს app დააყენოთ მანიფესტი - გთხოვთ დააყენოთ საჭირო apps from ბაზარი შემდეგ აღდგენა';
+$lang['configuration_backup_release_mismatch'] = 'ვერსია ინფორმაციის სარეზერვო ფაილი არ ემთხვევა ვერსია დაყენებული ამ სისტემაში.';
+$lang['configuration_backup_release_missing'] = 'ვერსია არ არის ხელმისაწვდომი ინფორმაცია სარეზერვო ფაილი.';
+$lang['configuration_backup_release_unsupported:'] = 'აღდგენა შემდეგი ვერსია არ არის მხარდაჭერილი:';
+$lang['configuration_backup_restarting_service:'] = 'ხელმეორედ მომსახურება:';
+$lang['configuration_backup_restore_already_in_progress'] = 'აღდგენა მიმდინარეობს.';
+$lang['configuration_backup_restore_complete'] = 'აღდგენა დასრულდა.';
+$lang['configuration_backup_restore_file'] = 'არქივი';
+$lang['configuration_backup_restore_from_archive'] = 'აღდგენა არქივი';
+$lang['configuration_backup_restore_log'] = 'აღდგენა შესვლა';
+$lang['configuration_backup_restore_type:'] = 'აღდგენა ტიპი:';
+$lang['configuration_backup_starting_event_system'] = 'დასაწყისი ღონისძიება მონიტორის მომსახურება';
+$lang['configuration_backup_status_data_not_available'] = 'სტატუსი მონაცემები არ არის შესაძლებელი.';
+$lang['configuration_backup_stopping_event_system'] = 'შეჩერების ღონისძიების მონიტორის მომსახურება';
+$lang['configuration_backup_unable_to_start_restore'] = 'ვერ ხერხდება ოპერაციის აღდგენა';
+$lang['configuration_backup_unpacking_archive'] = 'პაკეტების არქივი';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'ველოდები პროგრამული განახლება სისტემა';
+$lang['configuration_backup_yum_busy'] = 'პაკეტი მენეჯერი არის დაკავებული.';
+
