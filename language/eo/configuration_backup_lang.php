93c75721a37f29369c8ff94b288d148b10ebba0c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:41:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'La agordo rezerva kaj restarigi app permesas administranto preni instantánea de ĉiuj agordadaj difinoj de la sistemo, permesante facilan restarigo en la okazaĵo de datuma perdo.';
+$lang['configuration_backup_app_name'] = 'Agordo Rezerva kaj Restarigi';
+$lang['configuration_backup_archives'] = 'arkivoj';
+$lang['configuration_backup_backup_file_invalid'] = 'Rezerva dosiernomo estas nevalida.';
+$lang['configuration_backup_backup_now'] = 'rezerva Nun';
+$lang['configuration_backup_checking_version'] = 'Kontrolanta arkivo versio kongruo';
+$lang['configuration_backup_clear'] = 'klara';
+$lang['configuration_backup_confirm_restore'] = 'Ĉu vi vere volas restarigi uzante la sekvan arkivon?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Dosierujo ne povis esti konvertitaj.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Elŝuti kaj instali apps. Bonvolu pacienci.';
+$lang['configuration_backup_file_information'] = 'dosiero Informo';
+$lang['configuration_backup_importing_ldap'] = 'Importante LDAP dosierujo';
+$lang['configuration_backup_network_information'] = 'reto Informoj';
+$lang['configuration_backup_not_registered'] = 'Via sistemo devas esti registrita antaŭ reestigado agordadaj difinoj.';
+$lang['configuration_backup_older_version_warn'] = 'Malnovaj versioj ne inkludas app instali evidentigxos - bonvolu instali postulataj apps de la Marketplace poste restarigi';
+$lang['configuration_backup_release_mismatch'] = 'La versio informo en la rezervan dosieron ne kongruas la versio instalita sur tiu sistemo.';
+$lang['configuration_backup_release_missing'] = 'La versio informo ne disponebla en la rezervan dosieron.';
+$lang['configuration_backup_release_unsupported:'] = 'Reestigado de la sekvanta versio ne estas subtenata:';
+$lang['configuration_backup_restarting_service:'] = 'Rekomencanta servo:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restarigi en progreso.';
+$lang['configuration_backup_restore_complete'] = 'Restarigi kompleta.';
+$lang['configuration_backup_restore_file'] = 'arkivo Dosiero';
+$lang['configuration_backup_restore_from_archive'] = 'Restarigi El Arkivo';
+$lang['configuration_backup_restore_log'] = 'restarigi Ensalutu';
+$lang['configuration_backup_restore_type:'] = 'Restarigi tipo:';
+$lang['configuration_backup_starting_event_system'] = 'Komencante okazaĵo monitoro servo';
+$lang['configuration_backup_status_data_not_available'] = 'Statuso datumoj ne havebla.';
+$lang['configuration_backup_stopping_event_system'] = 'Haltante okazaĵo monitoro servo';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nekapabla komenci restarigi operacio';
+$lang['configuration_backup_unpacking_archive'] = 'Elpakas arkivon';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Atendante programaro ĝisdatigoj sistemo';
+$lang['configuration_backup_yum_busy'] = 'Pako direktisto estas okupita.';
+
