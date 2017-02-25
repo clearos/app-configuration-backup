d7a67f4cedf29d674559a1f91cdac387821d2cbd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:42:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'backup nan konfigirasyon ak restore app pèmèt yon administratè pran yon snapshot de tout anviwònman konfigirasyon nan sistèm lan, sa ki pèmèt restorasyon fasil nan evènman an nan pèt done.';
+$lang['configuration_backup_app_name'] = 'Nou konte ofri sèvis sovgad ak restore';
+$lang['configuration_backup_archives'] = 'Achiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup fichier se valab.';
+$lang['configuration_backup_backup_now'] = 'backup Koulye a,';
+$lang['configuration_backup_checking_version'] = 'Lè nap Peye achiv vèsyon konpatibilite';
+$lang['configuration_backup_clear'] = 'Klè';
+$lang['configuration_backup_confirm_restore'] = 'Èske ou sèten ou vle retabli lè l sèvi avèk dosye a achiv vin apre a?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Anyè pa t &#39;ka konvèti.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Chaje ak enstale apps. Tanpri pran pasyans.';
+$lang['configuration_backup_file_information'] = 'Enfòmasyon file';
+$lang['configuration_backup_importing_ldap'] = 'Enpòte anyè ldap';
+$lang['configuration_backup_network_information'] = 'Information Network';
+$lang['configuration_backup_not_registered'] = 'dwe sistèm ou an ap anrejistre anvan restore anviwònman konfigirasyon.';
+$lang['configuration_backup_older_version_warn'] = 'Vèsyon ki pi ansyen pa gen ladan app enstale manifeste - tanpri enstale apps nesesè nan men Marketplace la apre retabli';
+$lang['configuration_backup_release_mismatch'] = 'Enfòmasyon ki vèsyon nan dosye a backup pa matche ak vèsyon an enstale sou sistèm sa a.';
+$lang['configuration_backup_release_missing'] = 'Enfòmasyon ki vèsyon se pa disponib nan dosye a backup.';
+$lang['configuration_backup_release_unsupported:'] = 'Restore soti nan vèsyon ki anba la a se pa sa sipòte:';
+$lang['configuration_backup_restarting_service:'] = 'Rekòmanse sèvis:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Renmèt nan pwogrè.';
+$lang['configuration_backup_restore_complete'] = 'Renmèt konplè.';
+$lang['configuration_backup_restore_file'] = 'achiv yo File';
+$lang['configuration_backup_restore_from_archive'] = 'Renmèt Soti nan Achiv yo';
+$lang['configuration_backup_restore_log'] = 'renmèt Log';
+$lang['configuration_backup_restore_type:'] = 'Renmèt tip:';
+$lang['configuration_backup_starting_event_system'] = 'Koumanse evènman sèvis pou kontwole';
+$lang['configuration_backup_status_data_not_available'] = 'done Ki dènye nouvèl pa disponib.';
+$lang['configuration_backup_stopping_event_system'] = 'Sispann evènman sèvis pou kontwole';
+$lang['configuration_backup_unable_to_start_restore'] = 'Kapab kòmanse retabli operasyon';
+$lang['configuration_backup_unpacking_archive'] = 'Debale dosye achiv';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Ap tann pou dènye lojisyèl sistèm';
+$lang['configuration_backup_yum_busy'] = 'manadjè pake se ki okipe yo.';
+
