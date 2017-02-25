098ee7d89cf332394d9c44377bdcdc4b0dfe821d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:44:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'コンフィギュレーションのバックアップとアプリを復元し、データの損失が発生した場合に簡単に復元を可能にする、システムのすべての構成設定のスナップショットを取得するには、管理者を許可します。';
+$lang['configuration_backup_app_name'] = '構成のバックアップと復元';
+$lang['configuration_backup_archives'] = 'アーカイブズ';
+$lang['configuration_backup_backup_file_invalid'] = 'バックアップファイル名は無効です。';
+$lang['configuration_backup_backup_now'] = '今すぐバックアップ';
+$lang['configuration_backup_checking_version'] = 'アーカイブバージョンの互換性の確認';
+$lang['configuration_backup_clear'] = 'クリア';
+$lang['configuration_backup_confirm_restore'] = 'あなたは、次のアーカイブファイルを使用して復元してもよろしいですか？';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ディレクトリは変換できませんでした。';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ダウンロードとアプリケーションをインストールします。 しばらくお待ちください。';
+$lang['configuration_backup_file_information'] = 'ファイル情報';
+$lang['configuration_backup_importing_ldap'] = 'LDAPディレクトリのインポート';
+$lang['configuration_backup_network_information'] = 'ネットワーク情報';
+$lang['configuration_backup_not_registered'] = 'お使いのシステムには、構成設定を復元する前に登録する必要があります。';
+$lang['configuration_backup_older_version_warn'] = '古いバージョンでは、インストールマニフェストのアプリが含まれていない - 復元後Marketplaceから必要なアプリケーションをインストールしてください。';
+$lang['configuration_backup_release_mismatch'] = 'バックアップファイル内のバージョン情報は、このシステムにインストールされているバージョンと一致していません。';
+$lang['configuration_backup_release_missing'] = 'バージョン情報は、バックアップファイルでは使用できません。';
+$lang['configuration_backup_release_unsupported:'] = '次のバージョンからの復元がサポートされていません。';
+$lang['configuration_backup_restarting_service:'] = 'サービスを再起動：';
+$lang['configuration_backup_restore_already_in_progress'] = '進行中で復元します。';
+$lang['configuration_backup_restore_complete'] = '完全に復元します。';
+$lang['configuration_backup_restore_file'] = '保管ファイル';
+$lang['configuration_backup_restore_from_archive'] = 'アーカイブからのリストア';
+$lang['configuration_backup_restore_log'] = 'ログを復元';
+$lang['configuration_backup_restore_type:'] = 'タイプを復元します。';
+$lang['configuration_backup_starting_event_system'] = 'イベント・モニター・サービスを開始';
+$lang['configuration_backup_status_data_not_available'] = 'ステータスデータは利用できません。';
+$lang['configuration_backup_stopping_event_system'] = 'イベント・モニター・サービスの停止';
+$lang['configuration_backup_unable_to_start_restore'] = '復元操作を開始することができません。';
+$lang['configuration_backup_unpacking_archive'] = 'アーカイブファイルの展開';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'ソフトウェアの更新システムを待っています';
+$lang['configuration_backup_yum_busy'] = 'パッケージマネージャがビジー状態です。';
+
