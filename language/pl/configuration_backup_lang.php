10c49b66dffe3c68dbd14112490f7eea280f5806
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:47:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Kopia zapasowa i przywracanie konfiguracji aplikacji pozwala administratorowi na wykonanie zrzutu wszystkich ustawień konfiguracyjnych systemu, umożliwiając łatwe przywracanie w przypadku utraty danych.';
+$lang['configuration_backup_app_name'] = 'Tworzenie kopii zapasowych i przywracanie konfiguracji';
+$lang['configuration_backup_archives'] = 'Archiwa';
+$lang['configuration_backup_backup_file_invalid'] = 'Nazwa pliku kopii zapasowej jest niepoprawny.';
+$lang['configuration_backup_backup_now'] = 'backup Now';
+$lang['configuration_backup_checking_version'] = 'Sprawdzanie zgodności wersji archiwum';
+$lang['configuration_backup_clear'] = 'Jasny';
+$lang['configuration_backup_confirm_restore'] = 'Czy na pewno chcesz przywrócić stosując następujący plik archiwum?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Katalog nie może zostać zamienione.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Pobieranie i instalowanie aplikacji. Proszę być cierpliwym.';
+$lang['configuration_backup_file_information'] = 'Informacje o pliku';
+$lang['configuration_backup_importing_ldap'] = 'Importowanie katalogu LDAP';
+$lang['configuration_backup_network_information'] = 'Sieć informacji';
+$lang['configuration_backup_not_registered'] = 'System musi być zarejestrowane przed przywróceniem ustawień konfiguracyjnych.';
+$lang['configuration_backup_older_version_warn'] = 'Starsze wersje nie zawierają aplikację manifestu instalacji - należy zainstalować wymagane aplikacje z Marketplace po przywróceniu';
+$lang['configuration_backup_release_mismatch'] = 'Informacje o wersji w pliku kopii zapasowej nie odpowiada zainstalowanej w systemie wersji.';
+$lang['configuration_backup_release_missing'] = 'Informacje o wersji nie jest dostępny w pliku kopii zapasowej.';
+$lang['configuration_backup_release_unsupported:'] = 'Przywracanie z poniższej wersji nie są obsługiwane:';
+$lang['configuration_backup_restarting_service:'] = 'Ponowne uruchomienie usługi:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Przywracanie w toku.';
+$lang['configuration_backup_restore_complete'] = 'Przywracanie zakończone.';
+$lang['configuration_backup_restore_file'] = 'Archiwum plików';
+$lang['configuration_backup_restore_from_archive'] = 'Przywróć z archiwum';
+$lang['configuration_backup_restore_log'] = 'Przywracanie Zaloguj';
+$lang['configuration_backup_restore_type:'] = 'typ przywracania:';
+$lang['configuration_backup_starting_event_system'] = 'Zaczynając service monitor zdarzeń';
+$lang['configuration_backup_status_data_not_available'] = 'Dane status nie jest dostępna.';
+$lang['configuration_backup_stopping_event_system'] = 'Zatrzymywanie usługi monitor zdarzeń';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nie można uruchomić operację przywracania';
+$lang['configuration_backup_unpacking_archive'] = 'Rozpakowanie pliku archiwum';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Oczekiwanie na systemie aktualizacje oprogramowania';
+$lang['configuration_backup_yum_busy'] = 'Zarządca pakietu jest zajęty.';
+
