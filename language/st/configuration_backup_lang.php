8e78fe15aa659387c68055c42a41b7e9d70864a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:49:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Bekapo phetolo le ho tsosolosa app e lumella mohlokomeli oa ho nka paloma la maemo ao &#39;ohle phetolo ea tsamaiso, e le hore tsosoloso bonolo ho ketsahalo ea tahlehelo ya data.';
+$lang['configuration_backup_app_name'] = 'Phetolo bekapo le Khutlisa';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Bekapo filename to ha o sebetse.';
+$lang['configuration_backup_backup_now'] = 'bekapo Joale';
+$lang['configuration_backup_checking_version'] = 'Hlahloba akhaefe phetolelo sebeletsana';
+$lang['configuration_backup_clear'] = 'e hlakileng';
+$lang['configuration_backup_confirm_restore'] = 'Na ehlile u batla ho tsosolosa sebelisa latelang akhaefe faele?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory ne o ke ke sokoloha.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Downloading le kenya ditiriso. Ka kōpo ba le mamello.';
+$lang['configuration_backup_file_information'] = 'File Information';
+$lang['configuration_backup_importing_ldap'] = 'Kenya LDAP dibaka';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'tsamaiso ea hao ea lokela ho ngolisoa ka pel&#39;a tsosolosa di-setting phetolo.';
+$lang['configuration_backup_older_version_warn'] = 'liphetolelo ba baholo ha kenyeletsa app e kenya bonahatsoa - ka kōpo kenya ditiriso hlokahala &#39;Marakeng ka mor&#39;a tsosolosa';
+$lang['configuration_backup_release_mismatch'] = 'Bakeng sa tlhahisoleseding phetolelo e mo file bekapo ha tšoanang le phetolelo hlomamisa tsamaiso ena.';
+$lang['configuration_backup_release_missing'] = 'Bakeng sa tlhahisoleseding phetolelo e sa fumanehe ka faele bekapo.';
+$lang['configuration_backup_release_unsupported:'] = 'Tsosolosa tsoang phetolelo latelang e sa tšehetsoa:';
+$lang['configuration_backup_restarting_service:'] = 'Restarting tšebeletso ea:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Tsosolosa tsoela pele.';
+$lang['configuration_backup_restore_complete'] = 'Tsosolosa ka ho feletseng.';
+$lang['configuration_backup_restore_file'] = 'File Archive';
+$lang['configuration_backup_restore_from_archive'] = 'Tsosolosa From Archive';
+$lang['configuration_backup_restore_log'] = 'tsosolosa Log';
+$lang['configuration_backup_restore_type:'] = 'Tsosolosa wa sebaka:';
+$lang['configuration_backup_starting_event_system'] = 'Ho qala ketsahalo tšebeletso ea lepiwa';
+$lang['configuration_backup_status_data_not_available'] = 'Boemo ya data ha a fumanehe.';
+$lang['configuration_backup_stopping_event_system'] = 'Emisa ketsahalo tšebeletso ea lepiwa';
+$lang['configuration_backup_unable_to_start_restore'] = 'Ha ho kgonehe ho qala tsosolosa tshebetsong';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking akhaefe faele';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Emetse diapdeite tsa software tsamaiso';
+$lang['configuration_backup_yum_busy'] = 'Sephutheloana manejara ke phathahane.';
+
