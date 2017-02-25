b3a1ae86c743a2dc9c2671cb961b1f4d08703cc8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:47:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'De configuratie back-up en herstel app kan een beheerder een momentopname van de configuratie-instellingen van het systeem te maken, waardoor u gemakkelijk het herstel in het geval van verlies van gegevens.';
+$lang['configuration_backup_app_name'] = 'Configuratie Backup en Restore';
+$lang['configuration_backup_archives'] = 'archief';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup bestandsnaam is ongeldig.';
+$lang['configuration_backup_backup_now'] = 'Nu backuppen';
+$lang['configuration_backup_checking_version'] = 'Controleren archief versiecompatibiliteit';
+$lang['configuration_backup_clear'] = 'Duidelijk';
+$lang['configuration_backup_confirm_restore'] = 'Weet u zeker dat u wilt herstellen met behulp van de volgende archiefbestand?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory kan niet worden geconverteerd.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Downloaden en installeren van apps. Wees alstublieft geduldig.';
+$lang['configuration_backup_file_information'] = 'bestand Informatie';
+$lang['configuration_backup_importing_ldap'] = 'Het importeren van LDAP-directory';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Uw systeem moet voor het herstellen van de configuratie-instellingen worden geregistreerd.';
+$lang['configuration_backup_older_version_warn'] = 'Oudere versies niet app te installeren, inclusief manifest - installeert u nodig apps uit de Marketplace na te herstellen';
+$lang['configuration_backup_release_mismatch'] = 'De versie-informatie in het back-upbestand niet overeenkomt met de versie op dit systeem geïnstalleerd.';
+$lang['configuration_backup_release_missing'] = 'De versie-informatie is niet beschikbaar in het back-upbestand.';
+$lang['configuration_backup_release_unsupported:'] = 'Het herstellen van de volgende versie wordt niet ondersteund:';
+$lang['configuration_backup_restarting_service:'] = 'Herstarten van service:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Herstel in progress.';
+$lang['configuration_backup_restore_complete'] = 'Terugzetten voltooid.';
+$lang['configuration_backup_restore_file'] = 'Archive File';
+$lang['configuration_backup_restore_from_archive'] = 'Terugzetten uit archief';
+$lang['configuration_backup_restore_log'] = 'Herstel Log';
+$lang['configuration_backup_restore_type:'] = 'Restore Type:';
+$lang['configuration_backup_starting_event_system'] = 'Vanaf eventmonitor dienst';
+$lang['configuration_backup_status_data_not_available'] = 'Status gegevens niet beschikbaar.';
+$lang['configuration_backup_stopping_event_system'] = 'Stoppen met eventmonitor dienst';
+$lang['configuration_backup_unable_to_start_restore'] = 'Niet in staat om te beginnen met het herstel van de werking';
+$lang['configuration_backup_unpacking_archive'] = 'Uitpakken archiefbestand';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Het wachten op software-updates systeem';
+$lang['configuration_backup_yum_busy'] = 'Package manager is bezet.';
+
