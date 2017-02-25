18dfdfca134a7bc0be5363f431471b721ac42a2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:48:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Backup-ul de configurare și de restaurare a aplicației permite unui administrator să ia un instantaneu al tuturor setărilor de configurare ale sistemului, permițând restaurarea ușoară în caz de pierdere de date.';
+$lang['configuration_backup_app_name'] = 'Backup și restaurare de configurare';
+$lang['configuration_backup_archives'] = 'Arhive';
+$lang['configuration_backup_backup_file_invalid'] = 'numele fișierului de rezervă este nevalid.';
+$lang['configuration_backup_backup_now'] = 'Rezervă acum';
+$lang['configuration_backup_checking_version'] = 'Verificare arhiva compatibilitate Versiune';
+$lang['configuration_backup_clear'] = 'clar';
+$lang['configuration_backup_confirm_restore'] = 'Sunteți sigur că doriți să restaurați folosind următorul fișier arhivă?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Director nu au putut fi convertite.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Descărcarea și instalarea aplicațiilor. Te rog fii rabdator.';
+$lang['configuration_backup_file_information'] = 'Informații despre fișier';
+$lang['configuration_backup_importing_ldap'] = 'Se importă director LDAP';
+$lang['configuration_backup_network_information'] = 'Informații de rețea';
+$lang['configuration_backup_not_registered'] = 'Sistemul dvs. trebuie să fie înregistrate înainte de a reveni la setările de configurare.';
+$lang['configuration_backup_older_version_warn'] = 'Versiunile mai vechi nu includ aplicația de instalare evidentă - vă rugăm să instalați aplicațiile necesare din Marketplace după restaura';
+$lang['configuration_backup_release_mismatch'] = 'Informații despre versiunea în fișierul copie de rezervă nu se potrivește cu versiunea instalată pe acest sistem.';
+$lang['configuration_backup_release_missing'] = 'Informațiile de versiune nu este disponibilă în fișierul copie de rezervă.';
+$lang['configuration_backup_release_unsupported:'] = 'Restaurarea din următoarea versiune nu este acceptată:';
+$lang['configuration_backup_restarting_service:'] = 'Serviciul repornind:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restabiliți în curs de desfășurare.';
+$lang['configuration_backup_restore_complete'] = 'Restabiliți complet.';
+$lang['configuration_backup_restore_file'] = 'Fișier arhiva';
+$lang['configuration_backup_restore_from_archive'] = 'De la Arhiva de restaurare';
+$lang['configuration_backup_restore_log'] = 'Jurnal de restaurare';
+$lang['configuration_backup_restore_type:'] = 'Tip de restaurare:';
+$lang['configuration_backup_starting_event_system'] = 'Serviciul de pornire monitor de evenimente';
+$lang['configuration_backup_status_data_not_available'] = 'datele de stare nu este disponibil.';
+$lang['configuration_backup_stopping_event_system'] = 'Serviciul de oprire monitor de evenimente';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nu se poate porni a restabili funcționarea';
+$lang['configuration_backup_unpacking_archive'] = 'fișier arhivă dezambalarea';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Se așteaptă sistem actualizări de software';
+$lang['configuration_backup_yum_busy'] = 'Pachetul de manager este ocupat.';
+
