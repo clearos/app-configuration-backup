eb171e38ba19bc62c5637786936bd8a9c76df130
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:41:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfigurationen backup og gendannelse app giver en administrator til at tage et snapshot af alle konfigurationsindstillinger af systemet, der giver nem restaurering i tilfælde af tab af data.';
+$lang['configuration_backup_app_name'] = 'Konfiguration sikkerhedskopiering og gendannelse';
+$lang['configuration_backup_archives'] = 'Arkiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup filnavn er ugyldigt.';
+$lang['configuration_backup_backup_now'] = 'Backup nu';
+$lang['configuration_backup_checking_version'] = 'Kontrol arkiv udgave kompatibilitet';
+$lang['configuration_backup_clear'] = 'Klar';
+$lang['configuration_backup_confirm_restore'] = 'Er du sikker på, du vil gendanne ved hjælp af følgende arkivfil?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Register kunne ikke konverteres.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Download og installation af apps. Vær tålmodig.';
+$lang['configuration_backup_file_information'] = 'File Information';
+$lang['configuration_backup_importing_ldap'] = 'Import af LDAP';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Systemet skal være registreret før du gendanner konfigurationsindstillinger.';
+$lang['configuration_backup_older_version_warn'] = 'Ældre versioner omfatter ikke appinstallation manifest - du installere nødvendige apps fra Marketplace efter gendannelse';
+$lang['configuration_backup_release_mismatch'] = 'Versionen oplysninger i backup-filen svarer ikke til den version, der er installeret på systemet.';
+$lang['configuration_backup_release_missing'] = 'Versionen information er ikke tilgængelig i backup-fil.';
+$lang['configuration_backup_release_unsupported:'] = 'Gendannelse fra den følgende udgave understøttes ikke:';
+$lang['configuration_backup_restarting_service:'] = 'Genstart service:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Gendan i gang.';
+$lang['configuration_backup_restore_complete'] = 'Gendan komplet.';
+$lang['configuration_backup_restore_file'] = 'arkivfil';
+$lang['configuration_backup_restore_from_archive'] = 'Gendan fra arkiv';
+$lang['configuration_backup_restore_log'] = 'Gendan Log';
+$lang['configuration_backup_restore_type:'] = 'Gendan seværdighed:';
+$lang['configuration_backup_starting_event_system'] = 'Start begivenhed monitor tjeneste';
+$lang['configuration_backup_status_data_not_available'] = 'Status-data ikke er tilgængelige.';
+$lang['configuration_backup_stopping_event_system'] = 'Stopper begivenhed monitor tjeneste';
+$lang['configuration_backup_unable_to_start_restore'] = 'Kan ikke starte gendannelsen';
+$lang['configuration_backup_unpacking_archive'] = 'Udpakning arkivfil';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Venter på softwareopdateringer systemet';
+$lang['configuration_backup_yum_busy'] = 'Package manager er optaget.';
+
