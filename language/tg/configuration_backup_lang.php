ee49fa73d385a99673aaf153287b4c69fd7d1ce6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:50:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Дар нусха танзимот ва барқарор барнома имкон медиҳад, бо як аз мудирон бигирад дарљи ҳамаи танзимоти насби системаи, имкон барқарорсозии осон дар ҳолати аз даст додани маълумот.';
+$lang['configuration_backup_app_name'] = 'Кадани Танзимот ва Барқарор';
+$lang['configuration_backup_archives'] = 'архивҳо';
+$lang['configuration_backup_backup_file_invalid'] = 'Номи файлҳо аз эҳтиётии нодуруст аст.';
+$lang['configuration_backup_backup_now'] = 'Акнун эҳтиётии';
+$lang['configuration_backup_checking_version'] = 'Тафтиши бойгонии мувофиқати нусхаи';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'Оё шумо боварӣ доред, ки барои барқарор кардани истифодаи файли бойгонии зерин?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory карда наметавонистанд табдил шавад.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Зеркашӣ ва насби Барномаҳои. Лутфан, сабр кун.';
+$lang['configuration_backup_file_information'] = 'Маълумот File';
+$lang['configuration_backup_importing_ldap'] = 'Воридоти феҳристи LDAP';
+$lang['configuration_backup_network_information'] = 'Маълумот Network';
+$lang['configuration_backup_not_registered'] = 'Системаи Шумо бояд пеш аз барқарор сохтани насби танзимоти ба қайд гирифта мешавад.';
+$lang['configuration_backup_older_version_warn'] = 'Версияҳои калонсол оё барнома дохил намешавад насб ошкор - марҳамат карда онро коргузорӣ Барномаҳои талаб аз бозор пас аз барқарор';
+$lang['configuration_backup_release_mismatch'] = 'Маълумоти версияи дар файли нусха тавр нусхаи насбшуда дар ин мувофиқ нест.';
+$lang['configuration_backup_release_missing'] = 'Маълумоти нусхаи аст, дар файли нусха дастрас нест.';
+$lang['configuration_backup_release_unsupported:'] = 'Барқарорсозии аз версияи зерин дастгирӣ карда намешавад:';
+$lang['configuration_backup_restarting_service:'] = 'Дароред хизматрасонӣ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Барқарорсозии дар пешрафти.';
+$lang['configuration_backup_restore_complete'] = 'Барқарор пурра.';
+$lang['configuration_backup_restore_file'] = 'Акс Бойгонӣ';
+$lang['configuration_backup_restore_from_archive'] = 'Барқарор Аз Бойгонӣ';
+$lang['configuration_backup_restore_log'] = 'барқарор Русский';
+$lang['configuration_backup_restore_type:'] = 'Барқарор намуди:';
+$lang['configuration_backup_starting_event_system'] = 'Оғози хизмати монитор чорабинии';
+$lang['configuration_backup_status_data_not_available'] = 'маълумот Статуси дастрас нест.';
+$lang['configuration_backup_stopping_event_system'] = 'Қатъ хадамоти монитор чорабинии';
+$lang['configuration_backup_unable_to_start_restore'] = 'Қодир ба оғоз барқарор амалиёти';
+$lang['configuration_backup_unpacking_archive'] = 'файли бойгонии борҷомаи';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Интизорӣ барои системаи навсозиҳои нармафзор';
+$lang['configuration_backup_yum_busy'] = 'мудири бастаи банд аст.';
+
