938c8ff222e261fcfb3886be7e2da6b61a7edae9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:38:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Backup konfigurimit dhe rivendosjen app lejon një administrator për të marrë një pasqyrë të të gjitha parametrat e konfigurimit të sistemit, duke lejuar kthimin e lehtë në rast të humbjes së të dhënave.';
+$lang['configuration_backup_app_name'] = 'Backup dhe Restore Konfigurimi';
+$lang['configuration_backup_archives'] = 'arkiv';
+$lang['configuration_backup_backup_file_invalid'] = 'filename Backup është e pavlefshme.';
+$lang['configuration_backup_backup_now'] = 'backup Tani';
+$lang['configuration_backup_checking_version'] = 'Kontrollimi version compatibility arkivor';
+$lang['configuration_backup_clear'] = 'Qartë';
+$lang['configuration_backup_confirm_restore'] = 'Jeni te sigurte qe doni te rivendosur duke përdorur të arkivit fotografi në vijim?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Lista nuk mund të konvertohet.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Shkarkimit dhe instalimit të aplikacioneve. Ju lutemi të jetë i durueshëm.';
+$lang['configuration_backup_file_information'] = 'File Information';
+$lang['configuration_backup_importing_ldap'] = 'Importimi directory LDAP';
+$lang['configuration_backup_network_information'] = 'Informacione të rrjetit';
+$lang['configuration_backup_not_registered'] = 'Sistemi juaj duhet të jetë i regjistruar para dhënies konfigurimeve.';
+$lang['configuration_backup_older_version_warn'] = 'Versionet e vjetra nuk përfshijnë app instaluar qartë - ju lutem instaloni aplikacionet e kërkuara nga Marketplace pas rivendosur';
+$lang['configuration_backup_release_mismatch'] = 'Informacioni version në file backup nuk përputhet me versionin e instaluar në këtë sistem.';
+$lang['configuration_backup_release_missing'] = 'Versioni informacioni nuk është në dispozicion në file backup.';
+$lang['configuration_backup_release_unsupported:'] = 'Rivendosja e nga versioni në vijim nuk është i mbështetur:';
+$lang['configuration_backup_restarting_service:'] = 'Rifillimi shërbimit:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restore në progres.';
+$lang['configuration_backup_restore_complete'] = 'Rikthe plotë.';
+$lang['configuration_backup_restore_file'] = 'Arkivi i dokumentit';
+$lang['configuration_backup_restore_from_archive'] = 'Rikthe Nga Arkivi';
+$lang['configuration_backup_restore_log'] = 'Rikthe Kyçu';
+$lang['configuration_backup_restore_type:'] = 'Rikthe lloji:';
+$lang['configuration_backup_starting_event_system'] = 'Duke filluar ngjarje shërbimit monitor';
+$lang['configuration_backup_status_data_not_available'] = 'të dhënat e statusit nuk është në dispozicion.';
+$lang['configuration_backup_stopping_event_system'] = 'Ndalimi ngjarje e shërbimit monitor';
+$lang['configuration_backup_unable_to_start_restore'] = 'Në pamundësi për të filluar të rivendosur funksionimin';
+$lang['configuration_backup_unpacking_archive'] = 'fotografi Arkivi Unpacking';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Duke pritur për sistemin përditësime software';
+$lang['configuration_backup_yum_busy'] = 'menaxher paketë është i zënë.';
+
