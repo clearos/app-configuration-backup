eb1b7bae495150f885cdf8162cc98eef513def98
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:50:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfigurationen säkerhetskopiering och återställning app kan en administratör att ta en ögonblicksbild av alla konfigurationsinställningar i systemet, vilket gör det enkelt restaurering i händelse av förlust av data.';
+$lang['configuration_backup_app_name'] = 'Konfiguration Säkerhetskopiering och återställning';
+$lang['configuration_backup_archives'] = 'Arkiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup filnamn är ogiltigt.';
+$lang['configuration_backup_backup_now'] = 'backup nu';
+$lang['configuration_backup_checking_version'] = 'Kontroll arkivversion kompatibilitet';
+$lang['configuration_backup_clear'] = 'Klar';
+$lang['configuration_backup_confirm_restore'] = 'Är du säker på att du vill återställa med hjälp av följande arkivfilen?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Katalog kunde inte konverteras.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Ladda ned och installera program. Var tålmodig.';
+$lang['configuration_backup_file_information'] = 'filinformation';
+$lang['configuration_backup_importing_ldap'] = 'Importera LDAP';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Systemet måste registreras innan du återkonfigurationsinställningar.';
+$lang['configuration_backup_older_version_warn'] = 'Äldre versioner inkluderar inte appinstallation manifest - installera nödvändiga program från Marketplace efter återställning';
+$lang['configuration_backup_release_mismatch'] = 'Versionsinformation i säkerhetskopian inte matchar version installerad på datorn.';
+$lang['configuration_backup_release_missing'] = 'Versionsinformationen inte finns i säkerhetskopian.';
+$lang['configuration_backup_release_unsupported:'] = 'Återställa från följande version stöds inte:';
+$lang['configuration_backup_restarting_service:'] = 'Omstart service:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Återställa pågår.';
+$lang['configuration_backup_restore_complete'] = 'Återställa komplett.';
+$lang['configuration_backup_restore_file'] = 'arkivfil';
+$lang['configuration_backup_restore_from_archive'] = 'Återställ från arkiv';
+$lang['configuration_backup_restore_log'] = 'återställa Log';
+$lang['configuration_backup_restore_type:'] = 'Återställa typ:';
+$lang['configuration_backup_starting_event_system'] = 'Starthändelseövervakare tjänst';
+$lang['configuration_backup_status_data_not_available'] = 'Statusdata inte tillgängliga.';
+$lang['configuration_backup_stopping_event_system'] = 'Stoppa händelseövervakare tjänst';
+$lang['configuration_backup_unable_to_start_restore'] = 'Det går inte att starta återställningen';
+$lang['configuration_backup_unpacking_archive'] = 'Uppackning arkivfil';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Väntar på programuppdateringar systemet';
+$lang['configuration_backup_yum_busy'] = 'Pakethanteraren är upptagen.';
+
