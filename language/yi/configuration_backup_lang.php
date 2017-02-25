ecfebf8dcd29dff4fc37c61f9a1388ad52cfbb41
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:52:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'די קאָנפיגוראַטיאָן באַקקופּ און ומקערן אַפּ אַלאַוז אַ אַדמיניסטראַטאָר צו נעמען אַ מאָמענטבילד פון אַלע קאָנפיגוראַטיאָן סעטטינגס פון די סיסטעם, אַלאַוינג גרינג רעסטעריישאַן אין די געשעעניש פון דאַטן אָנווער.';
+$lang['configuration_backup_app_name'] = 'קאָנפיגוראַטיאָן באַקקופּ און ומקערן';
+$lang['configuration_backup_archives'] = 'אַרטשיוועס';
+$lang['configuration_backup_backup_file_invalid'] = 'באַקקופּ פילענאַמע איז פאַרקריפּלט.';
+$lang['configuration_backup_backup_now'] = 'באַקקופּ איצט';
+$lang['configuration_backup_checking_version'] = 'קאָנטראָלירונג אַרקייוו ווערסיע קאַמפּאַטאַבילאַטי';
+$lang['configuration_backup_clear'] = 'קלאָר';
+$lang['configuration_backup_confirm_restore'] = 'ביסט איר זיכער איר ווילן צו ומקערן ניצן די ווייַטערדיק אַרקייוו טעקע?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'דירעקטארי קען נישט ווערן קאָנווערטעד.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'דאַונלאָודינג און ינסטאָלינג אַפּפּס. ביטע זיין פּאַציענט.';
+$lang['configuration_backup_file_information'] = 'טעקע אינפֿאָרמאַציע';
+$lang['configuration_backup_importing_ldap'] = 'ימפּאָרטינג לדאַפּ Directory';
+$lang['configuration_backup_network_information'] = 'נעטוואָרק אינפֿאָרמאַציע';
+$lang['configuration_backup_not_registered'] = 'דיין סיסטעם מוזן זיין רעגיסטרירט איידער ריסטאָרינג קאָנפיגוראַטיאָן סעטטינגס.';
+$lang['configuration_backup_older_version_warn'] = 'עלטערע ווערסיעס טאָן ניט אַרייַננעמען אַפּ ינסטאַלירן באַשייַמפּערלעך - ביטע ינסטאַלירן required אַפּס פון די מאַרקעטפּלאַסע נאָך ומקערן';
+$lang['configuration_backup_release_mismatch'] = 'די ווערסיע אינפֿאָרמאַציע אין די באַקאַפּ טעקע טוט ניט צופּאַסן די ווערסיע אינסטאַלירן אויף דעם סיסטעם.';
+$lang['configuration_backup_release_missing'] = 'די ווערסיע אינפֿאָרמאַציע איז ניט בנימצא אין די באַקאַפּ טעקע.';
+$lang['configuration_backup_release_unsupported:'] = 'רעסטאָרינג פון די ווייַטערדיק ווערסיע איז ניט געשטיצט:';
+$lang['configuration_backup_restarting_service:'] = 'ריסטאַרטינג דינסט:';
+$lang['configuration_backup_restore_already_in_progress'] = 'ומקערן אין פּראָגרעס.';
+$lang['configuration_backup_restore_complete'] = 'ריסטאָר גאַנץ.';
+$lang['configuration_backup_restore_file'] = 'אַרטשיווע טעקע';
+$lang['configuration_backup_restore_from_archive'] = 'ומקערן פֿון אַרטשיווע';
+$lang['configuration_backup_restore_log'] = 'ומקערן קלאָץ';
+$lang['configuration_backup_restore_type:'] = 'ומקערן טיפּ:';
+$lang['configuration_backup_starting_event_system'] = 'סטאַרטינג געשעעניש מאָניטאָר דינסט';
+$lang['configuration_backup_status_data_not_available'] = 'סטאַטוס דאַטן ניט בנימצא.';
+$lang['configuration_backup_stopping_event_system'] = 'סטאָפּפּינג געשעעניש מאָניטאָר דינסט';
+$lang['configuration_backup_unable_to_start_restore'] = 'געקענט צו אָנהייבן ומקערן אָפּעראַציע';
+$lang['configuration_backup_unpacking_archive'] = 'אַנפּאַקינג אַרקייוו טעקע';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'ווארטן פֿאַר ווייכווארג ופּדאַטעס סיסטעם';
+$lang['configuration_backup_yum_busy'] = 'פּעקל פאַרוואַלטער איז פאַרנומען.';
+
