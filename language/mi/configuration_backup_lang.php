67bc86f1bdf5f198a175c5c5d7aa7e02ec04fee0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:45:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Te tārua whirihoranga, ka whakahoki taupānga taea te kaiwhakahaere ki te tango i te hopuāhua o tautuhinga whirihoranga katoa o te pūnaha, tuku ngāwari toe fakafoki i te hui o te ngaronga raraunga.';
+$lang['configuration_backup_app_name'] = 'Pūrua whirihoranga me Whakaora';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'He muhu ingoakōnae Pūrua.';
+$lang['configuration_backup_backup_now'] = 'Na Pūrua';
+$lang['configuration_backup_checking_version'] = 'Taki ana hototahi putanga pūranga';
+$lang['configuration_backup_clear'] = 'Ūkui';
+$lang['configuration_backup_confirm_restore'] = 'Kei te tino hiahia koe ki te whakahokia mai te whakamahi i te kōnae pūranga e whai ake nei?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'e kore i taea te tahuri Directory.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Te tikiake me te tāuta taupānga. Koa kia manawanui.';
+$lang['configuration_backup_file_information'] = 'Mōhiohio kōnae';
+$lang['configuration_backup_importing_ldap'] = 'Kawemai whaiaronga LDAP';
+$lang['configuration_backup_network_information'] = 'Mōhiohio whatunga';
+$lang['configuration_backup_not_registered'] = 'Me rēhita koutou pūnaha i mua i te whakaora i ngā tautuhinga whirihoranga.';
+$lang['configuration_backup_older_version_warn'] = 'e kore e putanga tawhito ngā taupānga tāuta kitea - tēnā tāuta taupānga hiahiatia i te Mākete muri whakahokia';
+$lang['configuration_backup_release_mismatch'] = 'e kore e te mōhiohio putanga i roto i te kōnae pūrua ōrite te putanga i tēnei pūnaha.';
+$lang['configuration_backup_release_missing'] = 'kahore he wātea i roto i te kōnae pūrua te mōhiohio putanga.';
+$lang['configuration_backup_release_unsupported:'] = 'e kore te whakaora i i te putanga e whai ake nei tautokona:';
+$lang['configuration_backup_restarting_service:'] = 'Tīmata anō mahi:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Whakaora i roto i te ahunga whakamua.';
+$lang['configuration_backup_restore_complete'] = 'Whakaora oti.';
+$lang['configuration_backup_restore_file'] = 'Kōnae pūranga';
+$lang['configuration_backup_restore_from_archive'] = 'Whakaora Mai Archive';
+$lang['configuration_backup_restore_log'] = 'Whakaora Rangitaki';
+$lang['configuration_backup_restore_type:'] = 'Whakaora momo:';
+$lang['configuration_backup_starting_event_system'] = 'Tīmata ratonga aroturuki takahanga';
+$lang['configuration_backup_status_data_not_available'] = 'raraunga Tūnga kore e wātea ana.';
+$lang['configuration_backup_stopping_event_system'] = 'Tu mahi aroturuki takahanga';
+$lang['configuration_backup_unable_to_start_restore'] = 'Kāore e taea te tīmata whakahokia mahi';
+$lang['configuration_backup_unpacking_archive'] = 'kōnae pūranga Wetewete';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Tatari hoki te pūnaha whakahōunga pūmanawa';
+$lang['configuration_backup_yum_busy'] = 'He pukumahi kaiwhakahaere Mōkī.';
+
