43dbdcaacc01fb6776ed584d2716198b53490947
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:40:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = '配置备份和恢复应用程序允许管理员利用系统的所有配置设置的快照，使数据丢失的情况下，容易恢复。';
+$lang['configuration_backup_app_name'] = '配置备份和恢复';
+$lang['configuration_backup_archives'] = '档案';
+$lang['configuration_backup_backup_file_invalid'] = '备份文件名无效。';
+$lang['configuration_backup_backup_now'] = '立即备份';
+$lang['configuration_backup_checking_version'] = '检查归档版本的兼容性';
+$lang['configuration_backup_clear'] = '明确';
+$lang['configuration_backup_confirm_restore'] = '你确定你想用下面的归档文件恢复？';
+$lang['configuration_backup_directory_could_not_be_converted'] = '目录无法转换。';
+$lang['configuration_backup_download_and_intstall_apps'] = '下载和安装应用程序。 请耐心等待。';
+$lang['configuration_backup_file_information'] = '文件信息';
+$lang['configuration_backup_importing_ldap'] = '导入LDAP目录';
+$lang['configuration_backup_network_information'] = '网络信息';
+$lang['configuration_backup_not_registered'] = '您的系统必须恢复配置设置之前进行登记。';
+$lang['configuration_backup_older_version_warn'] = '旧版本不包含应用程序安装清单 - 请从Marketplace安装所需的应用程序后恢复';
+$lang['configuration_backup_release_mismatch'] = '在备份文件中的版本信息不匹配该系统上安装的版本。';
+$lang['configuration_backup_release_missing'] = '版本信息是不是在备份文件中。';
+$lang['configuration_backup_release_unsupported:'] = '从以下版本恢复不支持：';
+$lang['configuration_backup_restarting_service:'] = '重新启动服务：';
+$lang['configuration_backup_restore_already_in_progress'] = '正在进行恢复。';
+$lang['configuration_backup_restore_complete'] = '恢复完成。';
+$lang['configuration_backup_restore_file'] = '存档文件';
+$lang['configuration_backup_restore_from_archive'] = '从档案中恢复';
+$lang['configuration_backup_restore_log'] = '恢复日志';
+$lang['configuration_backup_restore_type:'] = '恢复类型：';
+$lang['configuration_backup_starting_event_system'] = '启动事件监视器服务';
+$lang['configuration_backup_status_data_not_available'] = '不可用状态数据。';
+$lang['configuration_backup_stopping_event_system'] = '停止事件监视器服务';
+$lang['configuration_backup_unable_to_start_restore'] = '无法启动恢复操作';
+$lang['configuration_backup_unpacking_archive'] = '开箱存档文件';
+$lang['configuration_backup_waiting_for_software_updates_system'] = '等待软件更新系统';
+$lang['configuration_backup_yum_busy'] = '软件包管理器是忙。';
+
