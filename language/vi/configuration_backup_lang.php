be21d077358b6514ca15fc530c6c97fdb5d32d95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:51:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Các sao lưu cấu hình và khôi phục ứng dụng cho phép người quản trị để có một bản chụp của tất cả các thiết lập cấu hình của hệ thống, cho phép khôi phục dễ dàng trong trường hợp mất dữ liệu.';
+$lang['configuration_backup_app_name'] = 'Cấu hình sao lưu và phục hồi';
+$lang['configuration_backup_archives'] = 'văn khố';
+$lang['configuration_backup_backup_file_invalid'] = 'tên tập tin sao lưu là không hợp lệ.';
+$lang['configuration_backup_backup_now'] = 'backup Now';
+$lang['configuration_backup_checking_version'] = 'Kiểm tra khả năng tương thích phiên bản lưu trữ';
+$lang['configuration_backup_clear'] = 'Trong sáng';
+$lang['configuration_backup_confirm_restore'] = 'Bạn có chắc chắn bạn muốn khôi phục bằng cách sử dụng tập tin lưu trữ sau đây?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Thư mục không thể được chuyển đổi.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Tải về và cài đặt ứng dụng. Làm ơn hãy kiên nhẫn.';
+$lang['configuration_backup_file_information'] = 'Thông tin file';
+$lang['configuration_backup_importing_ldap'] = 'Nhập thư mục LDAP';
+$lang['configuration_backup_network_information'] = 'mạng thông tin';
+$lang['configuration_backup_not_registered'] = 'Hệ thống của bạn phải được đăng ký trước khi khôi phục các thiết lập cấu hình.';
+$lang['configuration_backup_older_version_warn'] = 'Các phiên bản cũ không bao gồm các ứng dụng cài đặt manifest - hãy cài đặt các ứng dụng cần thiết từ Marketplace sau khi khôi phục';
+$lang['configuration_backup_release_mismatch'] = 'Các thông tin phiên bản trong các tập tin sao lưu không phù hợp với phiên bản cài đặt trên hệ thống này.';
+$lang['configuration_backup_release_missing'] = 'Thông tin phiên bản là không có sẵn trong các tập tin sao lưu.';
+$lang['configuration_backup_release_unsupported:'] = 'Khôi phục lại từ các phiên bản sau đây không được hỗ trợ:';
+$lang['configuration_backup_restarting_service:'] = 'dịch vụ khởi động lại:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restore trong tiến trình.';
+$lang['configuration_backup_restore_complete'] = 'Khôi phục hoàn toàn.';
+$lang['configuration_backup_restore_file'] = 'lưu trữ file';
+$lang['configuration_backup_restore_from_archive'] = 'Khôi phục từ Lưu Trữ';
+$lang['configuration_backup_restore_log'] = 'Khôi phục Log';
+$lang['configuration_backup_restore_type:'] = 'Khôi phục loại:';
+$lang['configuration_backup_starting_event_system'] = 'Bắt đầu dịch vụ theo dõi sự kiện';
+$lang['configuration_backup_status_data_not_available'] = 'dữ liệu tình trạng không có sẵn.';
+$lang['configuration_backup_stopping_event_system'] = 'Dừng dịch vụ theo dõi sự kiện';
+$lang['configuration_backup_unable_to_start_restore'] = 'Không thể bắt đầu phục hồi hoạt động';
+$lang['configuration_backup_unpacking_archive'] = 'giải nén tập tin lưu trữ';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Đang đợi hệ thống cập nhật phần mềm';
+$lang['configuration_backup_yum_busy'] = 'quản lý gói là bận rộn.';
+
