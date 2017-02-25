1d50418a8fe717a2f16c0b61a1c26bd92cd78b45
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:39:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfiguracija backup i vraćanje aplikacija omogućava administratoru da snimili sve postavke konfiguracije sistema, omogućava lako obnove u slučaju gubitka podataka.';
+$lang['configuration_backup_app_name'] = 'Konfiguracija Backup and Restore';
+$lang['configuration_backup_archives'] = 'arhiva';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup filename je nevažeća.';
+$lang['configuration_backup_backup_now'] = 'backup Now';
+$lang['configuration_backup_checking_version'] = 'Provjera arhiva verzija kompatibilnost';
+$lang['configuration_backup_clear'] = 'jasno';
+$lang['configuration_backup_confirm_restore'] = 'Jeste li sigurni da želite vratiti koristeći sljedeće arhivu?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Imenik se ne može pretvoriti.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Preuzimanje i instaliranje aplikacija. Molimo budite strpljivi.';
+$lang['configuration_backup_file_information'] = 'Informacije o datoteci';
+$lang['configuration_backup_importing_ldap'] = 'Uvoz LDAP direktorija';
+$lang['configuration_backup_network_information'] = 'mrežnih informacija';
+$lang['configuration_backup_not_registered'] = 'Vaš sistem mora biti prijavljen prije vraćanja postavke konfiguracije.';
+$lang['configuration_backup_older_version_warn'] = 'Starije verzije ne uključuju aplikaciju instalirati manifest - instalirajte potrebne aplikacije iz Marketplace nakon vraćanja';
+$lang['configuration_backup_release_mismatch'] = 'Informacije o verziji u datoteci backup ne odgovara verzija instalirana na ovom sistemu.';
+$lang['configuration_backup_release_missing'] = 'Informacije o verzija nije dostupna u datoteci backup.';
+$lang['configuration_backup_release_unsupported:'] = 'Vraćanje iz sledećih verzija nije podržana:';
+$lang['configuration_backup_restarting_service:'] = 'Ponovno pokretanje usluga:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Vraćanje u toku.';
+$lang['configuration_backup_restore_complete'] = 'Vraćanje potpuna.';
+$lang['configuration_backup_restore_file'] = 'Arhiva File';
+$lang['configuration_backup_restore_from_archive'] = 'Vrati iz arhive';
+$lang['configuration_backup_restore_log'] = 'Vrati se Prijavite';
+$lang['configuration_backup_restore_type:'] = 'Restore tip:';
+$lang['configuration_backup_starting_event_system'] = 'Pokretanje usluga monitor događaj';
+$lang['configuration_backup_status_data_not_available'] = 'Status podaci nisu dostupni.';
+$lang['configuration_backup_stopping_event_system'] = 'Zaustavljanje usluga monitor događaj';
+$lang['configuration_backup_unable_to_start_restore'] = 'Može da se pokrene operaciju vraćanja';
+$lang['configuration_backup_unpacking_archive'] = 'Raspakivanje arhivi';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Čekajući ažuriranja softvera sistema';
+$lang['configuration_backup_yum_busy'] = 'Manager Paket je zauzet.';
+
