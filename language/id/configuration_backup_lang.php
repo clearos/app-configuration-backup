9e8541328ec46b2dad81a684a8c65e736aec8d32
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:43:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'The backup konfigurasi dan memulihkan aplikasi memungkinkan administrator untuk mengambil snapshot dari semua pengaturan konfigurasi sistem, yang memungkinkan pemulihan mudah dalam hal kehilangan data.';
+$lang['configuration_backup_app_name'] = 'Konfigurasi Backup dan Restore';
+$lang['configuration_backup_archives'] = 'Arsip';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup file tidak valid.';
+$lang['configuration_backup_backup_now'] = 'backup Sekarang';
+$lang['configuration_backup_checking_version'] = 'Memeriksa kompatibilitas versi arsip';
+$lang['configuration_backup_clear'] = 'Bersih';
+$lang['configuration_backup_confirm_restore'] = 'Apakah Anda yakin Anda ingin mengembalikan menggunakan file arsip berikut?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Direktori tidak dapat diubah.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Mendownload dan menginstal aplikasi. Harap bersabar.';
+$lang['configuration_backup_file_information'] = 'Informasi File';
+$lang['configuration_backup_importing_ldap'] = 'Mengimpor direktori LDAP';
+$lang['configuration_backup_network_information'] = 'jaringan Informasi';
+$lang['configuration_backup_not_registered'] = 'Sistem Anda harus terdaftar sebelum mengembalikan pengaturan konfigurasi.';
+$lang['configuration_backup_older_version_warn'] = 'versi lama tidak termasuk pemasangan aplikasi manifes - silahkan install aplikasi yang dibutuhkan dari Marketplace setelah mengembalikan';
+$lang['configuration_backup_release_mismatch'] = 'Informasi versi di file backup tidak cocok dengan versi diinstal pada sistem ini.';
+$lang['configuration_backup_release_missing'] = 'Informasi versi tidak tersedia dalam file backup.';
+$lang['configuration_backup_release_unsupported:'] = 'Memulihkan dari versi berikut tidak didukung:';
+$lang['configuration_backup_restarting_service:'] = 'Restart layanan:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Kembalikan berlangsung.';
+$lang['configuration_backup_restore_complete'] = 'Kembalikan lengkap.';
+$lang['configuration_backup_restore_file'] = 'Arsip Berkas';
+$lang['configuration_backup_restore_from_archive'] = 'Kembalikan Dari Arsip';
+$lang['configuration_backup_restore_log'] = 'Kembalikan Log';
+$lang['configuration_backup_restore_type:'] = 'Kembalikan Jenis:';
+$lang['configuration_backup_starting_event_system'] = 'Mulai layanan memantau acara';
+$lang['configuration_backup_status_data_not_available'] = 'data status tidak tersedia.';
+$lang['configuration_backup_stopping_event_system'] = 'Menghentikan layanan memantau acara';
+$lang['configuration_backup_unable_to_start_restore'] = 'Tidak dapat memulai operasi pemulihan';
+$lang['configuration_backup_unpacking_archive'] = 'file arsip unpacking';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Menunggu sistem pembaruan perangkat lunak';
+$lang['configuration_backup_yum_busy'] = 'Manajer paket sibuk.';
+
