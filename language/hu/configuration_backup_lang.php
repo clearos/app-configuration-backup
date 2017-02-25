b804fbabdb44298ea1a02a6ac4cdb41ed61cdc49
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:43:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'A konfiguráció biztonsági mentés és visszaállítás alkalmazás lehetővé teszi, hogy a rendszergazda pillanatfelvételt készíteni minden beállítás a rendszer, amely lehetővé teszi könnyű helyreállítás az adatvesztés esetén.';
+$lang['configuration_backup_app_name'] = 'Konfiguráció biztonsági mentés és visszaállítás';
+$lang['configuration_backup_archives'] = 'Levéltár';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup file érvénytelen.';
+$lang['configuration_backup_backup_now'] = 'Backup Now';
+$lang['configuration_backup_checking_version'] = 'Ellenőrzés archív verzió kompatibilitás';
+$lang['configuration_backup_clear'] = 'Egyértelmű';
+$lang['configuration_backup_confirm_restore'] = 'Biztos benne, hogy vissza akarja állítani az alábbi archív fájlt?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory nem alakítható.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Letöltése és telepítése alkalmazások. Kérjük várjon.';
+$lang['configuration_backup_file_information'] = 'fájl információ';
+$lang['configuration_backup_importing_ldap'] = 'Importálása LDAP címtár';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'A rendszer regisztrálni kell, mielőtt helyreállítása konfigurációs beállításokat.';
+$lang['configuration_backup_older_version_warn'] = 'A régebbi verziók nem tartalmazzák alkalmazástelepítési nyilvánvaló - kérjük telepítse szükséges alkalmazásokat a Marketplace után visszaállítani';
+$lang['configuration_backup_release_mismatch'] = 'A verzió információ a biztonsági mentés nem egyezik a verzió van telepítve a rendszeren.';
+$lang['configuration_backup_release_missing'] = 'A verzió információ nem áll rendelkezésre a biztonsági mentés.';
+$lang['configuration_backup_release_unsupported:'] = 'Kilépés az alábbi verzió nem támogatott:';
+$lang['configuration_backup_restarting_service:'] = 'Újraindítása szolgáltatás:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Visszaállítás folyamatban.';
+$lang['configuration_backup_restore_complete'] = 'A visszaállítás befejeződött.';
+$lang['configuration_backup_restore_file'] = 'Archív fájl';
+$lang['configuration_backup_restore_from_archive'] = 'Visszaállítás Archívum';
+$lang['configuration_backup_restore_log'] = 'Restore Belépés';
+$lang['configuration_backup_restore_type:'] = 'Visszaállítás típusa:';
+$lang['configuration_backup_starting_event_system'] = 'Kezdve eseménymegfigyelő szolgáltatás';
+$lang['configuration_backup_status_data_not_available'] = 'Állapot adatok nem állnak rendelkezésre.';
+$lang['configuration_backup_stopping_event_system'] = 'Leállítása esetén monitor szerviz';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nem sikerült elindítani a visszaállítási művelet';
+$lang['configuration_backup_unpacking_archive'] = 'Kicsomagolás archív fájl';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Várakozás szoftverfrissítések rendszer';
+$lang['configuration_backup_yum_busy'] = 'Csomagkezelő foglalt.';
+
