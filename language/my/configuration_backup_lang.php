3b3d9f9f492d551d8b05dab5dadf8bc7249d805e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:40:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'အဆိုပါ configuration ကို backup လုပ်ထားနဲ့ app restore data ကိုဆုံးရှုံးမှုဖြစ်ရပ်အတွက်လွယ်ကူသောပွနျလညျထူထောငျခွင့်စနစ်အားလုံးစီမံပြုပြင်မှု setting များ၏လျှပ်တစ်ပြက်ယူမှတစ်ဦးအုပ်ချုပ်ရေးမှူးခွင့်ပြုပါတယ်။';
+$lang['configuration_backup_app_name'] = 'configuration Backup ကိုနှင့် Restore';
+$lang['configuration_backup_archives'] = 'စာရွက်စာတမ်းဟောင်းများ';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup ကိုဖိုင်အမည်မမှန်ကန်ဘူး။';
+$lang['configuration_backup_backup_now'] = 'အခုတော့ Backup ကို';
+$lang['configuration_backup_checking_version'] = 'archive ကိုဗားရှင်း Compatibility ကိုစစ်ဆေးခြင်း';
+$lang['configuration_backup_clear'] = 'ရှင်းလင်းသော';
+$lang['configuration_backup_confirm_restore'] = 'သငျသညျအောကျပါ archive ကိုဖိုင်ကိုသုံးပြီး restore ချင်သေချာလား?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory ကိုကူးပြောင်းလို့မရပါ။';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ဒေါင်းလုဒ်လုပ်ခြင်းနှင့် app များကို install လုပ်ပါ။ လူနာဖြစ်ပါ။';
+$lang['configuration_backup_file_information'] = 'ပြန်ကြားရေး file';
+$lang['configuration_backup_importing_ldap'] = 'LDAP directory ကိုတင်သွင်းခြင်း';
+$lang['configuration_backup_network_information'] = 'Network မှပြန်ကြားရေး';
+$lang['configuration_backup_not_registered'] = 'သင်၏ system configuration က settings ကို restore ပြန်မီကမှတ်ပုံတင်ထားရပါမည်။';
+$lang['configuration_backup_older_version_warn'] = 'အဟောင်းတွေဗားရှင်း app ကိုကိုယ်ကိုကိုယ်ထင်ရှားစွာပြ install လုပ်မပါဝင်ကြဘူး - restore ပြီးနောက် Marketplace မှလိုအပ်တဲ့ app များကို install ကျေးဇူးပြုပြီး';
+$lang['configuration_backup_release_mismatch'] = 'အ backup ဖိုင်ကိုအတွက်ဗားရှင်းသတင်းအချက်အလက်ဒီစနစ်အပေါ် install လုပ် version ကိုမတိုက်ဆိုင်ပါဘူး။';
+$lang['configuration_backup_release_missing'] = 'အဆိုပါဗားရှင်းသတင်းအချက်အလက်များ backup ဖိုင်ကိုရရှိနိုင်သည်မဟုတ်။';
+$lang['configuration_backup_release_unsupported:'] = 'အောက်ပါ version ကိုမှပြန်ယူပါမထောက်ပံ့ထားပါ:';
+$lang['configuration_backup_restarting_service:'] = 'ကိုပြန်လည်စတင်ဝန်ဆောင်မှု:';
+$lang['configuration_backup_restore_already_in_progress'] = 'တိုးတက်မှုအတွက် Restore ။';
+$lang['configuration_backup_restore_complete'] = 'ပြီးပြည့်စုံသော Restore ။';
+$lang['configuration_backup_restore_file'] = 'archive ဖိုင်';
+$lang['configuration_backup_restore_from_archive'] = 'မော်ကွန်းတိုက် မှစ. Restore';
+$lang['configuration_backup_restore_log'] = 'Log in ဝင်ရန် Restore';
+$lang['configuration_backup_restore_type:'] = 'အမျိုးအစား Restore:';
+$lang['configuration_backup_starting_event_system'] = 'အဖြစ်အပျက်မော်နီတာဝန်ဆောင်မှုစတင်ခြင်း';
+$lang['configuration_backup_status_data_not_available'] = 'Status ကိုဒေတာမရရှိနိုင်ပါ။';
+$lang['configuration_backup_stopping_event_system'] = 'အဖြစ်အပျက်မော်နီတာဝန်ဆောင်မှုရပ်တန့်';
+$lang['configuration_backup_unable_to_start_restore'] = 'စစ်ဆင်ရေး restore စတင်နိုင်ခြင်း';
+$lang['configuration_backup_unpacking_archive'] = 'archive ကိုဖိုင်ကိုဖြည်';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'ဆော့ဖျဝဲ updates တွေကို system အတွက်စောင့်ဆိုင်းနေ';
+$lang['configuration_backup_yum_busy'] = 'package manager အလုပ်များဖြစ်ပါသည်။';
+
