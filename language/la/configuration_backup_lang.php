309498000ba9de0591cc75570ac33edaefb89098
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:45:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Quod restituo Configuratio app sino administratorem omnium Snapshot optionum conformationis uidere possunt accipere ratio patitur facile notitia eventus damni restitutionem.';
+$lang['configuration_backup_app_name'] = 'Configuration Tergum quod Restituo';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Filename Tergum est invalidum.';
+$lang['configuration_backup_backup_now'] = 'Nunc tergum';
+$lang['configuration_backup_checking_version'] = 'Poema poematis compatibility reprehendo Archive';
+$lang['configuration_backup_clear'] = 'Aequaliter Nubila';
+$lang['configuration_backup_confirm_restore'] = 'Certus vos es usura sequens reficiendum File?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory non convertitur.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Downloading et installing apps. Placere patientes estote.';
+$lang['configuration_backup_file_information'] = 'file Information';
+$lang['configuration_backup_importing_ldap'] = 'Importing LDAP Directory';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Ad restituendum esse profiteretur reponetis optionum conformationis uidere possunt.';
+$lang['configuration_backup_older_version_warn'] = 'Versions app videre non includit manifestam install - sis oportet apps install a foro et restituet';
+$lang['configuration_backup_release_mismatch'] = 'Versio Latina non convenienter ponitur notitia in tergum lima installed ratio.';
+$lang['configuration_backup_release_missing'] = 'Notitia haberi nequeat versio in tergum lima.';
+$lang['configuration_backup_release_unsupported:'] = 'Ex his non continetur versio restitueret:';
+$lang['configuration_backup_restarting_service:'] = 'Restarting ministerium:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Redde in progressu.';
+$lang['configuration_backup_restore_complete'] = 'Totum redde.';
+$lang['configuration_backup_restore_file'] = 'Video File:';
+$lang['configuration_backup_restore_from_archive'] = 'Redde from archive';
+$lang['configuration_backup_restore_log'] = 'Log redde';
+$lang['configuration_backup_restore_type:'] = 'Redde Type:';
+$lang['configuration_backup_starting_event_system'] = 'Monitor satus utriusque muneris';
+$lang['configuration_backup_status_data_not_available'] = 'Status notitia available.';
+$lang['configuration_backup_stopping_event_system'] = 'Restitit monitor res ministerium';
+$lang['configuration_backup_unable_to_start_restore'] = 'Restituere non potest committitur ad operari';
+$lang['configuration_backup_unpacking_archive'] = 'File Unpacking';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Expectantes updates software system';
+$lang['configuration_backup_yum_busy'] = 'Sarcina sit amet diam.';
+
