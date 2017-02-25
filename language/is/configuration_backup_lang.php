1194bbb4562352e426499b9485388a2b3a26f9e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:43:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'The stillingar varabúnaður og skila aftur app leyfa stjórnandi til að taka mynd af öllum stillingum kerfisins, leyfa þægilegur endurreisn í the atburður af gögn tap.';
+$lang['configuration_backup_app_name'] = 'Stillingar Backup og Restore';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Afritun er ógilt.';
+$lang['configuration_backup_backup_now'] = 'Backup Now';
+$lang['configuration_backup_checking_version'] = 'Athugun skjalasafn útgáfa eindrægni';
+$lang['configuration_backup_clear'] = 'Hreinsa';
+$lang['configuration_backup_confirm_restore'] = 'Ertu viss um að þú viljir endurheimta með eftirfarandi safnskrá?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Skrá getur ekki verið breytt.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Sækja og setja upp forrit. Vinsamlegast vertu þolinmóður.';
+$lang['configuration_backup_file_information'] = 'File Upplýsingar';
+$lang['configuration_backup_importing_ldap'] = 'Flyt LDAP skrá';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Kerfið verður að vera skráð áður endurheimta stillingar.';
+$lang['configuration_backup_older_version_warn'] = 'Eldri útgáfur innihalda ekki app setja augljóst - vinsamlegast setja nauðsynleg forrit frá Marketplace eftir endurheimt';
+$lang['configuration_backup_release_mismatch'] = 'The útgáfa upplýsingar í the varabúnaður skrá passar ekki við útgáfu uppsetta á þessu kerfi.';
+$lang['configuration_backup_release_missing'] = 'The útgáfa er ekki í boði í the varabúnaður skrá.';
+$lang['configuration_backup_release_unsupported:'] = 'Endurheimta frá eftirfarandi útgáfu er ekki studd:';
+$lang['configuration_backup_restarting_service:'] = 'Ræsa á ný þjónusta:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Endurheimta í gangi.';
+$lang['configuration_backup_restore_complete'] = 'Endurheimta lokið.';
+$lang['configuration_backup_restore_file'] = 'safnskrá';
+$lang['configuration_backup_restore_from_archive'] = 'Endurheimta Frá Archive';
+$lang['configuration_backup_restore_log'] = 'endurheimta Innskráning';
+$lang['configuration_backup_restore_type:'] = 'Endurheimta ferðar:';
+$lang['configuration_backup_starting_event_system'] = 'Byrjar atburður fylgjast þjónusta';
+$lang['configuration_backup_status_data_not_available'] = 'Staða gögn ekki tiltæk.';
+$lang['configuration_backup_stopping_event_system'] = 'Stífla atburður fylgjast þjónusta';
+$lang['configuration_backup_unable_to_start_restore'] = 'Ekki er hægt að hefja endurheimt';
+$lang['configuration_backup_unpacking_archive'] = 'Taka upp skjalasafn skrá';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Bíð eftir Hugbúnaðaruppfærslur kerfi';
+$lang['configuration_backup_yum_busy'] = 'Pakki framkvæmdastjóri er upptekinn.';
+
