363e03633b27501f2aef8463951c6b5b2e87cf38
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:48:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Zálohovanie konfigurácie a obnoviť aplikácia umožňuje správcovi vytvoriť snímku všetkých nastavení konfigurácie systému, umožňujúca jednoduché obnovenie v prípade straty dát.';
+$lang['configuration_backup_app_name'] = 'Konfigurácia zálohovanie a obnovenie';
+$lang['configuration_backup_archives'] = 'archív';
+$lang['configuration_backup_backup_file_invalid'] = 'Zálohovanie súboru je neplatný.';
+$lang['configuration_backup_backup_now'] = 'Zálohovať hneď';
+$lang['configuration_backup_checking_version'] = 'Kontrola archívu kompatibility verzia';
+$lang['configuration_backup_clear'] = 'jasný';
+$lang['configuration_backup_confirm_restore'] = 'Ste si istí, že chcete obnoviť pomocou nasledujúceho archívny súbor?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Adresár nemožno previesť.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Sťahovanie a inštaláciu aplikácií. Prosím, buďte trpezliví.';
+$lang['configuration_backup_file_information'] = 'Informácie o súbore';
+$lang['configuration_backup_importing_ldap'] = 'Import adresárov LDAP';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Systém musí byť registrované pred obnovením nastavenia.';
+$lang['configuration_backup_older_version_warn'] = 'Staršie verzie nezahŕňajú inštaláciu aplikácie manifest - je nutné nainštalovať potrebné aplikácie z Marketplace po obnovení';
+$lang['configuration_backup_release_mismatch'] = 'Informácie o verzii v súbore zálohy nezodpovedá verzia nainštalovaný na tomto systéme.';
+$lang['configuration_backup_release_missing'] = 'Informácie o verzii nie je k dispozícii v súbore zálohy.';
+$lang['configuration_backup_release_unsupported:'] = 'Obnovenie z nasledujúceho verzia nie je podporovaný:';
+$lang['configuration_backup_restarting_service:'] = 'Reštartovanie služby:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Obnovenie prebieha.';
+$lang['configuration_backup_restore_complete'] = 'Obnovenie bolo dokončené.';
+$lang['configuration_backup_restore_file'] = 'Archiv súborov';
+$lang['configuration_backup_restore_from_archive'] = 'Obnovenie zo zálohy';
+$lang['configuration_backup_restore_log'] = 'obnovenie Prihlásiť';
+$lang['configuration_backup_restore_type:'] = 'Obnovenie typ:';
+$lang['configuration_backup_starting_event_system'] = 'Spustenie monitora udalostí služby';
+$lang['configuration_backup_status_data_not_available'] = 'Stav údaje nie sú k dispozícii.';
+$lang['configuration_backup_stopping_event_system'] = 'Zastavenie sledovania služieb udalosť';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nemožno spustiť operáciu obnovenia';
+$lang['configuration_backup_unpacking_archive'] = 'Rozbalení archívne súbor';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Čakanie na systém aktualizácií softvéru';
+$lang['configuration_backup_yum_busy'] = 'správca balíčkov je zaneprázdnený.';
+
