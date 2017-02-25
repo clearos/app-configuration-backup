7141da2df4530276eec922130cff9d8e8837dc98
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:42:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'રૂપરેખાંકન બેકઅપ અને એપ્લિકેશન પુનઃસ્થાપિત માહિતી નુકશાન ઘટનામાં સરળ પુનઃસંગ્રહ માટે પરવાનગી આપે છે સિસ્ટમ તમામ રૂપરેખાંકન સેટિંગ્સ એક સ્નેપશોટ લેવા સંચાલક પરવાનગી આપે છે.';
+$lang['configuration_backup_app_name'] = 'રૂપરેખાંકન બૅકઅપ લો અને પુનઃસ્થાપિત';
+$lang['configuration_backup_archives'] = 'આર્કાઇવ્ઝ';
+$lang['configuration_backup_backup_file_invalid'] = 'બેકઅપ ફાઇલનામ અમાન્ય છે.';
+$lang['configuration_backup_backup_now'] = 'બેકઅપ હવે';
+$lang['configuration_backup_checking_version'] = 'તપાસી રહ્યું છે સંગ્રહિત આવૃત્તિ સુસંગતતા';
+$lang['configuration_backup_clear'] = 'ચોખ્ખુ';
+$lang['configuration_backup_confirm_restore'] = 'શું તમે ખરેખર નીચે આર્કાઇવ ફાઇલ ઉપયોગ કરીને પુનઃસ્થાપિત કરવા માંગો છો?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ડિરેક્ટરી રૂપાંતરિત કરી શકે છે.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ડાઉનલોડ અને એપ્લિકેશન્સ સ્થાપિત. શાંતિ જાળવો.';
+$lang['configuration_backup_file_information'] = 'ફાઈલ માહિતી';
+$lang['configuration_backup_importing_ldap'] = 'આયાત LDAP ડિરેક્ટરી';
+$lang['configuration_backup_network_information'] = 'નેટવર્ક માહિતી';
+$lang['configuration_backup_not_registered'] = 'તમારી સિસ્ટમ રૂપરેખાંકન સેટિંગ્સ રિસ્ટોરિંગ પહેલાં રજીસ્ટર થયેલ હોવું જ જોઈએ.';
+$lang['configuration_backup_older_version_warn'] = 'જૂની આવૃત્તિઓ પ્રગટ એપ્લિકેશન ઇન્સ્ટોલ સમાવેશ થતો નથી - બજારથી જરૂરી એપ્લિકેશન્સ સ્થાપિત કરો પછી પુનઃસ્થાપિત';
+$lang['configuration_backup_release_mismatch'] = 'બૅકઅપ ફાઇલ માં આવૃત્તિ જાણકારી આવૃત્તિ આ સિસ્ટમ પર સ્થાપિત મેળ ખાતું નથી.';
+$lang['configuration_backup_release_missing'] = 'આવૃત્તિ જાણકારી બેકઅપ ફાઈલ ઉપલબ્ધ નથી.';
+$lang['configuration_backup_release_unsupported:'] = 'નીચેના આવૃત્તિ પુનઃસ્થાપિત આધારભૂત નથી:';
+$lang['configuration_backup_restarting_service:'] = 'પુનઃશરૂ સેવા:';
+$lang['configuration_backup_restore_already_in_progress'] = 'ચાલુ પુનઃસ્થાપિત કરો.';
+$lang['configuration_backup_restore_complete'] = 'સંપૂર્ણ પુનઃસ્થાપિત કરો.';
+$lang['configuration_backup_restore_file'] = 'આર્કાઇવ ફાઇલ';
+$lang['configuration_backup_restore_from_archive'] = 'આર્કાઇવ પુનઃસ્થાપિત';
+$lang['configuration_backup_restore_log'] = 'પુનઃસ્થાપિત કરો લોગ';
+$lang['configuration_backup_restore_type:'] = 'પુનઃસ્થાપિત પ્રકાર:';
+$lang['configuration_backup_starting_event_system'] = 'શરૂ કરી રહ્યા છીએ ઘટના મોનીટર સેવા';
+$lang['configuration_backup_status_data_not_available'] = 'સ્થિતિ માહિતી ઉપલબ્ધ નથી.';
+$lang['configuration_backup_stopping_event_system'] = 'અટકાવવાનું ઘટના મોનીટર સેવા';
+$lang['configuration_backup_unable_to_start_restore'] = 'કામગીરી પુનઃસ્થાપિત શરૂ કરવા માટે અસમર્થ';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking આર્કાઇવ ફાઇલ';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'સોફ્ટવેર સુધારાઓ સિસ્ટમ માટે રાહ જુએ છે';
+$lang['configuration_backup_yum_busy'] = 'પેકેજ મેનેજર વ્યસ્ત છે.';
+
