22963cc0269d80ffcdb1b6963748b598f068356e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:47:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'कन्फिगरेसन जगेडा र अनुप्रयोग पुनर्स्थापना डाटा हानि को घटना मा सजिलो बहाली गर्न अनुमति दिने, प्रणाली को सबै विन्यास सेटिङहरू एउटा स्न्यापसट लिन प्रशासक अनुमति दिन्छ।';
+$lang['configuration_backup_app_name'] = 'कन्फिगरेसन जगेडा र पुनर्स्थापना';
+$lang['configuration_backup_archives'] = 'अभिलेख';
+$lang['configuration_backup_backup_file_invalid'] = 'जगेडा फाइल नाम अवैध छ।';
+$lang['configuration_backup_backup_now'] = 'आहिले संचित गर';
+$lang['configuration_backup_checking_version'] = 'अभिलेख संस्करण अनुकूलता जाँच गर्दै';
+$lang['configuration_backup_clear'] = 'स्पष्ट';
+$lang['configuration_backup_confirm_restore'] = 'तपाईं निम्न अभिलेख फाइल प्रयोग पुनर्स्थापना गर्न चाहनुहुन्छ?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'निर्देशिका रूपान्तरण गर्न सकिएन।';
+$lang['configuration_backup_download_and_intstall_apps'] = 'डाउनलोड र अनुप्रयोगहरू जडान गर्ने। कृपया धैर्य गर्नुहोस्।';
+$lang['configuration_backup_file_information'] = 'फाइल सूचना';
+$lang['configuration_backup_importing_ldap'] = 'LDAP निर्देशिका आयात';
+$lang['configuration_backup_network_information'] = 'नेटवर्क सूचना';
+$lang['configuration_backup_not_registered'] = 'तपाईंको सिस्टम कन्फिगरेसन सेटिङहरू पुनस्थापित अघि दर्ता हुनुपर्छ।';
+$lang['configuration_backup_older_version_warn'] = 'पुरानो संस्करण अनुप्रयोग समावेश छैन प्रकट स्थापना - कृपया बजार देखि आवश्यक अनुप्रयोगहरू स्थापना पछि पुनर्स्थापना';
+$lang['configuration_backup_release_mismatch'] = 'जगेडा फाइल मा संस्करण जानकारी यस प्रणालीमा स्थापना संस्करण मेल खाँदैन।';
+$lang['configuration_backup_release_missing'] = 'संस्करण जानकारी जगेडा फाइल उपलब्ध छैन।';
+$lang['configuration_backup_release_unsupported:'] = 'निम्न संस्करण पुनर्स्थापना समर्थित छैन:';
+$lang['configuration_backup_restarting_service:'] = 'सेवा पुन: सुरु:';
+$lang['configuration_backup_restore_already_in_progress'] = 'पुर्वावस्थामाल्याउनेप्रगतिमा।';
+$lang['configuration_backup_restore_complete'] = 'पुनर्स्थापना सम्पन्न।';
+$lang['configuration_backup_restore_file'] = 'अभिलेख फाइल';
+$lang['configuration_backup_restore_from_archive'] = 'संग्रहबाट पुनर्स्थापना';
+$lang['configuration_backup_restore_log'] = 'लग पुनर्स्थापना';
+$lang['configuration_backup_restore_type:'] = 'प्रकार पुनर्स्थापना:';
+$lang['configuration_backup_starting_event_system'] = 'शुरू घटना मनिटर सेवा';
+$lang['configuration_backup_status_data_not_available'] = 'स्थिति डाटा उपलब्ध छैन।';
+$lang['configuration_backup_stopping_event_system'] = 'रोक घटना मनिटर सेवा';
+$lang['configuration_backup_unable_to_start_restore'] = 'पुनर्स्थापना कार्य सुरु गर्न असमर्थ';
+$lang['configuration_backup_unpacking_archive'] = 'खोल्दै संग्रह फाइल';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'सफ्टवेयर अद्यावधिक सिस्टम लागि प्रतीक्षा गर्दै';
+$lang['configuration_backup_yum_busy'] = 'प्याकेज प्रबन्धक व्यस्त छ।';
+
