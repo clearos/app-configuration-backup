2ad86ac491659a8440541195752fb7dcd6f2a73f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:40:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'La còpia de seguretat i restaurar la configuració d&#39;aplicació permet a un administrador per prendre una instantània de tots els paràmetres de configuració del sistema, permetent una fàcil restauració en cas de pèrdua de dades.';
+$lang['configuration_backup_app_name'] = 'Configuració còpia de seguretat i restauració';
+$lang['configuration_backup_archives'] = 'arxiu';
+$lang['configuration_backup_backup_file_invalid'] = 'nom del fitxer de còpia de seguretat no és vàlida.';
+$lang['configuration_backup_backup_now'] = 'Còpia de seguretat ara';
+$lang['configuration_backup_checking_version'] = 'Comprovació de compatibilitat de la versió d&#39;arxiu';
+$lang['configuration_backup_clear'] = 'clar';
+$lang['configuration_backup_confirm_restore'] = 'Esteu segur que voleu restaurar utilitzant el següent arxiu d&#39;emmagatzematge?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directori No s&#39;ha pogut convertir.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Descàrrega i instal·lació d&#39;aplicacions. Si us plau sigui pacient.';
+$lang['configuration_backup_file_information'] = 'Informació del fitxer';
+$lang['configuration_backup_importing_ldap'] = 'La importació de directori LDAP';
+$lang['configuration_backup_network_information'] = 'Informació de la xarxa';
+$lang['configuration_backup_not_registered'] = 'El seu sistema ha d&#39;estar registrat abans de restaurar els valors de configuració.';
+$lang['configuration_backup_older_version_warn'] = 'Les versions anteriors no inclouen la instal·lació d&#39;aplicacions de manifest - si us plau instal·lar aplicacions necessàries des del mercat després de la restauració';
+$lang['configuration_backup_release_mismatch'] = 'La informació de versió en l&#39;arxiu de còpia de seguretat no coincideix amb la versió instal·lada en aquest sistema.';
+$lang['configuration_backup_release_missing'] = 'La informació de la versió no està disponible a l&#39;arxiu de còpia de seguretat.';
+$lang['configuration_backup_release_unsupported:'] = 'Restauració a partir de la següent versió no és compatible amb:';
+$lang['configuration_backup_restarting_service:'] = 'En reiniciar el servei:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restaura en curs.';
+$lang['configuration_backup_restore_complete'] = 'Restaurar completa.';
+$lang['configuration_backup_restore_file'] = 'arxiu arxiu';
+$lang['configuration_backup_restore_from_archive'] = 'Restaurar De Arxiu';
+$lang['configuration_backup_restore_log'] = 'restaurar Entrada';
+$lang['configuration_backup_restore_type:'] = 'Tipus de restauració:';
+$lang['configuration_backup_starting_event_system'] = 'A partir servei de monitoratge d&#39;esdeveniments';
+$lang['configuration_backup_status_data_not_available'] = 'dades d&#39;estat no disponible.';
+$lang['configuration_backup_stopping_event_system'] = 'Aturar servei de monitoratge d&#39;esdeveniments';
+$lang['configuration_backup_unable_to_start_restore'] = 'No es pot iniciar l&#39;operació de restauració';
+$lang['configuration_backup_unpacking_archive'] = 'arxiu històric de desembalatge';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Esperant un sistema d&#39;actualitzacions de programari';
+$lang['configuration_backup_yum_busy'] = 'gestor de paquets està ocupat.';
+
