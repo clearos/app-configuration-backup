0759b0ecc97672fb8cbbfa9efe4e7ec82bf29dc4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:46:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'резервната конфигурација и се врати стан овозможува на администраторот да се слика на сите конфигурациски поставки на системот, овозможувајќи лесен реставрација во случај на загуба на податоци.';
+$lang['configuration_backup_app_name'] = 'Конфигурација Бекап и обновување';
+$lang['configuration_backup_archives'] = 'Архива';
+$lang['configuration_backup_backup_file_invalid'] = 'Бекап датотеката не е валидна.';
+$lang['configuration_backup_backup_now'] = 'Backup Сега';
+$lang['configuration_backup_checking_version'] = 'Проверка на архива компатибилност верзија';
+$lang['configuration_backup_clear'] = 'Отстрани';
+$lang['configuration_backup_confirm_restore'] = 'Дали сте сигурни дека сакате да го направите со користење на следниве архива?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Директориумот не може да се конвертира.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Преземање и инсталирање на апликации. Ве молиме бидете трпеливи.';
+$lang['configuration_backup_file_information'] = 'Информации за датотека';
+$lang['configuration_backup_importing_ldap'] = 'Внесување на LDAP директориум';
+$lang['configuration_backup_network_information'] = 'Информации за мрежата';
+$lang['configuration_backup_not_registered'] = 'Вашиот систем мора да бидат регистрирани пред да го обновите конфигурациски поставки.';
+$lang['configuration_backup_older_version_warn'] = 'Постарите верзии не вклучуваат стан инсталирате манифестира - Ве молиме да се инсталира потребните апликации од пазарот откако се врати';
+$lang['configuration_backup_release_mismatch'] = 'информации верзијата на бекап датотеката не се совпаѓа со верзијата инсталиран на овој систем.';
+$lang['configuration_backup_release_missing'] = 'информации верзија не е достапна во бекап датотеката.';
+$lang['configuration_backup_release_unsupported:'] = 'Враќање од следната верзија не е поддржан:';
+$lang['configuration_backup_restarting_service:'] = 'Рестартирање на услугата:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Враќање во тек.';
+$lang['configuration_backup_restore_complete'] = 'Враќање заврши.';
+$lang['configuration_backup_restore_file'] = 'архива';
+$lang['configuration_backup_restore_from_archive'] = 'Враќање од архивата';
+$lang['configuration_backup_restore_log'] = 'врати се Влез';
+$lang['configuration_backup_restore_type:'] = 'Враќање тип:';
+$lang['configuration_backup_starting_event_system'] = 'Почетна услуга настан монитор';
+$lang['configuration_backup_status_data_not_available'] = 'не се достапни податоци за статус.';
+$lang['configuration_backup_stopping_event_system'] = 'Запирање на услуги настан монитор';
+$lang['configuration_backup_unable_to_start_restore'] = 'Можам да го стартувам операцијата за обновување';
+$lang['configuration_backup_unpacking_archive'] = 'Отпакување архива';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Чекање за ажурирање на софтверот на системот';
+$lang['configuration_backup_yum_busy'] = 'Менаџер на пакети е зафатен.';
+
