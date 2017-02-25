274716ccca54ae2461d7587d269fb3d3af679673
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:50:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'கட்டமைப்பு காப்பு மற்றும் பயன்பாட்டை மீட்க தரவு இழப்பு ஏற்பட்டால் எளிதாக மறுசீரமைப்பு அனுமதிக்கிறது, அமைப்பின் அனைத்து கட்டமைப்பு அமைப்புகள் ஒரு புகைப்படம் எடுத்து கொள்ள ஒரு நிர்வாகி அனுமதிக்கிறது.';
+$lang['configuration_backup_app_name'] = 'கட்டமைப்பு காப்பு மற்றும் மீட்டமை';
+$lang['configuration_backup_archives'] = 'சென்னை';
+$lang['configuration_backup_backup_file_invalid'] = 'காப்பு கோப்பின் பெயர் தவறானது.';
+$lang['configuration_backup_backup_now'] = 'இப்பொழது பாதுகாப்பிற்காக சேமித்து வை';
+$lang['configuration_backup_checking_version'] = 'காப்பகத்தை பதிப்பு பொருந்தக்கூடிய சரிபார்க்கிறது';
+$lang['configuration_backup_clear'] = 'தெளிவு';
+$lang['configuration_backup_confirm_restore'] = 'நீங்கள் தொடர்ந்து காப்பக கோப்பு பயன்படுத்தி மீட்க வேண்டும் என்பதில் உறுதியாக இருக்கிறீர்களா?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ஏற்கனவே மாற்றவும் முடியாது.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'பதிவிறக்கும் மற்றும் பயன்பாடுகள் நிறுவும். தயவுசெய்து பொருமைையாயிறு.';
+$lang['configuration_backup_file_information'] = 'கோப்பு தகவல்';
+$lang['configuration_backup_importing_ldap'] = 'இறக்குமதி LDAP அடைவு';
+$lang['configuration_backup_network_information'] = 'வலையமைப்பு தகவல்';
+$lang['configuration_backup_not_registered'] = 'உங்கள் கணினி கட்டமைப்பு அமைப்புகளை நிலைக்கு முன் பதிவு செய்ய வேண்டும்.';
+$lang['configuration_backup_older_version_warn'] = 'பழைய பதிப்புகள் பயன்பாட்டை சேர்க்க வேண்டாம் பகிரங்கமான நிறுவ - சந்தை இருந்து தேவைப்படும் பயன்பாடுகளை நிறுவ செய்து பின்னர் மீட்க';
+$lang['configuration_backup_release_mismatch'] = 'காப்பு கோப்பு பதிப்பு தகவல் நிறுவப்பட்ட பதிப்புடன் பொருந்தவில்லை.';
+$lang['configuration_backup_release_missing'] = 'பதிப்பு தகவல் காப்பு கோப்பு கிடைக்கவில்லை.';
+$lang['configuration_backup_release_unsupported:'] = 'பின்வரும் பதிப்பு மீட்டமைப்பதன் ஆதரவு இல்லை:';
+$lang['configuration_backup_restarting_service:'] = 'சேவை மீண்டும் துவக்கப்படும்:';
+$lang['configuration_backup_restore_already_in_progress'] = 'முன்னேற்றம் மீட்டமை.';
+$lang['configuration_backup_restore_complete'] = 'முழுமையான மீட்டமை.';
+$lang['configuration_backup_restore_file'] = 'காப்பக கோப்பு';
+$lang['configuration_backup_restore_from_archive'] = 'காப்பகம் இருந்து மீட்க';
+$lang['configuration_backup_restore_log'] = 'உள்நுழைய மீட்டமை';
+$lang['configuration_backup_restore_type:'] = 'மீட்டமை வகை:';
+$lang['configuration_backup_starting_event_system'] = 'தொடங்கி நிகழ்வு மானிட்டர் சேவை';
+$lang['configuration_backup_status_data_not_available'] = 'நிலைமை தரவு கிடைக்கவில்லை.';
+$lang['configuration_backup_stopping_event_system'] = 'நிறுத்துதல் நிகழ்வு மானிட்டர் சேவை';
+$lang['configuration_backup_unable_to_start_restore'] = 'மீட்டெடுக்கும் தொடங்க முடியவில்லை';
+$lang['configuration_backup_unpacking_archive'] = 'துறக்கிறேன் காப்பக கோப்பு';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'மென்பொருள் மேம்படுத்தல்கள் அமைப்பு காத்திருக்கிறது';
+$lang['configuration_backup_yum_busy'] = 'தொகுப்பு மேலாளர் பிஸியாக இருக்கிறார்.';
+
