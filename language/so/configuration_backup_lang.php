3e3b6c8410b053f441ebc924de5e6f976d708dc2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:49:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'gurmad qaabeynta iyo soo celin app wuxuu kuu ogolaanayaa maamulka in ay qaataan si dhekhso ah oo dhan goobaha qaabeynta nidaamka, jidaynayey dib u soo celinta sahlan in ay dhacdo of khasaaro xogta.';
+$lang['configuration_backup_app_name'] = 'Kaabta qaabeynta iyo Soo Celinta';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'filename kaabta waa baadil.';
+$lang['configuration_backup_backup_now'] = 'kaabta Hadda';
+$lang['configuration_backup_checking_version'] = 'Hubinta version archive waafaqid';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'Ma hubtaa in aad rabto in aad soo celin isticmaalaya faylka archive soo socda?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory laga yaabaa in aan la isku beddelaysaa.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Degsado iyo ku rakibidda apps. Fadlan samir.';
+$lang['configuration_backup_file_information'] = 'Information File';
+$lang['configuration_backup_importing_ldap'] = 'Keenista buugga LDAP';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Your nidaamka waa la diiwaan gelin ka hor soo celinta goobaha qaabeynta.';
+$lang['configuration_backup_older_version_warn'] = 'versions Older ha ku darin app rakibi Cad - fadlan rakibi apps loo baahan yahay ka Marketplace ka dib markii loo soo celiyo';
+$lang['configuration_backup_release_mismatch'] = 'Macluumaadka version in file gurmad uusan u dhigma version rakibay on nidaamka this.';
+$lang['configuration_backup_release_missing'] = 'Macluumaad version The aan la heli karin in file gurmad ah.';
+$lang['configuration_backup_release_unsupported:'] = 'Soo celinta ka version soo socda aan la taageeray:';
+$lang['configuration_backup_restarting_service:'] = 'Howleheedii adeegga:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Soo Celinta in horumarka.';
+$lang['configuration_backup_restore_complete'] = 'Soo Celinta dhamaystiran.';
+$lang['configuration_backup_restore_file'] = 'File archive';
+$lang['configuration_backup_restore_from_archive'] = 'Soo Celinta From Archive';
+$lang['configuration_backup_restore_log'] = 'Soo Celinta Log';
+$lang['configuration_backup_restore_type:'] = 'Soo Celinta nooca:';
+$lang['configuration_backup_starting_event_system'] = 'Laga bilaabo adeeg monitor dhacdo';
+$lang['configuration_backup_status_data_not_available'] = 'Xogta Xaaladda aan la heli karin.';
+$lang['configuration_backup_stopping_event_system'] = 'Joojinta adeegga monitor dhacdo';
+$lang['configuration_backup_unable_to_start_restore'] = 'Lama si ay u bilaabaan soo celin hawlgalka';
+$lang['configuration_backup_unpacking_archive'] = 'file archive unpacking';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Sugitaanka nidaamka updates software';
+$lang['configuration_backup_yum_busy'] = 'Tababaraha Xirmada waa mashquul.';
+
