37a1b81344df02fc813b2dbcf333fd810848e1f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:46:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'L-backup konfigurazzjoni u jirrestawraw app tippermetti amministratur biex tieħu stampa ta &#39;settings tal-konfigurazzjoni kollha tas-sistema, li tippermetti restawr faċli fil-każ ta&#39; telf ta &#39;data.';
+$lang['configuration_backup_app_name'] = 'Konfigurazzjoni backup u Restore';
+$lang['configuration_backup_archives'] = 'arkivji';
+$lang['configuration_backup_backup_file_invalid'] = 'isem tal-file backup huwa invalidu.';
+$lang['configuration_backup_backup_now'] = 'backup Issa';
+$lang['configuration_backup_checking_version'] = 'Iċċekkjar verżjoni kompatibilità arkivju';
+$lang['configuration_backup_clear'] = 'ċar';
+$lang['configuration_backup_confirm_restore'] = 'Inti żgur li trid terġa tuża l-fajl arkivju li ġejja?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory ma setgħux jiġu konvertiti.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Tniżżil u l-installazzjoni apps. Jekk jogħġbok kun pazjent.';
+$lang['configuration_backup_file_information'] = 'file Informazzjoni';
+$lang['configuration_backup_importing_ldap'] = 'Jimporta direttorju LDAP';
+$lang['configuration_backup_network_information'] = 'Informazzjoni netwerk';
+$lang['configuration_backup_not_registered'] = 'Your sistema jridu jkunu rreġistrati qabel ma &#39;jerga settings tal-konfigurazzjoni.';
+$lang['configuration_backup_older_version_warn'] = 'Verżjonijiet Older ma jinkludux app jinstallaw manifest - jekk jogħġbok jinstallaw apps meħtieġa mill-Suq wara jirrestawraw';
+$lang['configuration_backup_release_mismatch'] = 'L-informazzjoni verżjoni tal-fajl backup ma taqbilx ma l-verżjoni installata fuq din is-sistema.';
+$lang['configuration_backup_release_missing'] = 'L-informazzjoni verżjoni mhix disponibbli fil-fajl backup.';
+$lang['configuration_backup_release_unsupported:'] = 'Restawrar mill-verżjoni segwenti ma hija sostnuta:';
+$lang['configuration_backup_restarting_service:'] = 'Istartjar mill-ġdid is-servizz:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Restore fil-progress.';
+$lang['configuration_backup_restore_complete'] = 'Restore kompluta.';
+$lang['configuration_backup_restore_file'] = 'arkivju File';
+$lang['configuration_backup_restore_from_archive'] = 'Restore Mill Arkivju';
+$lang['configuration_backup_restore_log'] = 'Restore Log';
+$lang['configuration_backup_restore_type:'] = 'Restore tip:';
+$lang['configuration_backup_starting_event_system'] = 'Bidu servizz tissorvelja avveniment';
+$lang['configuration_backup_status_data_not_available'] = 'dejta Status mhux disponibbli.';
+$lang['configuration_backup_stopping_event_system'] = 'Inwaqqfu servizz tissorvelja avveniment';
+$lang['configuration_backup_unable_to_start_restore'] = 'Tkunx tista &#39;tistartja jirrestawraw operazzjoni';
+$lang['configuration_backup_unpacking_archive'] = 'fajl arkivju ispakkjar';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Stennija għas-sistema aġġornamenti tas-softwer';
+$lang['configuration_backup_yum_busy'] = 'maniġer pakkett hija okkupata.';
+
