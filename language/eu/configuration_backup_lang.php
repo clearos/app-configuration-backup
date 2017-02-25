6f42c4bad4c5b7dfed3b742e1f51230d6096e4e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:39:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'The konfigurazio babeskopia eta aplikazioa berrezartzeko administratzaile bat konfigurazio-sistemaren ezarpen guztiak argazki bat hartu esker, datuak galduz gero zaharberritze erraza eskaintzeko.';
+$lang['configuration_backup_app_name'] = 'Configuration Backup and Restore';
+$lang['configuration_backup_archives'] = 'Artxiboak';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup filename baliogabea da.';
+$lang['configuration_backup_backup_now'] = 'Backup Now';
+$lang['configuration_backup_checking_version'] = 'artxiboa bertsio bateragarritasuna egiaztatzen';
+$lang['configuration_backup_clear'] = 'Eguraldi';
+$lang['configuration_backup_confirm_restore'] = 'Ziur honako artxiboan fitxategia erabiliz leheneratu nahi duzula?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory ezin izan da bihurtu.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Deskarga eta aplikazioak instalatzen. Mesedez, izan pazientzia.';
+$lang['configuration_backup_file_information'] = 'File informazioa';
+$lang['configuration_backup_importing_ldap'] = 'Inportatzen LDAP direktorioa';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Zure sistemaren konfigurazio ezarpenak berrezarri aurretik erregistratuta egon.';
+$lang['configuration_backup_older_version_warn'] = 'bertsio zaharragoa ez besteak aplikazioa instalatu manifiesto - instalatu beharrezko aplikazioak Marketplace berreskuratu ondoren';
+$lang['configuration_backup_release_mismatch'] = 'Bertsio backup fitxategia ere informazioa ez da instalatuta sisteman bertsioarekin.';
+$lang['configuration_backup_release_missing'] = 'Bertsio informazioa ez da babeskopia fitxategia eskuragarri.';
+$lang['configuration_backup_release_unsupported:'] = 'Hurrengo bertsioan zaharberritzea ez da onartzen:';
+$lang['configuration_backup_restarting_service:'] = 'Berrabiatzen zerbitzua:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Berreskurapena.';
+$lang['configuration_backup_restore_complete'] = 'Berreskuratu osoa.';
+$lang['configuration_backup_restore_file'] = 'Artxiboa File';
+$lang['configuration_backup_restore_from_archive'] = 'Berreskuratu artxibotik';
+$lang['configuration_backup_restore_log'] = 'Berreskuratu Login';
+$lang['configuration_backup_restore_type:'] = 'Berreskuratu mota:';
+$lang['configuration_backup_starting_event_system'] = 'Aurrera ekitaldi monitore zerbitzua';
+$lang['configuration_backup_status_data_not_available'] = 'Egoera datuak ez daude eskuragarri.';
+$lang['configuration_backup_stopping_event_system'] = 'Gelditzea gertaera monitore zerbitzua';
+$lang['configuration_backup_unable_to_start_restore'] = 'Ezin da berreskuratu eragiketa hasteko';
+$lang['configuration_backup_unpacking_archive'] = 'Deskonprimitzen artxiboa fitxategia';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'software eguneraketak sistema bat itxaroten';
+$lang['configuration_backup_yum_busy'] = 'Pakete kudeatzailea lanpetuta dago.';
+
