688d78f3c7cf15343cb5f49ffc3fb81bcc383a74
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:50:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Ang configuration backup at ibalik ang app ay nagbibigay-daan sa isang administrator upang kumuha ng snapshot ng lahat ng mga setting ng configuration ng sistema, na nagpapahintulot sa madaling pagpapanumbalik sa kaganapan ng pagkawala ng data.';
+$lang['configuration_backup_app_name'] = 'Configuration Backup at Ibalik';
+$lang['configuration_backup_archives'] = 'arkibos';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup filename ay hindi wasto.';
+$lang['configuration_backup_backup_now'] = 'backup Now';
+$lang['configuration_backup_checking_version'] = 'Sinusuri archive bersyon compatibility';
+$lang['configuration_backup_clear'] = 'malinaw';
+$lang['configuration_backup_confirm_restore'] = 'Sigurado ka bang gusto mong ibalik ang paggamit ng mga sumusunod na file archive?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Direktoryo ay hindi ma-convert.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Nagda-download at pag-install ng apps. Mangyaring maging mahinahon.';
+$lang['configuration_backup_file_information'] = 'Impormasyon file';
+$lang['configuration_backup_importing_ldap'] = 'Pag-import ng LDAP directory';
+$lang['configuration_backup_network_information'] = 'Information network';
+$lang['configuration_backup_not_registered'] = 'Ang iyong sistema ay dapat na nakarehistro bago pagpapanumbalik configuration settings.';
+$lang['configuration_backup_older_version_warn'] = 'Mas lumang mga bersyon ay hindi isama ang app install manifest - mangyaring i-install kinakailangan apps mula sa Marketplace matapos ibalik';
+$lang['configuration_backup_release_mismatch'] = 'Ang bersyon impormasyon sa backup file ay hindi tumutugma sa bersyon-install sa system na ito.';
+$lang['configuration_backup_release_missing'] = 'Ang bersyon impormasyon ay hindi magagamit sa ang backup file.';
+$lang['configuration_backup_release_unsupported:'] = 'Ipinapanumbalik mula sa mga sumusunod na bersyon ay hindi suportado:';
+$lang['configuration_backup_restarting_service:'] = 'Restarting serbisyo:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Ibalik sa progreso.';
+$lang['configuration_backup_restore_complete'] = 'Ibalik kumpleto.';
+$lang['configuration_backup_restore_file'] = 'archive File';
+$lang['configuration_backup_restore_from_archive'] = 'Ibalik Mula Archive';
+$lang['configuration_backup_restore_log'] = 'ibalik Log';
+$lang['configuration_backup_restore_type:'] = 'Ibalik uri:';
+$lang['configuration_backup_starting_event_system'] = 'Simula kaganapan monitor service';
+$lang['configuration_backup_status_data_not_available'] = 'data Status hindi magagamit.';
+$lang['configuration_backup_stopping_event_system'] = 'Pagtigil kaganapan monitor service';
+$lang['configuration_backup_unable_to_start_restore'] = 'Hindi makapagsimula ibalik ang operasyon';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking archive file';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Naghihintay para sa mga update ng software ng sistema';
+$lang['configuration_backup_yum_busy'] = 'Package manager ay abala.';
+
