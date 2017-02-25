dbc819e0af4215f771d46eb92388eeae59358777
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:41:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Zálohování konfigurace a obnovit aplikace umožňuje správci pořídit snímek všech nastavení konfigurace systému, umožňující snadné obnovení v případě ztráty dat.';
+$lang['configuration_backup_app_name'] = 'Konfigurace zálohování a obnovení';
+$lang['configuration_backup_archives'] = 'Archiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Zálohování souboru je neplatný.';
+$lang['configuration_backup_backup_now'] = 'zálohování teď';
+$lang['configuration_backup_checking_version'] = 'Kontrola archivu kompatibility verze';
+$lang['configuration_backup_clear'] = 'Průhledná';
+$lang['configuration_backup_confirm_restore'] = 'Jste si jisti, že chcete obnovit pomocí následujícího archivní soubor?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Adresář nelze převést.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Stahování a instalaci aplikací. Prosím, buďte trpěliví.';
+$lang['configuration_backup_file_information'] = 'Informace o souboru';
+$lang['configuration_backup_importing_ldap'] = 'Import adresářů LDAP';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Systém musí být registrovány před obnovením nastavení.';
+$lang['configuration_backup_older_version_warn'] = 'Starší verze nezahrnují instalaci aplikace manifest - je nutné nainstalovat potřebné aplikace z Marketplace po obnovení';
+$lang['configuration_backup_release_mismatch'] = 'Informace o verzi v souboru zálohy neodpovídá verze nainstalován na tomto systému.';
+$lang['configuration_backup_release_missing'] = 'Informace o verzi není k dispozici v souboru zálohy.';
+$lang['configuration_backup_release_unsupported:'] = 'Obnovení z následujícího verze není podporován:';
+$lang['configuration_backup_restarting_service:'] = 'Restartování služby:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Obnovení probíhá.';
+$lang['configuration_backup_restore_complete'] = 'Obnovení bylo dokončeno.';
+$lang['configuration_backup_restore_file'] = 'Archiv souborů';
+$lang['configuration_backup_restore_from_archive'] = 'Obnovení ze zálohy';
+$lang['configuration_backup_restore_log'] = 'obnovení Přihlásit';
+$lang['configuration_backup_restore_type:'] = 'Obnovení typ:';
+$lang['configuration_backup_starting_event_system'] = 'Spuštění monitoru událostí služby';
+$lang['configuration_backup_status_data_not_available'] = 'Stav údaje nejsou k dispozici.';
+$lang['configuration_backup_stopping_event_system'] = 'Zastavení sledování služeb událost';
+$lang['configuration_backup_unable_to_start_restore'] = 'Nelze spustit operaci obnovení';
+$lang['configuration_backup_unpacking_archive'] = 'Rozbalení archivní soubor';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Čekání na systém aktualizací softwaru';
+$lang['configuration_backup_yum_busy'] = 'správce balíčků je zaneprázdněn.';
+
