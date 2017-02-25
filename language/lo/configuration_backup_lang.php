33f2c468657fd5f307f1c464de1d8a77d3329d4c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:45:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'ການສໍາຮອງຂໍ້ມູນການຕັ້ງຄ່າແລະການຟື້ນຟູ app ອະນຸຍາດໃຫ້ຜູ້ບໍລິຫານທີ່ຈະໃຊ້ເວລາພາບບັນທຶກຂອງການຕັ້ງຄ່າການຕັ້ງຄ່າທັງຫມົດຂອງລະບົບ, ໃຫ້ການຟື້ນຟູງ່າຍໃນກໍລະນີຂອງການສູນເສຍຂໍ້ມູນ.';
+$lang['configuration_backup_app_name'] = 'Backup ການຕັ້ງຄ່າແລະການຟື້ນຟູ';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'filename ສໍາຮອງຂໍ້ມູນທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['configuration_backup_backup_now'] = 'ສໍາ​ຮອງ​ຂໍ້​ມູນ​ໃນ​ປັດ​ຈຸ​ບັນ';
+$lang['configuration_backup_checking_version'] = 'ການກວດສອບສະບັບເກັບເຂົ້າກັນໄດ້';
+$lang['configuration_backup_clear'] = 'ຈະແຈ້ງ';
+$lang['configuration_backup_confirm_restore'] = 'ທ່ານແນ່ໃຈວ່າທ່ານຕ້ອງການທີ່ຈະຟື້ນຟູການນໍາໃຊ້ເອກະສານທີ່ເກັບຕໍ່ໄປນີ້?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'ລະບົບບໍ່ສາມາດປ່ຽນໃຈເຫລື້ອມໃສ.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'ການດາວໂຫຼດແລະຕິດຕັ້ງກິດ. ກະລຸນາຄວາມອົດທົນ.';
+$lang['configuration_backup_file_information'] = 'ຂໍ້ມູນເອກະສານ';
+$lang['configuration_backup_importing_ldap'] = 'ລະບົບ LDAP ການນໍາເຂົ້າ';
+$lang['configuration_backup_network_information'] = 'ຂໍ້ມູນເຄືອຂ່າຍ';
+$lang['configuration_backup_not_registered'] = 'ລະບົບຂອງທ່ານຕ້ອງໄດ້ຮັບການຈົດທະບຽນກ່ອນທີ່ຈະຟື້ນຟູການຕັ້ງຄ່າການຕັ້ງຄ່າ.';
+$lang['configuration_backup_older_version_warn'] = 'ສະບັບເກົ່າບໍ່ປະກອບມີ app ຕິດຕັ້ງ manifest - ກະລຸນາຕິດຕັ້ງກິດຕ້ອງການຈາກຕະຫຼາດຫຼັງຈາກການຟື້ນຟູ';
+$lang['configuration_backup_release_mismatch'] = 'ຂໍ້ມູນຂ່າວສານສະບັບໃນໄຟລ໌າຮອງບໍ່ມີຄໍາວ່າສະບັບທີ່ຕິດຕັ້ງໃນລະບົບນີ້.';
+$lang['configuration_backup_release_missing'] = 'ຂໍ້ມູນຂ່າວສານສະບັບດັ່ງກ່າວແມ່ນບໍ່ມີຢູ່ໃນເອກະສານສໍາຮອງຂໍ້ມູນ.';
+$lang['configuration_backup_release_unsupported:'] = 'ການຟື້ນຟູຈາກສະບັບດັ່ງຕໍ່ໄປນີ້ແມ່ນບໍ່ສະຫນັບສະຫນູນ:';
+$lang['configuration_backup_restarting_service:'] = 'ການເລີ່ມຕົ້ນໃຫມ່ການບໍລິການ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'ການຟື້ນຟູໃນຄວາມຄືບຫນ້າ.';
+$lang['configuration_backup_restore_complete'] = 'ການຟື້ນຟູຄົບຖ້ວນສົມບູນ.';
+$lang['configuration_backup_restore_file'] = 'ໄຟລ໌ຮວບຮວມ';
+$lang['configuration_backup_restore_from_archive'] = 'ການຟື້ນຟູຈາກການຮວບຮວມ';
+$lang['configuration_backup_restore_log'] = 'ການຟື້ນຟູເຂົ້າສູ່ລະບົບ';
+$lang['configuration_backup_restore_type:'] = 'ການຟື້ນຟູປະເພດ:';
+$lang['configuration_backup_starting_event_system'] = 'ເລີ່ມໃຫ້ບໍລິການຕິດຕາມກໍລະນີ';
+$lang['configuration_backup_status_data_not_available'] = 'ຂໍ້ມູນສະຖານະພາບບໍ່ມີ.';
+$lang['configuration_backup_stopping_event_system'] = 'ການຂາຍການບໍລິການຕິດຕາມກວດກາກິດຈະກໍາ';
+$lang['configuration_backup_unable_to_start_restore'] = 'ບໍ່ສາມາດເພື່ອເລີ່ມຕົ້ນການປະຕິບັດງານຟື້ນຟູ';
+$lang['configuration_backup_unpacking_archive'] = 'ເອກະສານເກັບ Unpacking';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'ລໍຖ້າສໍາລັບລະບົບການປັບປຸງຊອບແວ';
+$lang['configuration_backup_yum_busy'] = 'ຜູ້ຈັດການຊຸດເປັນທຸລະກິດ.';
+
