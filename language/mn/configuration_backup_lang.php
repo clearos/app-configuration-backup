8693fe61eddd859d0b1c3fca1873cfe8a8761cc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:46:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'тохиргооны нөөцлөлт болон апп сэргээх өгөгдлийн алдагдалд тохиолдолд амархан сэргээх боломжтой, тогтолцооны бүхий л тохиргоонууд нь хормын хувилбарыг авч администраторт олгодог.';
+$lang['configuration_backup_app_name'] = 'Тохиргоо Нөөцлөх болон нөхөн сэргээх';
+$lang['configuration_backup_archives'] = 'архив';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup файлын нэр хүчингүй байна.';
+$lang['configuration_backup_backup_now'] = 'Нөөцлөх Одоо';
+$lang['configuration_backup_checking_version'] = 'Архивын хувилбар нийлэмжийг шалгах';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'Та дараах архив файл ашиглан сэргээх итгэлтэй байна уу?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Лавлах хөрвүүлж үл болно.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Татаж авах болон програм суулгах. Тэвчээртэй байна уу.';
+$lang['configuration_backup_file_information'] = 'Файлын мэдээлэл';
+$lang['configuration_backup_importing_ldap'] = 'Импортлогч LDAP сан';
+$lang['configuration_backup_network_information'] = 'Сүлжээний мэдээлэл';
+$lang['configuration_backup_not_registered'] = 'Таны системийн тохиргоог сэргээх нь өмнө бүртгүүлсэн байх ёстой.';
+$lang['configuration_backup_older_version_warn'] = 'Хуучин хувилбарууд апп манифест суулгах битгий оруулаарай - Marketplace-аас шаардлагатай програм суулгана уу дараа сэргээх';
+$lang['configuration_backup_release_mismatch'] = 'нөөц файл дахь хувилбар мэдээллийг энэ системд суулгаагүй хувилбартай авцалдахгүй байна.';
+$lang['configuration_backup_release_missing'] = 'хувилбар мэдээллийн нөөц файлд байхгүй байна.';
+$lang['configuration_backup_release_unsupported:'] = 'Дараах хувилбараас сэргээх нь дэмжигдээгүй байна:';
+$lang['configuration_backup_restarting_service:'] = 'Дахин эхлүүлэх үйлчилгээ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'явагдаж сэргээ.';
+$lang['configuration_backup_restore_complete'] = 'бүрэн сэргээ.';
+$lang['configuration_backup_restore_file'] = 'архивын Файлын';
+$lang['configuration_backup_restore_from_archive'] = 'Архиваас сэргээх';
+$lang['configuration_backup_restore_log'] = 'Нэвтрэх Нэвтрэх сэргээх';
+$lang['configuration_backup_restore_type:'] = 'Сэргээх төрөл:';
+$lang['configuration_backup_starting_event_system'] = 'Эхлэн үйл явдал хянах үйлчилгээ';
+$lang['configuration_backup_status_data_not_available'] = 'Төлөв мэдээлэл байхгүй.';
+$lang['configuration_backup_stopping_event_system'] = 'Зогсоох арга хэмжээ хянах үйлчилгээ';
+$lang['configuration_backup_unable_to_start_restore'] = 'үйл ажиллагааг сэргээх эхлэх боломжгүй';
+$lang['configuration_backup_unpacking_archive'] = 'Зарах архив файл';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'програм хангамжийн шинэчлэл системийн хүлээж байна';
+$lang['configuration_backup_yum_busy'] = 'Багц менежер завгүй байна.';
+
