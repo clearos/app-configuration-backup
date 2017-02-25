63cc9fb4e6b810fab44c714113efcf61fd4188ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:42:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'The sanyi wariyar ajiya da mayar app damar mai gudanarwa a yi a hoto dukan saitunan sanyi daga cikin tsarin, kyale sauki sabuntawa a taron na data hasãra.';
+$lang['configuration_backup_app_name'] = 'Kanfigareshan Ajiyayyen da kuma mayar';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Ajiyayyen filename ba daidai ba ne.';
+$lang['configuration_backup_backup_now'] = 'Ajiyayyen Yanzu';
+$lang['configuration_backup_checking_version'] = 'Dubawa archive version karfinsu';
+$lang['configuration_backup_clear'] = 'Sunny';
+$lang['configuration_backup_confirm_restore'] = 'Ka tabbata kana son ka mayar yin amfani da wadannan archive file?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory ba za a iya tuba.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Sauke da installing apps. Don Allah ku yi haƙuri.';
+$lang['configuration_backup_file_information'] = 'file Information';
+$lang['configuration_backup_importing_ldap'] = 'Shigo LDAP directory';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Your tsarin dole ne a rajista da tanadi saitunan sanyi.';
+$lang['configuration_backup_older_version_warn'] = 'Older versions ba sun hada da app shigar bayyananne - don Allah shigar da ake bukata apps daga kasuwa bayan mayar';
+$lang['configuration_backup_release_mismatch'] = 'The version bayanai a madadin fayil bai daidaita da version shigar a kan wannan tsarin.';
+$lang['configuration_backup_release_missing'] = 'The version bayanai ba samuwa a madadin fayil.';
+$lang['configuration_backup_release_unsupported:'] = 'Tanadi daga cikin wadannan version aka ba da goyan:';
+$lang['configuration_backup_restarting_service:'] = 'Sake farawa, sabis:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Mayar a ci gaba.';
+$lang['configuration_backup_restore_complete'] = 'Mayar complete.';
+$lang['configuration_backup_restore_file'] = 'Amsoshi File';
+$lang['configuration_backup_restore_from_archive'] = 'Mayar Daga Amsoshi';
+$lang['configuration_backup_restore_log'] = 'mayar Log';
+$lang['configuration_backup_restore_type:'] = 'Mayar type:';
+$lang['configuration_backup_starting_event_system'] = 'An fara taron duba sabis';
+$lang['configuration_backup_status_data_not_available'] = 'Status data ba available.';
+$lang['configuration_backup_stopping_event_system'] = 'Tsayawa taron duba sabis';
+$lang['configuration_backup_unable_to_start_restore'] = 'An kasa don fara mayar aiki';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking archive fayil';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Jiran software updates tsarin';
+$lang['configuration_backup_yum_busy'] = 'Package sarrafa ne m.';
+
