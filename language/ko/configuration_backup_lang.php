55e2c2420bff635a12e3a1f7f25fd77bf88abb4f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:44:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = '구성 백업 앱 복원 데이터 손실 이벤트의 복구를 용이하므로, 시스템의 모든 구성 설정의 스냅 샷을 관리자에게 허용한다.';
+$lang['configuration_backup_app_name'] = '구성 백업 및 복원';
+$lang['configuration_backup_archives'] = '기록 보관소';
+$lang['configuration_backup_backup_file_invalid'] = '백업 파일 이름이 잘못되었습니다.';
+$lang['configuration_backup_backup_now'] = '백업 지금';
+$lang['configuration_backup_checking_version'] = '아카이브 버전 호환성 확인';
+$lang['configuration_backup_clear'] = '명확한';
+$lang['configuration_backup_confirm_restore'] = '당신은 당신이 다음 아카이브 파일을 사용하여 복원 하시겠습니까?';
+$lang['configuration_backup_directory_could_not_be_converted'] = '디렉토리 변환 할 수 없습니다.';
+$lang['configuration_backup_download_and_intstall_apps'] = '다운로드 및 응용 프로그램을 설치. 기다려주십시오.';
+$lang['configuration_backup_file_information'] = '파일 정보';
+$lang['configuration_backup_importing_ldap'] = '가져 오기 LDAP 디렉토리';
+$lang['configuration_backup_network_information'] = '네트워크 정보';
+$lang['configuration_backup_not_registered'] = '시스템이 구성 설정을 복원하기 전에 등록해야합니다.';
+$lang['configuration_backup_older_version_warn'] = '이전 버전의 매니페스트 앱 설치를 포함하지 않습니다 - 후 복원 마켓 플레이스에서 필요한 응용 프로그램을 설치하시기 바랍니다';
+$lang['configuration_backup_release_mismatch'] = '백업 파일의 버전 정보는 본 시스템에 설치된 버전과 일치하지 않는다.';
+$lang['configuration_backup_release_missing'] = '버전 정보가 백업 파일에서 사용할 수 없습니다.';
+$lang['configuration_backup_release_unsupported:'] = '다음 버전에서 복원하는 것은 지원되지 않습니다';
+$lang['configuration_backup_restarting_service:'] = '서비스를 다시 시작 :';
+$lang['configuration_backup_restore_already_in_progress'] = '진행 복원합니다.';
+$lang['configuration_backup_restore_complete'] = '완전한 복원합니다.';
+$lang['configuration_backup_restore_file'] = '아카이브 파일';
+$lang['configuration_backup_restore_from_archive'] = '아카이브에서 복원';
+$lang['configuration_backup_restore_log'] = '로그인 복원';
+$lang['configuration_backup_restore_type:'] = '유형을 복원';
+$lang['configuration_backup_starting_event_system'] = '시작 이벤트 모니터 서비스';
+$lang['configuration_backup_status_data_not_available'] = '상태 데이터를 사용할 수 없습니다.';
+$lang['configuration_backup_stopping_event_system'] = '중지 이벤트 모니터 서비스';
+$lang['configuration_backup_unable_to_start_restore'] = '복원 작업을 시작할 수 없습니다';
+$lang['configuration_backup_unpacking_archive'] = '포장 풀기 아카이브 파일';
+$lang['configuration_backup_waiting_for_software_updates_system'] = '소프트웨어 업데이트 시스템 대기';
+$lang['configuration_backup_yum_busy'] = '패키지 관리자가 사용 중입니다.';
+
