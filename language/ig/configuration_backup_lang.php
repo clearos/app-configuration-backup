efbf140cb235d57f7cecd596ab259b347aad2e8f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:43:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'The nhazi nkwado ma weghachite ngwa na-enye ohere onye nchịkwa na-a snapshot niile nhazi ntọala nke usoro, ikwe mfe mweghachi na ihe omume nke data ọnwụ.';
+$lang['configuration_backup_app_name'] = 'Nhazi nkwado ma weghachite';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Ndabere filename bụ ghara ịdị irè.';
+$lang['configuration_backup_backup_now'] = 'ndabere Ugbu a';
+$lang['configuration_backup_checking_version'] = 'Ịlele Archive version ndakọrịta';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'Ì ji n&#39;aka na ị chọrọ iji weghachi iji na-esonụ Archive faịlụ?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory enweghị ike nwee nchegharị.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Nbudata na wụnye ngwa ọdịnala. Biko na-enwe ndidi.';
+$lang['configuration_backup_file_information'] = 'File Ozi';
+$lang['configuration_backup_importing_ldap'] = 'Importing enye Òhèrèozī ọrụ nankenke directory';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Gị usoro ga-ịdenyere aha tupu Iweghachi nhazi ntọala.';
+$lang['configuration_backup_older_version_warn'] = 'Okenye na nsụgharị adịghị agụnye ngwa awụnyere pụta ìhè - biko wụnye chọrọ ngwa ọdịnala si Marketplace mgbe weghachi';
+$lang['configuration_backup_release_mismatch'] = 'The version ọmụma na nkwado ndabere na mpaghara faịlụ adabaghị na version arụnyere na usoro.';
+$lang['configuration_backup_release_missing'] = 'The version ọmụma a adịghị na nkwado ndabere na mpaghara faịlụ.';
+$lang['configuration_backup_release_unsupported:'] = 'Iweghachi si na-esonụ version akwadoghi:';
+$lang['configuration_backup_restarting_service:'] = 'Ibidogharịa ọrụ:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Weghachi-aga n&#39;ihu.';
+$lang['configuration_backup_restore_complete'] = 'Weghachi zuru ezu.';
+$lang['configuration_backup_restore_file'] = 'Archive File';
+$lang['configuration_backup_restore_from_archive'] = 'Weghachi Site Archive';
+$lang['configuration_backup_restore_log'] = 'weghachi Log';
+$lang['configuration_backup_restore_type:'] = 'Weghachi ụdị:';
+$lang['configuration_backup_starting_event_system'] = 'Malite omume monitor ọrụ';
+$lang['configuration_backup_status_data_not_available'] = 'Status data adịghị.';
+$lang['configuration_backup_stopping_event_system'] = 'Nkwụsị omume monitor ọrụ';
+$lang['configuration_backup_unable_to_start_restore'] = 'Enweghị ike na-amalite weghachi ọrụ';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking Archive faịlụ';
+$lang['configuration_backup_waiting_for_software_updates_system'] = '-Eche software mmelite usoro';
+$lang['configuration_backup_yum_busy'] = 'Ngwugwu njikwa ahụ nọ n&#39;ọrụ.';
+
