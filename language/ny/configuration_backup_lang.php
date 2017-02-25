b4581952a97f3b617befb084bfbee5aff84e2db4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:40:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'The kubwerera kasinthidwe ndi kubwezeretsa app amalola mkulu kutenga chithunzithunzi cha zoikamo onse kasinthidwe wa dongosolo, kulola kubwezeretsa zosavuta mwambo wa imfa deta.';
+$lang['configuration_backup_app_name'] = 'Kasinthidwe zosunga zobwezeretsera ndi Bwezerani';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Kubwerera filename to ndi opanda pake.';
+$lang['configuration_backup_backup_now'] = 'kubwerera Tsopano';
+$lang['configuration_backup_checking_version'] = 'Afufuze Archive Baibulo ngakhale';
+$lang['configuration_backup_clear'] = 'Chotsani';
+$lang['configuration_backup_confirm_restore'] = 'Kodi inu mukutsimikiza kuti abwezeretse ntchito zotsatirazi Archive file?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory sakanakhoza angatembenuke.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Otsitsira ndi khazikitsa mapulogalamu. Chonde pirira.';
+$lang['configuration_backup_file_information'] = 'wapamwamba Information';
+$lang['configuration_backup_importing_ldap'] = 'Yoti LDAP Directory';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Makina anu ayenera mayina pamaso kubwezeretsa zoikamo kasinthidwe.';
+$lang['configuration_backup_older_version_warn'] = 'Mabaibulo akulu mulibe app kukhazikitsa kuwonetseredwa - chonde kwabasi mapulogalamu ankafuna kwa Msika pambuyo kubwezeretsa';
+$lang['configuration_backup_release_mismatch'] = 'Mfundo Baibulo mu wapamwamba kubwerera sakugwirizana Baibulo anaika dongosolo.';
+$lang['configuration_backup_release_missing'] = 'Mfundo Baibulo sipezeka wapamwamba kubwerera.';
+$lang['configuration_backup_release_unsupported:'] = 'Kubwezeretsa kwa nkhaniyi motere sagwirizana:';
+$lang['configuration_backup_restarting_service:'] = 'Restarting utumiki:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Kubwezeretsa mu patsogolo.';
+$lang['configuration_backup_restore_complete'] = 'Kubwezeretsa kotheratu.';
+$lang['configuration_backup_restore_file'] = 'Buku Archive';
+$lang['configuration_backup_restore_from_archive'] = 'Kubwezeretsa Kuchokera Archive';
+$lang['configuration_backup_restore_log'] = 'kubwezeretsa Log';
+$lang['configuration_backup_restore_type:'] = 'Kubwezeretsa mtundu:';
+$lang['configuration_backup_starting_event_system'] = 'Kuyambira chochitika utumiki polojekiti';
+$lang['configuration_backup_status_data_not_available'] = 'Udindo deta palibe.';
+$lang['configuration_backup_stopping_event_system'] = 'Kuyima chochitika utumiki polojekiti';
+$lang['configuration_backup_unable_to_start_restore'] = 'Sangathe kuyamba kubwezeretsa ntchito';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking Archive file';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Kudikira mapulogalamu zosintha dongosolo';
+$lang['configuration_backup_yum_busy'] = 'Phukusi bwana ali wotanganidwa.';
+
