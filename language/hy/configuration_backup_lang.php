0763d1eb4a472ed96273a10a1d4a20f9e3be1ad8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:39:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'The կոնֆիգուրացիա կրկնօրնկ եւ վերականգնել հավելվածը թույլ է տալիս ադմինիստրատորին վերցնելու ՊԱՏԿԵՐԸ բոլոր կազմաձեւման պարամետրերը համակարգի, որը թույլ է տալիս հեշտ վերականգնումը դեպքում տվյալների կորստի.';
+$lang['configuration_backup_app_name'] = 'Կազմաձեւման Backup եւ վերականգնել';
+$lang['configuration_backup_archives'] = 'արխիվ';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup ֆայլի անունը անվավեր է:';
+$lang['configuration_backup_backup_now'] = 'Backup Այժմ';
+$lang['configuration_backup_checking_version'] = 'Ստուգում արխիվային տարբերակ համատեղելիությունը';
+$lang['configuration_backup_clear'] = 'պարզ';
+$lang['configuration_backup_confirm_restore'] = 'Համոզված եք, որ ցանկանում եք վերականգնել օգտագործելով հետեւյալ արխիվ ֆայլ.';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Ուղեցույց չի կարող ձեւափոխվել:';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Բեռնել եւ տեղադրել ծրագրեր. Խնդրում ենք լինել համբերատար.';
+$lang['configuration_backup_file_information'] = 'Պատկեր Տեղեկություն';
+$lang['configuration_backup_importing_ldap'] = 'Importing LDAP ցուցակում';
+$lang['configuration_backup_network_information'] = 'Network Տեղեկություն';
+$lang['configuration_backup_not_registered'] = 'Ձեր համակարգը պետք է գրանցվեն մինչեւ վերականգնման կազմաձեւման պարամետրեր.';
+$lang['configuration_backup_older_version_warn'] = 'Ավելի հին տարբերակները չեն ներառում հավելվածը տեղադրել յայտնի - Խնդրում ենք տեղադրել պահանջվող ծրագրեր է շուկայում, հետո վերականգնել';
+$lang['configuration_backup_release_mismatch'] = 'Որ տարբերակը տեղեկատվությունը կրկնօրինակ ֆայլը չի ​​համապատասխանում այն ​​տարբերակը տեղադրված է այս համակարգին:';
+$lang['configuration_backup_release_missing'] = 'Տարբերակը տեղեկատվությունը հասանելի չէ կրկնօրինակ ֆայլը.';
+$lang['configuration_backup_release_unsupported:'] = 'Վերականգնելով հետեւյալ տարբերակը չի աջակցվում:';
+$lang['configuration_backup_restarting_service:'] = 'Վերսկսելու ծառայությունը:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Վերականգնել ընթացքի մեջ է:';
+$lang['configuration_backup_restore_complete'] = 'Վերականգնել ավարտված է:';
+$lang['configuration_backup_restore_file'] = 'Արխիվ Պատկեր';
+$lang['configuration_backup_restore_from_archive'] = 'Վերականգնել արխիվային';
+$lang['configuration_backup_restore_log'] = 'Վերականգնել Մուտք';
+$lang['configuration_backup_restore_type:'] = 'Վերականգնել տեսակը:';
+$lang['configuration_backup_starting_event_system'] = 'Սկսած միջոցառումը Monitor ծառայությունը';
+$lang['configuration_backup_status_data_not_available'] = 'Status տվյալները հասանելի չեն:';
+$lang['configuration_backup_stopping_event_system'] = 'Կանգ միջոցառումը Monitor ծառայությունը';
+$lang['configuration_backup_unable_to_start_restore'] = 'Անհնար է սկսել վերականգնել շահագործումը';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking պահոց ֆայլի';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Սպասում է ծրագրային թարմացումները համակարգի';
+$lang['configuration_backup_yum_busy'] = 'Փաթեթ մենեջեր է զբաղված:';
+
