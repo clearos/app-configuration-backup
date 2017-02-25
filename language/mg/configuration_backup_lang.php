79e430cb1f307268f5bbdbc77bc0e78a73b4d2b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:45:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Ny fanahafana nomaniny ho solon&#39;izay, ary hamerina amin&#39;ny laoniny fampiharana mamela ny mpitantana mba haka ahitana ny toe-javatra rehetra fanahafana ny rafitra, hamela mora ny famerenana amin&#39;ny laoniny ao amin&#39;ny hetsika ny tahirin-kevitra very.';
+$lang['configuration_backup_app_name'] = 'Fanahafana Backup ary Ampodio';
+$lang['configuration_backup_archives'] = 'Archives';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup Filename dia tsy manan-kery.';
+$lang['configuration_backup_backup_now'] = 'Backup Ankehitriny';
+$lang['configuration_backup_checking_version'] = 'Archive fanamarinana dika mifanentana';
+$lang['configuration_backup_clear'] = 'Clear';
+$lang['configuration_backup_confirm_restore'] = 'Azonao antoka ianao te-hamerina fampiasana manaraka Archive rakitra?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory tsy hiova fo.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Downloading sy fametrahana fampiharana. Masìna ianao, hanam-paharetana.';
+$lang['configuration_backup_file_information'] = 'File Information';
+$lang['configuration_backup_importing_ldap'] = 'Fanafarana LDAP Directory';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Ny rafitra dia tsy maintsy voasoratra ara-panjakana eo anatrehan&#39;ny toe-javatra indray fanahafana.';
+$lang['configuration_backup_older_version_warn'] = 'Older dikan tsy ahitana fampiharana hametraka hita - azafady hametraka ilaina fampiharana avy amin&#39;ny Marketplace rehefa hamerina';
+$lang['configuration_backup_release_mismatch'] = 'Ny dikan-vaovao ao amin&#39;ny rakitra nomaniny ho solon&#39;izay tsy mitovy ny dikan-nametraka teo amin&#39;ity tontolo ity.';
+$lang['configuration_backup_release_missing'] = 'Ny dikan-baovao dia tsy hita ao amin&#39;ny nomaniny ho solon&#39;izay rakitra.';
+$lang['configuration_backup_release_unsupported:'] = 'Indray avy ao amin&#39;ny dikan-manaraka dia tsy manohana:';
+$lang['configuration_backup_restarting_service:'] = 'Famerenana fanompoana:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Ampodio amin&#39;ny fandrosoana.';
+$lang['configuration_backup_restore_complete'] = 'Ampodio feno.';
+$lang['configuration_backup_restore_file'] = 'Archive File';
+$lang['configuration_backup_restore_from_archive'] = 'Ampodio avy Archive';
+$lang['configuration_backup_restore_log'] = 'Ampodio Log';
+$lang['configuration_backup_restore_type:'] = 'Ampodio karazana:';
+$lang['configuration_backup_starting_event_system'] = 'Manomboka hetsika Monitor fanompoana';
+$lang['configuration_backup_status_data_not_available'] = 'Status tahirin-kevitra tsy misy.';
+$lang['configuration_backup_stopping_event_system'] = 'Nijanona hetsika Monitor fanompoana';
+$lang['configuration_backup_unable_to_start_restore'] = 'Tsy afaka hanomboka hamerina ny fandidiana';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking Archive rakitra';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Miandry ny rindrambaiko fanavaozam-baovao rafitra';
+$lang['configuration_backup_yum_busy'] = 'Package mpitantana dia be atao.';
+
