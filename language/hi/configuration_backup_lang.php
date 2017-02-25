1e8f80a60a3ba8d52fbc46c590727ff5d39fdc65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:42:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'विन्यास बैकअप और एप्लिकेशन को बहाल डेटा हानि की स्थिति में आसान बहाली की इजाजत दी प्रणाली के सभी विन्यास सेटिंग्स की एक स्नैपशॉट लेने के लिए एक प्रशासक की अनुमति देता है।';
+$lang['configuration_backup_app_name'] = 'विन्यास बैकअप और पुनर्स्थापना';
+$lang['configuration_backup_archives'] = 'अभिलेखागार';
+$lang['configuration_backup_backup_file_invalid'] = 'बैकअप फ़ाइल नाम अवैध है।';
+$lang['configuration_backup_backup_now'] = 'अब समर्थन देना';
+$lang['configuration_backup_checking_version'] = 'जांच की जा रही पुरालेख संस्करण अनुकूलता';
+$lang['configuration_backup_clear'] = 'स्पष्ट';
+$lang['configuration_backup_confirm_restore'] = 'क्या आप वाकई निम्नलिखित संग्रह फ़ाइल का उपयोग करते हुए पुनर्स्थापित करना चाहते हैं?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'निर्देशिका परिवर्तित नहीं किया जा सकता है।';
+$lang['configuration_backup_download_and_intstall_apps'] = 'डाउनलोड कर रहा है और क्षुधा स्थापित। कृपया धैर्य रखें।';
+$lang['configuration_backup_file_information'] = 'फ़ाइल जानकारी';
+$lang['configuration_backup_importing_ldap'] = 'आयात LDAP निर्देशिका';
+$lang['configuration_backup_network_information'] = 'नेटवर्क सूचना';
+$lang['configuration_backup_not_registered'] = 'अपने सिस्टम विन्यास सेटिंग्स को बहाल करने से पहले पंजीकृत होना चाहिए।';
+$lang['configuration_backup_older_version_warn'] = 'पुराने संस्करणों प्रकट स्थापित एप्लिकेशन को शामिल नहीं हैं - बाज़ार से आवश्यक क्षुधा स्थापित करें के बाद बहाल';
+$lang['configuration_backup_release_mismatch'] = 'बैकअप फ़ाइल में संस्करण जानकारी संस्करण इस सिस्टम पर स्थापित मेल नहीं खाता।';
+$lang['configuration_backup_release_missing'] = 'संस्करण जानकारी बैकअप फ़ाइल में उपलब्ध नहीं है।';
+$lang['configuration_backup_release_unsupported:'] = 'निम्नलिखित संस्करण से बहाल करना समर्थित नहीं है:';
+$lang['configuration_backup_restarting_service:'] = 'शुरु होने के बाद सेवा:';
+$lang['configuration_backup_restore_already_in_progress'] = 'प्रगति में पुनर्स्थापित।';
+$lang['configuration_backup_restore_complete'] = 'पूरा पुनर्स्थापित।';
+$lang['configuration_backup_restore_file'] = 'संग्रह फ़ाइल';
+$lang['configuration_backup_restore_from_archive'] = 'आर्काइव से बहाल';
+$lang['configuration_backup_restore_log'] = 'पुनर्स्थापित लॉग';
+$lang['configuration_backup_restore_type:'] = 'पुनर्स्थापना के प्रकार:';
+$lang['configuration_backup_starting_event_system'] = 'शुरू घटना की निगरानी सेवा';
+$lang['configuration_backup_status_data_not_available'] = 'स्थिति डेटा उपलब्ध नहीं है।';
+$lang['configuration_backup_stopping_event_system'] = 'रोकना घटना की निगरानी सेवा';
+$lang['configuration_backup_unable_to_start_restore'] = 'पुनर्स्थापना कार्रवाई प्रारंभ करने में अक्षम';
+$lang['configuration_backup_unpacking_archive'] = 'Unpacking संग्रह फ़ाइल';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'सॉफ्टवेयर अद्यतन प्रणाली के लिए प्रतीक्षा कर रहा है';
+$lang['configuration_backup_yum_busy'] = 'पैकेज प्रबंधक व्यस्त है।';
+
