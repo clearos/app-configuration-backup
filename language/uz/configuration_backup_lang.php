455502b93210a4f73a3d177a963a22bc58796ef6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:51:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'konfiguratsiya zaxira va ilovadan tiklash ma&#39;lumotlar halok taqdirda oson tiklash beruvchi, tizimining barcha konfiguratsiya parametrlarini, bir oniy tasvirini olish uchun administrator beradi.';
+$lang['configuration_backup_app_name'] = 'Konfiguratsiya Zaxira va tiklash';
+$lang['configuration_backup_archives'] = 'arxiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Zaxira filename haqiqiy emas.';
+$lang['configuration_backup_backup_now'] = 'zaxira Endi';
+$lang['configuration_backup_checking_version'] = 'arxiv versiya mosligini tekshirish';
+$lang['configuration_backup_clear'] = 'aniq';
+$lang['configuration_backup_confirm_restore'] = 'Agar quyidagi arxiv fayl yordamida qayta tiklash tashlamoqchimisiz?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Directory aylanadi bo&#39;lmadi.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Yuklab olish va ilovalarni o&#39;rnatish. iltimos, sabr qiling.';
+$lang['configuration_backup_file_information'] = 'Fayl haqida ma&#39;lumot';
+$lang['configuration_backup_importing_ldap'] = 'Import LDAP katalog';
+$lang['configuration_backup_network_information'] = 'tarmoq axborot';
+$lang['configuration_backup_not_registered'] = 'Tizim konfiguratsiya parametrlarini tiklash oldin ro&#39;yxatdan o&#39;tishingiz shart.';
+$lang['configuration_backup_older_version_warn'] = 'Older versiyalari ochiq-oydin o&#39;rnatish ilovasini o&#39;z ichiga olmaydi - keyin tiklash bozori talab ilovalarni o&#39;rnatish iltimos';
+$lang['configuration_backup_release_mismatch'] = 'zaxira faylida versiya ma&#39;lumot Ushbu tizimda o&#39;rnatilgan versiyasini mos emas.';
+$lang['configuration_backup_release_missing'] = 'versiya axborot zaxira faylida mavjud emas.';
+$lang['configuration_backup_release_unsupported:'] = 'Quyidagi versiyasiga tiklash mavjud emas:';
+$lang['configuration_backup_restarting_service:'] = 'xizmatini qayta:';
+$lang['configuration_backup_restore_already_in_progress'] = 'amalga tiklash.';
+$lang['configuration_backup_restore_complete'] = 'to&#39;liq tiklash.';
+$lang['configuration_backup_restore_file'] = 'Arxiv fayl';
+$lang['configuration_backup_restore_from_archive'] = 'Arxivini tiklash';
+$lang['configuration_backup_restore_log'] = 'Kirish tiklash';
+$lang['configuration_backup_restore_type:'] = 'turini tiklash:';
+$lang['configuration_backup_starting_event_system'] = 'Boshlang&#39;ich tadbir monitor xizmati';
+$lang['configuration_backup_status_data_not_available'] = 'Status ma&#39;lumotlar mavjud emas.';
+$lang['configuration_backup_stopping_event_system'] = 'To&#39;xtatish tadbir monitor xizmati';
+$lang['configuration_backup_unable_to_start_restore'] = 'tiklash jarayonini boshlab bo&#39;lmadi';
+$lang['configuration_backup_unpacking_archive'] = 'Ochish arxiv fayl';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'dasturiy ta&#39;minot yangilanishlar tizimi kutib';
+$lang['configuration_backup_yum_busy'] = 'To&#39;plam menejeri band.';
+
