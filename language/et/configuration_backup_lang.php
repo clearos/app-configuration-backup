b9c5620859a0cc4314dae9aeafe05f6686dc1b87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 00:41:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/configuration_backup_lang.php
@@ -0,0 +1,35 @@
+<?php
+
+$lang['configuration_backup_app_description'] = 'Konfiguratsiooni varukoopia ja taastada app võimaldab administraatori võtta läbilõige Kõikide seadistuste süsteem, mis võimaldab lihtne taastamise korral andmete kadu.';
+$lang['configuration_backup_app_name'] = 'Configuration varundamine ja taastamine';
+$lang['configuration_backup_archives'] = 'arhiiv';
+$lang['configuration_backup_backup_file_invalid'] = 'Backup faili nimi on kehtetu.';
+$lang['configuration_backup_backup_now'] = 'backup Now';
+$lang['configuration_backup_checking_version'] = 'Kontroll arhiiv versiooni ühilduvuse';
+$lang['configuration_backup_clear'] = 'selge';
+$lang['configuration_backup_confirm_restore'] = 'Oled sa kindel, et soovid taastada, kasutades järgmist arhiivfailide?';
+$lang['configuration_backup_directory_could_not_be_converted'] = 'Kataloogi ei saanud teisendada.';
+$lang['configuration_backup_download_and_intstall_apps'] = 'Allalaadimist ja installimist rakendusi. Palun ole kannatlik.';
+$lang['configuration_backup_file_information'] = 'faili info';
+$lang['configuration_backup_importing_ldap'] = 'Importimine LDAP kataloogi';
+$lang['configuration_backup_network_information'] = 'Network Information';
+$lang['configuration_backup_not_registered'] = 'Teie süsteem peab olema registreeritud enne taastamist seadistused.';
+$lang['configuration_backup_older_version_warn'] = 'Vanemad versioonid ei sisalda installireklaamide manifest - palun paigalda vajalikud rakendused Marketplace pärast taastada';
+$lang['configuration_backup_release_mismatch'] = 'Versioon teabe backup fail ei sobi versiooni arvutisse paigaldatud.';
+$lang['configuration_backup_release_missing'] = 'Versioon andmed ei ole kättesaadavad varukoopia.';
+$lang['configuration_backup_release_unsupported:'] = 'Taastamine järgmine versioon ei toeta:';
+$lang['configuration_backup_restarting_service:'] = 'Taaskäivitamine teenust:';
+$lang['configuration_backup_restore_already_in_progress'] = 'Taastamine käib.';
+$lang['configuration_backup_restore_complete'] = 'Taastamine tehtud.';
+$lang['configuration_backup_restore_file'] = 'arhiivfailide';
+$lang['configuration_backup_restore_from_archive'] = 'Taasta Arhiiv';
+$lang['configuration_backup_restore_log'] = 'Restore Logi';
+$lang['configuration_backup_restore_type:'] = 'Restore tüüp:';
+$lang['configuration_backup_starting_event_system'] = 'Alustades korral monitor teenust';
+$lang['configuration_backup_status_data_not_available'] = 'Status andmed puuduvad.';
+$lang['configuration_backup_stopping_event_system'] = 'Peatumine juhul monitori teenus';
+$lang['configuration_backup_unable_to_start_restore'] = 'Ei saa alustada taastetoimingut';
+$lang['configuration_backup_unpacking_archive'] = 'Lahtipakkimis- arhiivfailide';
+$lang['configuration_backup_waiting_for_software_updates_system'] = 'Ootan tarkvarauuendused süsteemi';
+$lang['configuration_backup_yum_busy'] = 'Pakihalduri on hõivatud.';
+
