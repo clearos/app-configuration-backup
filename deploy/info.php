f59ae50a1cf66235b19af3d309e9b5440724608f
pcbaldwin <peter@egloo.ca>
2017-08-30 16:33:42 -0400

    Added /etc/letsencrypt [tracker #16771]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'configuration_backup';
-$app['version'] = '2.3.26';
+$app['version'] = '2.4.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
